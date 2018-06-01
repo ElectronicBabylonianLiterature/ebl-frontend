68fc5e1fd96715ed496709b6af9e5aaa319d81e9
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-01 14:35:41 +0200

    Use public class field syntax for callback
    See https://www.andreasreiterer.at/web-development/bind-callback-function-react/

--- src/dictionary/WordSearch.js
+++ src/dictionary/WordSearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Input, Button } from 'element-react'
+import _ from 'lodash'
 
 class WordSearch extends Component {
   constructor (props) {
@@ -13,13 +14,13 @@ class WordSearch extends Component {
     }
   }
 
-  onChange (key, value) {
+  onChange = (key, value) => {
     this.setState({
       form: Object.assign(this.state.form, { [key]: value })
     })
   }
 
-  submit (e) {
+  submit = (e) => {
     e.preventDefault()
     fetch(`http://localhost:8000/words/${this.state.form.lemma}/${this.state.form.homonym}`)
       .then(response => response.json())
@@ -28,12 +29,12 @@ class WordSearch extends Component {
 
   render () {
     return (
-      <Form inline model={this.state.form} onSubmit={this.submit.bind(this)}>
+      <Form inline model={this.state.form} onSubmit={this.submit}>
         <Form.Item prop='lemma'>
-          <Input placeholder='lemma' onChange={this.onChange.bind(this, 'lemma')} />
+          <Input placeholder='lemma' onChange={_.partial(this.onChange, 'lemma')} />
         </Form.Item>
         <Form.Item prop='homonym'>
-          <Input placeholder='homonym' onChange={this.onChange.bind(this, 'homonym')} />
+          <Input placeholder='homonym' onChange={_.partial(this.onChange, 'homonym')} />
         </Form.Item>
         <Form.Item>
           <Button nativeType='submit' type='primary'>Query</Button>
