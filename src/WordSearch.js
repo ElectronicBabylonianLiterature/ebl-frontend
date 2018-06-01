a3377eeff192ec2b2a53710fa679564397803598
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-01 12:54:53 +0200

    Implement querying words from dictionary API

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react'
 import { Form, Input, Button } from 'element-react'
 
 class WordSearch extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      form: {
+        lemma: '',
+        homonym: ''
+      }
+    }
+  }
+
+  onChange (key, value) {
+    this.setState({
+      form: Object.assign(this.state.form, { [key]: value })
+    })
+  }
+
+  submit (e) {
+    e.preventDefault()
+    fetch(`http://localhost:8000/words/${this.state.form.lemma}/${this.state.form.homonym}`)
+      .then(response => response.json())
+      .then(this.props.onResponse)
+  }
+
   render () {
     return (
-      <Form inline>
-        <Form.Item>
-          <Input placeholder='lemma' />
+      <Form inline model={this.state.form} onSubmit={this.submit.bind(this)}>
+        <Form.Item prop='lemma'>
+          <Input placeholder='lemma' onChange={this.onChange.bind(this, 'lemma')} />
         </Form.Item>
-        <Form.Item>
-          <Input placeholder='homonym' />
+        <Form.Item prop='homonym'>
+          <Input placeholder='homonym' onChange={this.onChange.bind(this, 'homonym')} />
         </Form.Item>
         <Form.Item>
           <Button nativeType='submit' type='primary'>Query</Button>
@@ -17,6 +41,6 @@ class WordSearch extends Component {
       </Form>
     )
   }
-};
+}
 
 export default WordSearch
--- src/WordSearch.test.js
+++ src/WordSearch.test.js
@@ -1,9 +1,40 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {fireEvent, renderIntoDocument, cleanup, wait} from 'react-testing-library'
 import WordSearch from './WordSearch'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<WordSearch />, div)
-  ReactDOM.unmountComponentAtNode(div)
+let onResponse
+
+beforeEach(async () => {
+  fetch.resetMocks()
+  fetch.mockResponseOnce(JSON.stringify({ lemma: ['lemma'] }))
+  onResponse = jest.fn()
+  const {getByText, getByPlaceholderText} = renderIntoDocument(<WordSearch onResponse={onResponse} />)
+
+  const lemma = getByPlaceholderText('lemma')
+  lemma.value = 'lemma'
+  fireEvent.change(lemma)
+
+  const homonym = getByPlaceholderText('homonym')
+  homonym.value = 'homonym'
+  fireEvent.change(homonym)
+
+  await wait()
+
+  fireEvent.click(
+    getByText((content, element) => element.tagName.toLowerCase() === 'button')
+  )
+})
+
+afterEach(cleanup)
+
+it('Calls onResponse with the response JSON', async () => {
+  expect(await onResponse.mock.calls[0][0]).toEqual({ lemma: ['lemma'] })
+})
+
+it('Makes one request', async () => {
+  expect(fetch).toBeCalled()
+})
+
+it('Queries the Dictionary API with fiven parameters', async () => {
+  expect(fetch).toBeCalledWith('http://localhost:8000/words/lemma/homonym')
 })
