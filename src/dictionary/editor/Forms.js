a1d2f66d3bd4586b3e861079945fa94654fa18f9
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-21 08:21:22 +0200

    Use li in Forms

--- src/dictionary/editor/Forms.js
+++ src/dictionary/editor/Forms.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react'
-import { FormGroup, ControlLabel, Button } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { FormGroup, Button } from 'react-bootstrap'
 import _ from 'lodash'
 
 import LemmaInput from './LemmaInput'
@@ -9,23 +9,25 @@ class Forms extends Component {
   render () {
     return (
       <FormGroup>
-        <ControlLabel>Forms</ControlLabel>
-        {this.props.value.map((form, index) =>
-          <FormGroup key={index}>
-            {_.isString(form) ? (
-              <span>{form}</span>
-            ) : (
-              <Fragment>
-                <LemmaInput id={`forms-${index}-lemma`} value={form} onChange={_.noop} />
-                <ListInput id={`forms-${index}-notes`} value={form.notes} onChange={_.noop}>
-                  Notes
-                </ListInput>
-              </Fragment>
-            )}
-            <Button>Delete form</Button>
-          </FormGroup>
-        )}
-        <Button>Add form</Button>
+        <label>Forms</label>
+        <ul>
+          {this.props.value.map((form, index) =>
+            <li key={index}>
+              {_.isString(form) ? (
+                <span>{form}</span>
+              ) : (
+                <FormGroup>
+                  <LemmaInput id={`forms-${index}-lemma`} value={form} onChange={_.noop} />
+                  <ListInput id={`forms-${index}-notes`} value={form.notes} onChange={_.noop}>
+                    Notes
+                  </ListInput>
+                </FormGroup>
+              )}
+              <Button>Delete form</Button>
+            </li>
+          )}
+          <li><Button>Add form</Button></li>
+        </ul>
       </FormGroup>
     )
   }
