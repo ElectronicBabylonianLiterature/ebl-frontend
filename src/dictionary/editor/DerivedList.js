e16d3b210942b6de75313ce4477e1a0427abd5c8
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-22 10:25:59 +0200

    Introduce DerivedList

---
+++ src/dictionary/editor/DerivedList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { FormGroup, Button } from 'react-bootstrap'
+import _ from 'lodash'
+
+import FormList from './FormList'
+
+class DerivedList extends Component {
+  addDerived = () => this.props.onChange([...this.props.value, []])
+
+  deleteDerived = index => () => {
+    this.props.onChange(_.compact([
+      ...this.props.value.slice(0, index),
+      ...this.props.value.slice(index + 1)
+    ]))
+  }
+
+  updateDerived = index => group => {
+    this.props.onChange(_.compact([
+      ...this.props.value.slice(0, index),
+      group,
+      ...this.props.value.slice(index + 1)
+    ]))
+  }
+
+  render () {
+    return (
+      <FormGroup>
+        <label>{this.props.children}</label>
+        <ul>
+          {this.props.value.map((group, groupIndex) =>
+            <li key={groupIndex}>
+              <FormList
+                id={`${this.props.id}-${groupIndex}`}
+                value={group}
+                onChange={this.updateDerived(groupIndex)}
+                fields={['lemma', 'homonym', 'notes']}>{groupIndex + 1}. group</FormList>
+              <Button onClick={this.deleteDerived(groupIndex)}>Delete group</Button>
+            </li>
+          )}
+          <li><Button onClick={this.addDerived}>Add group</Button></li>
+        </ul>
+      </FormGroup>
+    )
+  }
+}
+
+export default DerivedList
---
+++ src/dictionary/editor/DerivedList.test.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import _ from 'lodash'
+import DerivedList from './DerivedList'
+import {render, cleanup, fireEvent, wait} from 'react-testing-library'
+
+afterEach(cleanup)
+
+const label = 'Derived'
+
+let value
+let element
+let onChange
+
+afterEach(cleanup)
+
+beforeEach(() => {
+  onChange = jest.fn()
+})
+
+beforeEach(() => {
+  value = [
+    [{lemma: ['a', 'b'], homonym: 'I', notes: ['note']}],
+    [{lemma: ['c', 'd'], homonym: 'II', notes: []}]
+  ]
+  element = renderDerivedList()
+})
+
+it('Displays all forms', () => {
+  _(value).flatten().forEach(item => expect(element.getByValue(item.lemma.join(' '))).toBeVisible())
+})
+
+it('Displays label', () => {
+  expect(element.getByText(label)).toBeVisible()
+})
+
+it('Adds new group when Add is cliked', async () => {
+  const add = element.getByText('Add group')
+  fireEvent.click(add)
+
+  await wait()
+
+  expect(onChange).toHaveBeenCalledWith([...value, []])
+})
+
+it('Removes group when Delete is clicked', async () => {
+  const indexToDelete = 1
+  const del = element.getAllByText('Delete group')[indexToDelete]
+  fireEvent.click(del)
+
+  await wait()
+
+  expect(onChange).toHaveBeenCalledWith(_.reject(value, (value, index) => index === indexToDelete))
+})
+
+it('Calls onChange with updated value on change', async () => {
+  const newValue = 'IV'
+  const input = element.getByValue(value[0][0].homonym)
+  input.value = newValue
+  fireEvent.change(input)
+
+  await wait()
+
+  expect(onChange).toHaveBeenCalledWith([[{...value[0][0], homonym: newValue}], ..._.tail(value)])
+})
+
+function renderDerivedList () {
+  return render(<DerivedList id='derived' value={value} onChange={onChange}>{label}</DerivedList>)
+}
