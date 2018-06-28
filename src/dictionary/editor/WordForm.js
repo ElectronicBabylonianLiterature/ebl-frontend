867ab0e7e7e1d6d66de115ab88c7288e64b2ca5a
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-28 17:43:22 +0200

    Extract methods

--- src/dictionary/editor/WordForm.js
+++ src/dictionary/editor/WordForm.js
@@ -40,13 +40,6 @@ class WordForm extends Component {
     this.updateWord({ [key]: value })
   }
 
-  jsonPreview = () => {
-    return <div>
-      <h3>JSON preview</h3>
-      <pre>{JSON.stringify(this.state.word, null, 2)}</pre>
-    </div>
-  }
-
   textInput = ({property}) => (
     <TextInput
       id={property}
@@ -56,51 +49,95 @@ class WordForm extends Component {
     </TextInput>
   )
 
+  lemma = () => (
+    <LemmaInput
+      id='lemma'
+      value={this.state.word}
+      onChange={this.updateWord} />
+  )
+
+  pos = () => (
+    <PosInput
+      id='pos'
+      value={this.state.word}
+      onChange={this.updateWord} />
+  )
+
+  amplifiedMeanings = () => (
+    <AmplifiedMeaningList
+      id={`amplifiedMeaning`}
+      value={this.state.word.amplifiedMeanings}
+      onChange={this.onChangeValue('amplifiedMeanings')}>
+      Amplified meanings
+    </AmplifiedMeaningList>
+  )
+
+  forms = () => (
+    <FormList
+      id='forms'
+      value={this.state.word.forms}
+      onChange={this.onChangeValue('forms')}
+      fields={['lemma', 'attested', 'notes']}>
+      Forms
+    </FormList>
+  )
+
+  logograms = () => (
+    <ArrayWithNotesList
+      id='logograms'
+      value={this.state.word.logograms}
+      separator=' '
+      property='logogram'
+      noun='logogram'
+      onChange={this.onChangeValue('logograms')} />
+  )
+
+  derived = () => (
+    <DerivedList
+      id='derived'
+      value={this.state.word.derived}
+      onChange={this.onChangeValue('derived')}>
+      Derived
+    </DerivedList>
+  )
+
+  derivedFrom = () => (
+    <DerivedFromInput
+      id='derivedFrom'
+      value={this.state.word.derivedFrom}
+      onChange={this.onChangeValue('derivedFrom')} />
+  )
+
+  jsonPreview = () => {
+    return <div>
+      <h3>JSON preview</h3>
+      <pre>{JSON.stringify(this.state.word, null, 2)}</pre>
+    </div>
+  }
+
   render () {
     return (
       <form className='WordForm' onSubmit={this.submit}>
-        <LemmaInput id='lemma' value={this.state.word} onChange={this.updateWord} />
+        <this.lemma />
         <this.textInput property='legacyLemma' />
         <this.textInput property='homonym' />
-        <PosInput id='pos' value={this.state.word} onChange={this.updateWord} />
+        <this.pos />
         <this.textInput property='meaning' />
-
         <hr />
 
-        <FormList
-          id='forms'
-          value={this.state.word.forms}
-          onChange={this.onChangeValue('forms')}
-          fields={['lemma', 'attested', 'notes']}>Forms</FormList>
-
+        <this.forms />
         <hr />
 
-        <AmplifiedMeaningList
-          id={`amplifiedMeaning`}
-          value={this.state.word.amplifiedMeanings}
-          onChange={this.onChangeValue('amplifiedMeanings')}>
-          Amplified meanings
-        </AmplifiedMeaningList>
-
+        <this.amplifiedMeanings />
         <hr />
 
-        <ArrayWithNotesList
-          id='logograms'
-          value={this.state.word.logograms}
-          separator=' '
-          property='logogram'
-          noun='logogram'
-          onChange={this.onChangeValue('logograms')} />
-
+        <this.logograms />
         <hr />
 
-        <DerivedList id='derived' value={this.state.word.derived} onChange={this.onChangeValue('derived')}>
-          Derived
-        </DerivedList>
-
+        <this.derived />
         <hr />
 
-        <DerivedFromInput id='derivedFrom' value={this.state.word.derivedFrom} onChange={this.onChangeValue('derivedFrom')} />
+        <this.derivedFrom />
 
         <Button type='submit' bsStyle='primary'>Save</Button>
         {process.env.NODE_ENV === 'development' && <this.jsonPreview />}
