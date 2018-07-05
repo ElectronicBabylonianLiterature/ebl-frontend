f3c629c7ce59cb8e2b12221c73258b81d428ef3d
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-05 12:39:53 +0200

    Use time element for record

--- src/fragmentarium/Record.js
+++ src/fragmentarium/Record.js
@@ -4,13 +4,15 @@ import moment from 'moment'
 
 import './Record.css'
 
-function formatDate (date) {
-  return moment(date).format('D/M/YYYY')
-}
-
 class Record extends Component {
   get record () {
     return _.sortBy(this.props.record, 'date')
+  }
+
+  date ({date}) {
+    const humanDate = moment(date).format('D/M/YYYY')
+    const machineDate = moment(date).format('YYYY-MM-DD')
+    return <time dateTime={machineDate}>{humanDate}</time>
   }
 
   render () {
@@ -18,7 +20,7 @@ class Record extends Component {
       <ol className='Record'>
         {this.record.map((entry, index) =>
           <li className='Record-entry' key={index}>
-            {entry.user} ({entry.type}, {formatDate(entry.date)})
+            {entry.user} ({entry.type}, <this.date date={entry.date} />)
           </li>
         )}
         {_.isEmpty(this.record) && <li className='Record-entry'>No record</li>}
--- src/fragmentarium/Record.test.js
+++ src/fragmentarium/Record.test.js
@@ -5,6 +5,7 @@ import Record from './Record'
 import moment from 'moment'
 
 let record
+let element
 let container
 
 afterEach(cleanup)
@@ -12,12 +13,21 @@ afterEach(cleanup)
 describe('Record has entries', () => {
   beforeEach(async () => {
     record = await factory.buildMany('record', 3)
-    container = render(<Record record={record} />).container
+    element = render(<Record record={record} />)
+    container = element.container
   })
 
   it(`Renders all entries`, () => {
     for (let entry of record) {
-      expect(container).toHaveTextContent(`${entry.user} (${entry.type}, ${moment(entry.date).format('D/M/YYYY')})`)
+      const expectedEntry = `${entry.user} (${entry.type}, ${moment(entry.date).format('D/M/YYYY')})`
+      expect(container).toHaveTextContent(expectedEntry)
+    }
+  })
+
+  it(`Entries have correct datetTime`, () => {
+    for (let entry of record) {
+      expect(element.getByText(moment(entry.date).format('D/M/YYYY')))
+        .toHaveAttribute('datetime', moment(entry.date).format('YYYY-MM-DD'))
     }
   })
 })
