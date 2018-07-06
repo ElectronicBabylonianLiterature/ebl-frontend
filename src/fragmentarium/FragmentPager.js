613528fca5b9328b07e36f65ef8aa5774d3569d6
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-06 13:37:36 +0200

    Fix pager arrows

--- src/fragmentarium/FragmentPager.js
+++ src/fragmentarium/FragmentPager.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 import './FragmentPager.css'
 
@@ -10,8 +11,15 @@ export default function FragmentPager ({number}) {
   if (match) {
     const prefix = match[1]
     const current = Number(match[2] || 0)
-    const PagerLink = ({offset, label}) =>
-      <Link to={`/fragmentarium/${prefix}${current + offset}`} aria-label={label}><i className='fas fa-angle-left' aria-hidden /></Link>
+    const PagerLink = ({offset, label}) => (
+      <Link to={`/fragmentarium/${prefix}${current + offset}`} aria-label={label}>
+        <i className={classNames({
+          fas: true,
+          'fa-angle-left': offset < 0,
+          'fa-angle-right': offset >= 0
+        })} aria-hidden />
+      </Link>
+    )
 
     return (
       <div className='FragmentPager'>
