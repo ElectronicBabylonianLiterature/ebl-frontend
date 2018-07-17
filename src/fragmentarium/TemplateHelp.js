535a16e5244811661e3aefe91e1a333378a068ed
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-17 09:38:34 +0200

    Condense template help

--- src/fragmentarium/TemplateHelp.js
+++ src/fragmentarium/TemplateHelp.js
@@ -2,46 +2,32 @@ import React from 'react'
 import { Popover } from 'react-bootstrap'
 
 const obverse = `1. [...]  [...]
-2. [...]  [...]
-3. [...]  [...]`
+2. [...]  [...]`
 
-const obverseAndReverse = `@obverse
-1. [...]  [...]
-2. [...]  [...]
-
-@reverse
-1. [...]  [...]
-2. [...]  [...]
-3. [...]  [...]`
-
-const suffix = `@obverse
+const obverseReverseAndsuffix = `@obverse
 1'. [...]  [...]
-2'. [...]  [...]
 
 @reverse
 1. [...]  [...]
-2. [...]  [...]
-3. [...]  [...]`
+2. [...]  [...]`
 
 export default function HelpPopover () {
   return (
-    <Popover id='Template help' title='Create a list of numbers with the suffix ". [...]  [...]"'>
+    <Popover id='Template help' title='Create template'>
       <p>
         If only the first number is given, then only one list is created:
-        <code>3</code> results in:
+        <code>2</code> results in:
       </p>
       <pre>{obverse}</pre>
       <p>
         If two numbers separated by a comma are given, then two lists are created,
-        the first preceded by <code>@obverse</code> and the second by <code>@reverse</code>:
-        <code>2, 3</code> results in:
+        the first preceded by <code>@obverse</code> and the second by <code>@reverse</code>.
       </p>
-      <pre>{obverseAndReverse}</pre>
       <p>
         If the numbers of the field contain a suffix, then the suffix is added to each number of the list.
-        Thus, <code>2', 3</code> results in:
+        Thus, <code>1', 2</code> results in:
       </p>
-      <pre>{suffix}</pre>
+      <pre>{obverseReverseAndsuffix}</pre>
     </Popover>
   )
 }
