20833b97a6b7d9bf9e6851daccfe5d28ab7030b4
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-17 10:43:25 +0200

    Extract methods

--- src/fragmentarium/CuneiformFragment.js
+++ src/fragmentarium/CuneiformFragment.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
 
 import Details from './Details'
@@ -14,34 +14,52 @@ class CuneiformFragment extends Component {
     return this.props.fragment
   }
 
+  leftColumn = () => (
+    <Fragment>
+      <Details fragment={this.fragment} />
+      <Record record={this.fragment.record} />
+      <OrganizationLinks
+        cdliNumber={this.fragment.cdliNumber}
+        bmIdNumber={this.fragment.bmIdNumber} />
+    </Fragment>
+  )
+
+  middleColumn = () => (
+    <Fragment>
+      <p className='CuneifromFragment__description'>
+        {this.fragment.description}
+      </p>
+      <p className='CuneifromFragment__publication'>
+        (Publication: {this.fragment.publication || '- '})
+      </p>
+      <TransliteratioForm
+        number={this.fragment._id}
+        transliteration={this.fragment.transliteration}
+        notes={this.fragment.notes}
+        apiClient={this.props.apiClient}
+        onChange={this.props.onChange} />
+    </Fragment>
+  )
+
+  rightColumn = () => (
+    <Folio
+      folio={this.fragment.folio}
+      apiClient={this.props.apiClient}
+      cdliNumber={this.fragment.cdliNumber} />
+  )
+
   render () {
     return (
       <Grid fluid>
         <Row>
           <Col md={2}>
-            <Details fragment={this.fragment} />
-            <Record record={this.fragment.record} />
-            <OrganizationLinks
-              cdliNumber={this.fragment.cdliNumber}
-              bmIdNumber={this.fragment.bmIdNumber} />
+            <this.leftColumn />
           </Col>
           <Col md={5}>
-            <p className='CuneifromFragment__description'>{this.fragment.description}</p>
-            <p className='CuneifromFragment__publication'>
-              (Publication: {this.fragment.publication || '- '})
-            </p>
-            <TransliteratioForm
-              number={this.fragment._id}
-              transliteration={this.fragment.transliteration}
-              notes={this.fragment.notes}
-              apiClient={this.props.apiClient}
-              onChange={this.props.onChange} />
+            <this.middleColumn />
           </Col>
           <Col md={5}>
-            <Folio
-              folio={this.fragment.folio}
-              apiClient={this.props.apiClient}
-              cdliNumber={this.fragment.cdliNumber} />
+            <this.rightColumn />
           </Col>
         </Row>
       </Grid>
