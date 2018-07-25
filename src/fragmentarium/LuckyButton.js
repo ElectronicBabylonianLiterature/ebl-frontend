86977df6cc4c0e8b8e25856800a5e06af8d91f73
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-25 11:30:20 +0200

    Add Spinner to LuckyButton

--- src/fragmentarium/LuckyButton.js
+++ src/fragmentarium/LuckyButton.js
@@ -1,24 +1,32 @@
 import React, { Component, Fragment } from 'react'
 import { Button } from 'react-bootstrap'
 import Error from 'Error'
+import Spinner from 'Spinner'
 import { withRouter } from 'react-router-dom'
 
 class LuckyButton extends Component {
   state = {
-    error: null
+    error: null,
+    loading: false
   }
 
   click = event => {
+    this.setState({error: null, loading: true})
     this.props.apiClient
       .fetchJson(`/fragments?random=true`, true)
       .then(fragments => this.props.history.push(`/fragmentarium/${fragments[0]._id}`))
-      .catch(error => this.setState({error: error}))
+      .catch(error => this.setState({error: error, loading: false}))
   }
 
   render () {
     return (
       <Fragment>
-        <Button bsStyle='primary' onClick={this.click}>I'm feeling lucky</Button>
+        <Button bsStyle='primary' onClick={this.click}>
+          {this.state.loading
+            ? <Spinner />
+            : 'I\'m feeling lucky'
+          }
+        </Button>
         <Error error={this.state.error} />
       </Fragment>
     )
