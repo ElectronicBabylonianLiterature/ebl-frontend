85a84eccbe0bd5d22cfb4fee84abb4b928f12675
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-25 11:19:39 +0200

    Add error display to LuckyButton

--- src/fragmentarium/LuckyButton.js
+++ src/fragmentarium/LuckyButton.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Button } from 'react-bootstrap'
+import Error from 'Error'
 import { withRouter } from 'react-router-dom'
 
 class LuckyButton extends Component {
+  state = {
+    error: null
+  }
+
   click = event => {
     this.props.apiClient
       .fetchJson(`/fragments?random=true`, true)
       .then(fragments => this.props.history.push(`/fragmentarium/${fragments[0]._id}`))
+      .catch(error => this.setState({error: error}))
   }
 
   render () {
     return (
-      <Button bsStyle='primary' onClick={this.click}>I'm feeling lucky</Button>
+      <Fragment>
+        <Button bsStyle='primary' onClick={this.click}>I'm feeling lucky</Button>
+        <Error error={this.state.error} />
+      </Fragment>
     )
   }
 }
--- src/fragmentarium/LuckyButton.test.js
+++ src/fragmentarium/LuckyButton.test.js
@@ -3,37 +3,61 @@ import { Router } from 'react-router-dom'
 import createMemoryHistory from 'history/createMemoryHistory'
 import { render, cleanup } from 'react-testing-library'
 import {factory} from 'factory-girl'
-import { whenClicked } from 'testHelpers'
+import { whenClicked, clickNth } from 'testHelpers'
 import LuckyButton from './LuckyButton'
 
 const buttonText = 'I\'m feeling lucky'
 
-let fragment
 let history
 let apiClient
 let element
 
 afterEach(cleanup)
 
-beforeEach(async () => {
-  fragment = await factory.build('fragment')
+beforeEach(() => {
   history = createMemoryHistory()
   jest.spyOn(history, 'push')
   apiClient = {
     fetchJson: jest.fn()
   }
-  apiClient.fetchJson.mockReturnValueOnce(Promise.resolve([fragment]))
   element = render(<Router history={history}><LuckyButton apiClient={apiClient} /></Router>)
 })
 
-it('Redirects to fragment when clicked', async () => {
-  await whenClicked(element, buttonText)
-    .expect(history.push)
-    .toHaveBeenCalledWith(`/fragmentarium/${fragment._id}`)
+describe('Successful request', () => {
+  let fragment
+
+  beforeEach(async () => {
+    fragment = await factory.build('fragment')
+    apiClient.fetchJson.mockReturnValueOnce(Promise.resolve([fragment]))
+  })
+
+  it('Redirects to fragment when clicked', async () => {
+    await whenClicked(element, buttonText)
+      .expect(history.push)
+      .toHaveBeenCalledWith(`/fragmentarium/${fragment._id}`)
+  })
+
+  it('Fetches random fragment from the API', async () => {
+    await whenClicked(element, buttonText)
+      .expect(apiClient.fetchJson)
+      .toHaveBeenCalledWith('/fragments?random=true', true)
+  })
 })
 
-it('Fetches random fragment from the API', async () => {
-  await whenClicked(element, buttonText)
-    .expect(apiClient.fetchJson)
-    .toHaveBeenCalledWith('/fragments?random=true', true)
+describe('Failed request', () => {
+  const message = 'Error'
+
+  beforeEach(() => {
+    apiClient.fetchJson.mockReturnValueOnce(Promise.reject(new Error(message)))
+  })
+
+  it('Shows error message', async () => {
+    await clickNth(element, buttonText, 0)
+    expect(element.container).toHaveTextContent(message)
+  })
+
+  it('Does not redirect', async () => {
+    await clickNth(element, buttonText, 0)
+    expect(history.push).not.toHaveBeenCalled()
+  })
 })
