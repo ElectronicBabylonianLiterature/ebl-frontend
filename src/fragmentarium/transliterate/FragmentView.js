1f39db95cc3928223e6bc8bced9cd273f79a2583
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-27 11:35:43 +0200

    Use withData in FragmentView

--- src/fragmentarium/transliterate/FragmentView.js
+++ src/fragmentarium/transliterate/FragmentView.js
@@ -1,83 +1,33 @@
-/* global AbortController */
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 
 import Breadcrumbs from 'Breadcrumbs'
-import Error from 'Error'
-import Spinner from 'Spinner'
 import CuneiformFragment from './CuneiformFragment'
 import FragmentPager from './FragmentPager'
+import withData from 'http/withData'
 
-class FragmentView extends Component {
-  abortController = new AbortController()
+const FragmentWithData = withData(
+  ({data, reload, apiClient}) => <CuneiformFragment
+    fragment={data}
+    apiClient={apiClient}
+    onChange={reload}
+  />,
+  props => `/fragments/${props.number}`,
+  (prevProps, props) => prevProps.number !== props.number
+)
 
-  state = {
-    fragment: null,
-    error: null
-  }
-
-  get fragmentUrl () {
-    return `/fragments/${this.props.match.params.id}`
-  }
-
-  get isLoading () {
-    return !this.state.fragment && !this.state.error
-  }
-
-  componentDidMount () {
-    this.fetchFragment()
-  }
-
-  componentDidUpdate (prevProps, prevState, snapshot) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.fetchFragment()
-    }
-  }
-
-  componentWillUnmount () {
-    this.abortController.abort()
-  }
-
-  fetchFragment = () => {
-    this.setState({fragment: null, error: null})
-    this.props.apiClient
-      .fetchJson(this.fragmentUrl, true, this.abortController.signal)
-      .then(json => this.setState({fragment: json, error: null}))
-      .catch(error => {
-        if (error.name !== 'AbortError') {
-          this.setState({fragment: null, error: error})
-        }
-      })
-  }
-
-  header = () => {
-    return (
+export default function FragmentView ({apiClient, auth, match}) {
+  const number = match.params.id
+  return (
+    <section className='App-content App-content--wide'>
       <header>
-        <Breadcrumbs section='Fragmentarium' active={this.props.match.params.id} />
-        <h2><FragmentPager number={this.props.match.params.id}> {this.props.match.params.id} </FragmentPager></h2>
+        <Breadcrumbs section='Fragmentarium' active={number} />
+        <h2><FragmentPager number={number}> {number} </FragmentPager></h2>
       </header>
-    )
-  }
-
-  render () {
-    return (
-      <section className='App-content App-content--wide'>
-        <this.header />
-        {this.props.auth.isAuthenticated()
-          ? (
-            <Fragment>
-              {this.isLoading && <section><Spinner /></section>}
-              {this.state.fragment && <CuneiformFragment
-                fragment={this.state.fragment}
-                apiClient={this.props.apiClient}
-                onChange={this.fetchFragment}
-              />}
-              <Error error={this.state.error} />
-            </Fragment>
-          ) : 'You need to be logged in to access the fragmentarium.'
-        }
-      </section>
-    )
-  }
+      {auth.isAuthenticated()
+        ? (
+          <FragmentWithData apiClient={apiClient} number={number} />
+        ) : 'You need to be logged in to access the fragmentarium.'
+      }
+    </section>
+  )
 }
-
-export default FragmentView
