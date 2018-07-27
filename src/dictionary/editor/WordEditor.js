798d0b10d0d31e13fb067c36aa3b6d2635e7f329
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-27 09:43:22 +0200

    Use withData in WordEditor

--- src/dictionary/editor/WordEditor.js
+++ src/dictionary/editor/WordEditor.js
@@ -6,33 +6,22 @@ import Breadcrumbs from 'Breadcrumbs'
 import WordForm from './WordForm'
 import Spinner from 'Spinner'
 import Error from 'Error'
+import withData from 'http/withData'
+
+function getWordUrl (props) {
+  return `/words/${props.match.params.id}`
+}
 
 class WordEditor extends Component {
   abortController = new AbortController()
 
-  state = {
-    word: null,
-    error: null,
-    saving: false
-  }
-
-  get wordUrl () {
-    return `/words/${this.props.match.params.id}`
-  }
-
-  get isLoading () {
-    return !this.state.word && !this.state.error
-  }
-
-  componentDidMount () {
-    this.props.apiClient
-      .fetchJson(this.wordUrl, true, this.abortController.signal)
-      .then(json => this.setState({word: json, error: null, saving: false}))
-      .catch(error => {
-        if (error.name !== 'AbortError') {
-          this.setState({word: null, error: error, saving: false})
-        }
-      })
+  constructor (props) {
+    super(props)
+    this.state = {
+      word: props.data,
+      error: null,
+      saving: false
+    }
   }
 
   componentWillUnmount () {
@@ -42,7 +31,7 @@ class WordEditor extends Component {
   updateWord = word => {
     this.setState({word: this.state.word, error: null, saving: true})
     this.props.apiClient
-      .postJson(this.wordUrl, word, this.abortController.signal)
+      .postJson(getWordUrl(this.props), word, this.abortController.signal)
       .then(() => this.setState({word: word, error: null, saving: false}))
       .catch(error => {
         if (error.name !== 'AbortError') {
@@ -52,25 +41,26 @@ class WordEditor extends Component {
   }
 
   render () {
-    return this.isLoading
-      ? <section className='App-content'><Spinner /></section>
-      : (
-        <section className='App-content'>
-          <header>
-            <Breadcrumbs section='Dictionary' active={this.props.match.params.id} />
-            {this.state.word && <h2>
-              Edit <strong>{this.state.word.attested === false && '*'}{this.state.word.lemma.join(' ')}</strong> {this.state.word.homonym}
-              {' '}
-              <small>({this.state.word._id})</small>
-            </h2>}
-            {this.state.word && <ReactMarkdown source={this.state.word.source} />}
-            {this.state.saving && <Spinner>Saving...</Spinner>}
-          </header>
-          {this.state.word && <WordForm value={this.state.word} onSubmit={this.updateWord} />}
-          <Error error={this.state.error} />
-        </section>
-      )
+    return (
+      <section className='App-content'>
+        <header>
+          <Breadcrumbs section='Dictionary' active={this.props.match.params.id} />
+          <h2>
+            Edit <strong>{this.state.word.attested === false && '*'}{this.state.word.lemma.join(' ')}</strong> {this.state.word.homonym}
+            {' '}
+            <small>({this.state.word._id})</small>
+          </h2>
+          <ReactMarkdown source={this.state.word.source} />
+          <Spinner loading={this.state.saving}>Saving...</Spinner>
+        </header>
+        <WordForm value={this.state.word} onSubmit={this.updateWord} />
+        <Error error={this.state.error} />
+      </section>
+    )
   }
 }
 
-export default WordEditor
+export default withData(
+  WordEditor,
+  getWordUrl
+)
