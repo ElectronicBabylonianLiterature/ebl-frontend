972e2bbd990d6550866da083ab27cc1e49f27e4d
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-27 09:31:17 +0200

    Use withData in WordSearch

--- src/dictionary/search/WordSearch.js
+++ src/dictionary/search/WordSearch.js
@@ -1,71 +1,28 @@
-/* global AbortController */
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import _ from 'lodash'
 
-import Spinner from 'Spinner'
 import Word from './Word'
-import Error from 'Error'
+import withData from 'http/withData'
 
 import './WordSearch.css'
 
-class WordSearch extends Component {
-  abortController = new AbortController()
-
-  state = {
-    words: null,
-    error: null
-  }
-
-  get isLoading () {
-    return _.isNil(this.state.error) && _.isNil(this.state.words)
-  }
-
-  fetchWords () {
-    if (_.isEmpty(this.props.query)) {
-      this.setState({words: [], error: null})
-    } else {
-      this.setState({words: null, error: null})
-      this.props.apiClient
-        .fetchJson(`/words?query=${encodeURIComponent(this.props.query)}`, true, this.abortController.signal)
-        .then(words => this.setState({words: words, error: null}))
-        .catch(error => {
-          if (error.name !== 'AbortError') {
-            this.setState({words: [], error: error})
-          }
-        })
-    }
-  }
-
-  componentDidMount () {
-    this.fetchWords()
-  }
-
-  componentDidUpdate (prevProps, prevState, snapshot) {
-    if (prevProps.query !== this.props.query) {
-      this.fetchWords()
-    }
-  }
-
-  componentWillUnmount () {
-    this.abortController.abort()
-  }
-
-  render () {
-    return this.isLoading
-      ? <Spinner />
-      : (
-        <Fragment>
-          <ul className='WordSearch-results'>
-            {this.state.words.map(word =>
-              <li key={word._id} className='WordSearch-results__result'>
-                <Word value={word} />
-              </li>
-            )}
-          </ul>
-          <Error error={this.state.error} />
-        </Fragment>
-      )
-  }
+function WordSearch ({data}) {
+  return (
+    <ul className='WordSearch-results'>
+      {data.map(word =>
+        <li key={word._id} className='WordSearch-results__result'>
+          <Word value={word} />
+        </li>
+      )}
+    </ul>
+  )
 }
 
-export default WordSearch
+export default withData(
+  WordSearch,
+  props => `/words?query=${encodeURIComponent(props.query)}`,
+  (prevProps, props) => prevProps.query !== props.query,
+  true,
+  props => !_.isEmpty(props.query),
+  []
+)
