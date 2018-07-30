f717eb03c954d0b8eef523a13b6a97f0c3e7b935
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-30 10:46:59 +0200

    Add error boundary around the App and routes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css'
 
 import Header from './Header'
 import Callback from './auth0/Callback'
-import Introduction from './Introduction'
-import Dictionary from './dictionary/search/Dictionary'
-import WordEditor from './dictionary/editor/WordEditor'
-import FragmentView from './fragmentarium//transliterate/FragmentView'
-import Fragmentarium from './fragmentarium/Fragmentarium'
-import ApiClient from './http/ApiClient'
+import Introduction from 'Introduction'
+import Dictionary from 'dictionary/search/Dictionary'
+import WordEditor from 'dictionary/editor/WordEditor'
+import FragmentView from 'fragmentarium//transliterate/FragmentView'
+import Fragmentarium from 'fragmentarium/Fragmentarium'
+import ApiClient from 'http/ApiClient'
+import ErrorBoundary from 'ErrorBoundary'
 
 class App extends Component {
   render () {
@@ -18,14 +19,16 @@ class App extends Component {
     return (
       <Fragment>
         <Header auth={this.props.auth} />
-        <Switch>
-          <Route path='/dictionary/:id' render={props => <WordEditor apiClient={apiClient} {...props} />} />
-          <Route path='/dictionary' render={props => <Dictionary auth={this.props.auth} apiClient={apiClient} {...props} />} />
-          <Route path='/fragmentarium/:id' render={props => <FragmentView auth={this.props.auth} apiClient={apiClient} {...props} />} />
-          <Route path='/fragmentarium' render={props => <Fragmentarium auth={this.props.auth} apiClient={apiClient} {...props} />} />
-          <Route path='/callback' render={props => <Callback auth={this.props.auth} {...props} />} />
-          <Route component={Introduction} />
-        </Switch>
+        <ErrorBoundary>
+          <Switch>
+            <Route path='/dictionary/:id' render={props => <WordEditor apiClient={apiClient} {...props} />} />
+            <Route path='/dictionary' render={props => <Dictionary auth={this.props.auth} apiClient={apiClient} {...props} />} />
+            <Route path='/fragmentarium/:id' render={props => <FragmentView auth={this.props.auth} apiClient={apiClient} {...props} />} />
+            <Route path='/fragmentarium' render={props => <Fragmentarium auth={this.props.auth} apiClient={apiClient} {...props} />} />
+            <Route path='/callback' render={props => <Callback auth={this.props.auth} {...props} />} />
+            <Route component={Introduction} />
+          </Switch>
+        </ErrorBoundary>
       </Fragment>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import ErrorBoundary from 'ErrorBoundary'
 import registerServiceWorker from './registerServiceWorker'
 
 import Auth from './auth0/Auth'
 
 const auth = new Auth()
 
-ReactDOM.render(<Router><App auth={auth} /></Router>, document.getElementById('root'))
+ReactDOM.render(
+  <ErrorBoundary>
+    <Router>
+      <App auth={auth} />
+    </Router>
+  </ErrorBoundary>,
+  document.getElementById('root')
+)
 registerServiceWorker()
