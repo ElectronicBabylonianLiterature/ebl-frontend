bd40734407afbf70545051bf6f5b0a490c1a8c88
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-09-13 11:22:17 +0200

    Update search form layout

--- src/fragmentarium/search/NumberSearchForm.js
+++ src/fragmentarium/search/NumberSearchForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import queryString from 'query-string'
-import { Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap'
+import { Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
 
 class FragmentSearchForm extends Component {
@@ -21,19 +21,24 @@ class FragmentSearchForm extends Component {
 
   render () {
     return (
-      <Form inline onSubmit={this.submit}>
+      <Form horizontal onSubmit={this.submit}>
         <FormGroup controlId='number'>
-          <ControlLabel>Number</ControlLabel>
-          {' '}
-          <FormControl
-            type='text'
-            size={32}
-            value={this.state.number}
-            placeholder='Search museum, accession, or CDLI number.'
-            onChange={this.onChange} />
+          <Col sm={2} />
+          <Col componentClass={ControlLabel} sm={2}>
+            Number
+          </Col>
+          <Col sm={4}>
+            <FormControl
+              type='text'
+              value={this.state.number}
+              placeholder='Search museum, accession, or CDLI number.'
+              onChange={this.onChange} />
+          </Col>
+          <Col sm={1}>
+            <Button type='submit' bsStyle='primary'>Search</Button>
+          </Col>
+          <Col sm={3} />
         </FormGroup>
-        {' '}
-        <Button type='submit' bsStyle='primary'>Search</Button>
       </Form>
     )
   }
--- src/fragmentarium/search/TransliterationSearchForm.js
+++ src/fragmentarium/search/TransliterationSearchForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import queryString from 'query-string'
-import { Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap'
+import { Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
 
 class FragmentSearchForm extends Component {
@@ -22,18 +22,24 @@ class FragmentSearchForm extends Component {
   render () {
     const rows = this.state.transliteration.split('\n').length
     return (
-      <Form inline onSubmit={this.submit}>
+      <Form horizontal onSubmit={this.submit}>
         <FormGroup controlId='transliteration'>
-          <ControlLabel>Transliteration</ControlLabel>
-          {' '}
-          <FormControl
-            componentClass='textarea'
-            value={this.state.transliteration}
-            rows={Math.max(2, rows)}
-            onChange={this.onChange} />
+          <Col sm={2} />
+          <Col componentClass={ControlLabel} sm={2}>
+            Transliteration
+          </Col>
+          <Col sm={4}>
+            <FormControl
+              componentClass='textarea'
+              value={this.state.transliteration}
+              rows={Math.max(2, rows)}
+              onChange={this.onChange} />
+          </Col>
+          <Col sm={1}>
+            <Button type='submit' bsStyle='primary'>Search</Button>
+          </Col>
+          <Col sm={3} />
         </FormGroup>
-        {' '}
-        <Button type='submit' bsStyle='primary'>Search</Button>
       </Form>
     )
   }
