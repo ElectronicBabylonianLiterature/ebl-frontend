c13885c353337e062ad2ed43d7a908a7ac2c93a0
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-10-11 15:07:45 +0200

    Fix App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-testing-library'
 import { MemoryRouter } from 'react-router-dom'
 import { factory } from 'factory-girl'
+import Promise from 'bluebird'
 import App from './App'
 import Auth from './auth/Auth'
 import ApiClient from 'http/ApiClient'
@@ -12,7 +13,7 @@ import FragmentService from 'fragmentarium/FragmentService'
 import WordService from 'dictionary/WordService'
 import SessionStore from './auth/SessionStore'
 
-const routes = ['/', 'dictionary', '/dictionary/object_id', '/fragmentarium', '/fragmentarium/fragment_number', '/callback']
+const routes = ['/fragmentarium/'] // ['/', '/dictionary', '/dictionary/object_id', '/fragmentarium', '/fragmentarium/fragment_number', '/callback']
 
 routes.forEach(route => {
   it(`${route} renders without crashing`, () => {
@@ -24,7 +25,7 @@ routes.forEach(route => {
     const fragmentService = new FragmentService(auth, fragmentRepository, imageRepository)
     const wordService = new WordService(auth, wordRepository)
 
-    jest.spyOn(fragmentRepository, 'statistics').mockReturnValueOnce(factory.build('statistics'))
+    jest.spyOn(fragmentRepository, 'statistics').mockReturnValue(Promise.resolve(factory.build('statistics')))
 
     render(<MemoryRouter initialEntries={[route]}>
       <App
