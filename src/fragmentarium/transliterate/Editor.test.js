96f41bf0f28f3cd49a2d3c85c5f47c2bac52c0f9
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-11-12 15:36:51 +0100

    Add test for Editor
    Unfortunaly, ACE seems to render only as XXX... while testing.

---
+++ src/fragmentarium/transliterate/Editor.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import Editor from './Editor'
+
+test.each([
+  ['text\nmore text', false, null],
+  ['', false, null],
+  ['value', true, null],
+  ['value', false, {}],
+  ['value', false, { lineNumber: '2' }],
+  ['value', false, { lineNumber: 'invalid' }]
+])('Renders without crashing with props %s %p %p', (value, disabled, error) => {
+  const onChange = jest.fn()
+  const name = 'transliteration'
+  render(<Editor name={name} value={value} onChange={onChange} disabled={disabled} error={error} />)
+})
