9fb970cc8e6d5b06f99a9cfa9f13c134b2a9d9ea
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2018-11-28 10:23:59 +0100

    Change the info popup text

--- src/fragmentarium/transliterate/TransliterationHelp.js
+++ src/fragmentarium/transliterate/TransliterationHelp.js
@@ -5,14 +5,16 @@ export default function TransliterationHelp () {
   return (
     <Popover id='Transliteration help' title='Special characters'>
       <p>
-        Use <code>Ctrl</code> or <code>Command</code> for characters with a macron, <code>š</code> and subindex numbers (e.g. <code>Ctrl-a</code> results in <code>ā</code>).
-        Use <code>Ctrl-'</code> for <code>ʾ</code>.
+        For characters with a macron and <code>š</code> use <code>Ctrl</code> or <code>Command</code> (e.g. <code>Ctrl-a</code> results in <code>ā</code>).
       </p>
       <p>
-        Use <code>Alt</code> or <code>Option</code> for characters with a circumflex, emphatic consonants and <code>ḫ</code> (e.g. <code>Alt-a</code> results in <code>â</code>).
+        For characters with a circumflex, emphatic consonants, subindex characters and <code>ḫ</code> use <code>Alt</code> or <code>Option</code> (e.g. <code>Alt-a</code> results in <code>â</code>).
       </p>
       <p>
-      Use <code>Shift</code> for capital letters (e.g. <code>Ctrl-Shift-a</code> results in <code>Ā</code> and <code>Alt-Shift-a</code> results in <code>Â</code>).
+        For aleph use <code>Ctrl-Alt-a</code> or <code>Command-Option-a</code>.
+      </p>
+      <p>
+        For capital letters use <code>Shift</code> (e.g. <code>Ctrl-Shift-a</code> results in <code>Ā</code> and <code>Alt-Shift-a</code> results in <code>Â</code>).
       </p>
     </Popover>
   )
