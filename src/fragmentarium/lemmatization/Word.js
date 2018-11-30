e4f9f0836f6231cf4a13e03dd20dfc60549a945c
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-11-30 13:54:44 +0100

    Remove unneeded props

--- src/fragmentarium/lemmatization/Word.js
+++ src/fragmentarium/lemmatization/Word.js
@@ -6,16 +6,16 @@ function isClickable (token) {
   return !/x|X|\/|\d+.?\.|\.\.\.|^[#$&$@]|^\s*$/.test(token.value)
 }
 
-function Word ({ token, columnIndex, rowIndex, onClick }) {
+function Word ({ token, onClick }) {
   return (
     <Fragment>
       {isClickable(token)
-        ? <Button key={columnIndex} onClick={onClick}>
+        ? <Button onClick={onClick}>
           {!_.isEmpty(token.uniqueLemma)
             ? <b>{token.value}</b>
             : token.value}
         </Button>
-        : <span key={columnIndex}>{token.value}</span>}
+        : <span>{token.value}</span>}
       {' '}
     </Fragment>
   )
