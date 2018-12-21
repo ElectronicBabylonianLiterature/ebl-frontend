5dce97535a8100c182570336b9ba248b68bd701b
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2018-12-21 16:23:00 +0100

    Use the error reporter in the ErrorBoundary

--- src/common/ErrorBoundary.js
+++ src/common/ErrorBoundary.js
@@ -1,6 +1,7 @@
-/* global Raven */
+
 import React, { Component } from 'react'
 import { Alert, Button } from 'react-bootstrap'
+import ErrorReporterContext from 'ErrorReporterContext'
 
 class ErrorBoundary extends Component {
   state = {
@@ -8,9 +9,11 @@ class ErrorBoundary extends Component {
     info: null
   }
 
+  static contextType = ErrorReporterContext;
+
   componentDidCatch (error, info) {
     this.setState({ error: error, info: info })
-    Raven.captureException(error, { extra: info })
+    this.context.captureException(error, { extra: { componentStack: expect.any(String) } })
   }
 
   render () {
@@ -20,7 +23,7 @@ class ErrorBoundary extends Component {
           <h4>Something's gone wrong.</h4>
           <p>Our team has been notified, but you can fill out a report by clicking the button below.</p>
           <p>
-            <Button bsStyle='danger' onClick={() => Raven.lastEventId() && Raven.showReportDialog()}>
+            <Button bsStyle='danger' onClick={() => this.context.showReportDialog()}>
               Send a report
             </Button>
           </p>
--- src/common/ErrorBoundary.test.js
+++ src/common/ErrorBoundary.test.js
@@ -1,18 +1,23 @@
-/* global Raven */
+
 import React from 'react'
 import { render } from 'react-testing-library'
 import { clickNth } from 'testHelpers'
 import ErrorBoundary from './ErrorBoundary'
+import ErrorReporterContext from 'ErrorReporterContext'
 
 describe('Children throw an error', () => {
   let element
   let error
+  let errorReportingService
 
   beforeEach(async () => {
     error = new Error('Error happened!')
-    Raven.lastEventId.mockReturnValueOnce('mockEventId')
+    errorReportingService = {
+      captureException: jest.fn(),
+      showReportDialog: jest.fn()
+    }
     const CrashingComponent = () => { throw error }
-    element = render(<ErrorBoundary><CrashingComponent /></ErrorBoundary>)
+    element = render(<ErrorReporterContext.Provider value={errorReportingService}><ErrorBoundary><CrashingComponent /></ErrorBoundary></ErrorReporterContext.Provider>)
   })
 
   it('Displays error message if children crash', async () => {
@@ -20,19 +25,21 @@ describe('Children throw an error', () => {
   })
 
   it('Sends report to Sentry', async () => {
-    expect(Raven.captureException).toHaveBeenCalledWith(error, { extra: { componentStack: expect.any(String) } })
+    expect(errorReportingService.captureException).toHaveBeenCalledWith(error, { extra: { componentStack: 'some string' } })
   })
 
   it('Clicking report button opens report dialog', async () => {
     await clickNth(element, 'Send a report', 0)
 
-    expect(Raven.showReportDialog).toHaveBeenCalled()
+    expect(errorReportingService.showReportDialog).toHaveBeenCalled()
   })
 })
 
 it('Displays children if they do not crash', async () => {
   const content = 'Did not crash'
-  const { container } = render(<ErrorBoundary>{content}</ErrorBoundary>)
-
+  const { container } = render(
+    <ErrorReporterContext.Provider value={null}>
+      <ErrorBoundary>{content}</ErrorBoundary>
+    </ErrorReporterContext.Provider>)
   expect(container).toHaveTextContent(content)
 })
