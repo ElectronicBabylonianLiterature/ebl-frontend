d37540211f551f436da15debe70bd770807daebd
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2018-12-21 14:15:19 +0100

    Remove describe
    Fix commit 300cf920cace686c814233a2f0218d25160004ab

--- src/common/RavenErrorReporter.test.js
+++ src/common/RavenErrorReporter.test.js
@@ -2,22 +2,20 @@
 
 import RavenErrorReporter from './RavenErrorReporter.js'
 
-describe('Error', () => {
-  const error = new Error('error')
-  const ravenErrorReporter = new RavenErrorReporter()
+const error = new Error('error')
+const ravenErrorReporter = new RavenErrorReporter()
 
-  beforeEach(async () => {
-    Raven.lastEventId.mockReturnValueOnce('mockEventId')
-  })
+beforeEach(async () => {
+  Raven.lastEventId.mockReturnValueOnce('mockEventId')
+})
 
-  it('Reports error', async () => {
-    const info = { componentStack: 'Error happened!' }
-    ravenErrorReporter.captureException(error, info)
-    expect(Raven.captureException).toHaveBeenCalledWith(error, { extra: info })
-  })
+it('Reports error', async () => {
+  const info = { componentStack: 'Error happened!' }
+  ravenErrorReporter.captureException(error, info)
+  expect(Raven.captureException).toHaveBeenCalledWith(error, { extra: info })
+})
 
-  it('Shows report dialog', async () => {
-    ravenErrorReporter.showReportDialog()
-    expect(Raven.showReportDialog).toHaveBeenCalled()
-  })
+it('Shows report dialog', async () => {
+  ravenErrorReporter.showReportDialog()
+  expect(Raven.showReportDialog).toHaveBeenCalled()
 })
