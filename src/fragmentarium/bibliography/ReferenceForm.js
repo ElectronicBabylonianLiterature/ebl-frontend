4d8b52e935aaa9db6275d050c2b10bb69c0ba606
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-01-25 17:32:19 +0100

    Add onChange to ReferenceForm

--- src/fragmentarium/bibliography/ReferenceForm.js
+++ src/fragmentarium/bibliography/ReferenceForm.js
@@ -10,6 +10,13 @@ export default class ReferenceForm extends Component {
     this.id = _.uniqueId('ReferenceForm-')
   }
 
+  handleChange = property => value => this.props.onChange({
+    ...this.props.value,
+    [property]: value
+  })
+
+  handleEvent = property => ({ target }) => this.handleChange(property)(target.value)
+
   render () {
     return (
       <form>
@@ -17,17 +24,22 @@ export default class ReferenceForm extends Component {
           <ControlLabel>ID</ControlLabel>
           <FormControl
             type='text'
-            defaultValue={this.props.value.id} />
+            value={this.props.value.id}
+            onChange={this.handleEvent('id')} />
         </FormGroup>
         <FormGroup controlId={`${this.id}-Pages`}>
           <ControlLabel>Pages</ControlLabel>
           <FormControl
             type='text'
-            defaultValue={this.props.value.pages} />
+            value={this.props.value.pages}
+            onChange={this.handleEvent('pages')} />
         </FormGroup>
         <FormGroup controlId={`${this.id}-Type`}>
           <ControlLabel>Type</ControlLabel>
-          <FormControl componentClass='select' defaultValue={this.props.value.type}>
+          <FormControl
+            componentClass='select'
+            value={this.props.value.type}
+            onChange={this.handleEvent('type')}>
             <option value='EDITION'>Edition</option>
             <option value='DISCUSSION'>Discussion</option>
             <option value='COPY'>Copy</option>
@@ -38,12 +50,13 @@ export default class ReferenceForm extends Component {
           <ControlLabel>Notes</ControlLabel>
           <FormControl
             type='text'
-            defaultValue={this.props.value.notes} />
+            defaultValue={this.props.value.notes}
+            onChange={this.handleEvent('notes')} />
         </FormGroup>
         <ArrayInput
           separator=','
           value={this.props.value.linesCited}
-          onChange={_.noop}>
+          onChange={this.handleChange('linesCited')}>
           Lines Cited
         </ArrayInput>
       </form>
--- src/fragmentarium/bibliography/ReferenceForm.test.js
+++ src/fragmentarium/bibliography/ReferenceForm.test.js
@@ -1,34 +1,44 @@
 import React from 'react'
 import { render } from 'react-testing-library'
 import { factory } from 'factory-girl'
+import _ from 'lodash'
 
-// import { whenClicked, changeValueByLabel } from 'testHelpers'
+import { whenChangedByLabel } from 'testHelpers'
 import ReferenceForm from './ReferenceForm'
 
 let reference
 let element
+let onChange
 
 beforeEach(async () => {
   reference = await factory.build('reference')
-  element = render(<ReferenceForm value={reference} />)
+  onChange = jest.fn()
+  element = render(<ReferenceForm value={reference} onChange={onChange} />)
 })
 
-it('Displays ID', () => {
-  expect(element.getByLabelText('ID').value).toEqual(reference.id)
-})
+describe.each([
+  ['ID', 'id', 'new id'],
+  ['Pages', 'pages', '3'],
+  ['Type', 'type', 'EDITION'],
+  ['Notes', 'notes', 'new notes']
+])('%s', (label, property, newValue) => {
+  it(`Has correct label and value`, () => {
+    expect(element.getByLabelText(label).value).toEqual(reference[property])
+  })
 
-it('Displays Pages', () => {
-  expect(element.getByLabelText('Pages').value).toEqual(reference.pages)
-})
-
-it('Displays Type', () => {
-  expect(element.getByLabelText('Type').value).toEqual(reference.type)
-})
-
-it('Displays Notes', () => {
-  expect(element.getByLabelText('Notes').value).toEqual(reference.notes)
+  it(`Calls onChange with updated value`, async () => {
+    await whenChangedByLabel(element, label, newValue)
+      .expect(onChange)
+      .toHaveBeenCalledWith(updatedItem => ({ ...reference, [property]: updatedItem }))
+  })
 })
 
 it('Displays Lines Cited', () => {
   expect(element.getByLabelText('Lines Cited').value).toEqual(reference.linesCited.join(','))
+})
+
+it(`Calls onChange with updated Lines Cited`, async () => {
+  await whenChangedByLabel(element, 'Lines Cited', '3.1,2')
+    .expect(onChange)
+    .toHaveBeenCalledWith(updatedItem => ({ ...reference, linesCited: updatedItem.split(',') }))
 })
