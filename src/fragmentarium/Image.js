3985f385f4196e0cf70c95c40bdcd638bd9880bd
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-01-31 14:58:22 +0100

    Inline Image

--- src/fragmentarium/Image.js
+++ src/fragmentarium/Image.js
@@ -2,13 +2,7 @@ import React from 'react'
 import BlobImage from 'common/BlobImage'
 import withData from 'http/withData'
 
-function Image (props) {
-  return (
-    <BlobImage data={props.data} alt={props.fileName} />
-  )
-}
-
 export default withData(
-  Image,
+  ({ data, fileName }) => <BlobImage data={data} alt={fileName} />,
   props => props.fragmentService.findImage(props.fileName)
 )
