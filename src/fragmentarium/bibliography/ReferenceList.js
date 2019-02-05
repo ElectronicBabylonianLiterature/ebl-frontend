8d6a9e2148dffa5f7487ad696f571e30b3d53321
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-02-05 13:14:09 +0100

    Fix ReferenceList sort

--- src/fragmentarium/bibliography/ReferenceList.js
+++ src/fragmentarium/bibliography/ReferenceList.js
@@ -11,8 +11,7 @@ const typeOrder = {
 function compare (reference, other) {
   if (reference.type !== other.type) {
     return Math.sign(
-      _.get(typeOrder, reference.type, 5),
-      _.get(typeOrder, other.type, 5)
+      _.get(typeOrder, reference.type, 5) - _.get(typeOrder, other.type, 5)
     )
   } else {
     return reference.document.author[0].family.localeCompare(other.document.author[0].family)
