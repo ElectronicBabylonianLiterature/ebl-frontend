ed3c12b31b797d7fd7f0d8b4eea1c5f19207f929
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-02-07 08:34:55 +0100

    Remove unnecessary describe

--- src/bibliography/bibliographyEntry.test.js
+++ src/bibliography/bibliographyEntry.test.js
@@ -3,43 +3,41 @@ import _ from 'lodash'
 import Cite from 'citation-js'
 import BibliographyEntry from './bibliographyEntry'
 
-describe('BibliographyEntry', () => {
-  let cslData
-  let entry
+let cslData
+let entry
 
-  beforeEach(async () => {
-    cslData = await factory.build('cslData')
-    entry = new BibliographyEntry(cslData)
-  })
+beforeEach(async () => {
+  cslData = await factory.build('cslData')
+  entry = new BibliographyEntry(cslData)
+})
 
-  test.each([
-    ['id', 'id'],
-    ['author', 'author.0.family'],
-    ['year', 'issued.date-parts.0.0'],
-    ['title', 'title'],
-    ['link', 'URL']
-  ])('%s', async (property, path) =>
-    expect(entry[property]).toEqual(_.get(cslData, path))
-  )
+test.each([
+  ['id', 'id'],
+  ['author', 'author.0.family'],
+  ['year', 'issued.date-parts.0.0'],
+  ['title', 'title'],
+  ['link', 'URL']
+])('%s', async (property, path) =>
+  expect(entry[property]).toEqual(_.get(cslData, path))
+)
 
-  test('non-dropping particle', async () => {
-    cslData = await factory.build('cslData', { author: [
-      {
-        'non-dropping-particle': 'von',
-        'family': 'Soden'
-      }
-    ] })
-    entry = new BibliographyEntry(cslData)
-    expect(entry.author).toEqual('von Soden')
-  })
+test('non-dropping particle', async () => {
+  cslData = await factory.build('cslData', { author: [
+    {
+      'non-dropping-particle': 'von',
+      'family': 'Soden'
+    }
+  ] })
+  entry = new BibliographyEntry(cslData)
+  expect(entry.author).toEqual('von Soden')
+})
 
-  test('fallback link', async () => {
-    cslData = await factory.build('cslData', { URL: null, DOI: 'doi' })
-    entry = new BibliographyEntry(cslData)
-    expect(entry.link).toEqual(`https://doi.org/${cslData.DOI}`)
-  })
+test('fallback link', async () => {
+  cslData = await factory.build('cslData', { URL: null, DOI: 'doi' })
+  entry = new BibliographyEntry(cslData)
+  expect(entry.link).toEqual(`https://doi.org/${cslData.DOI}`)
+})
 
-  test('citation', () => {
-    expect(entry.citation).toBeInstanceOf(Cite)
-  })
+test('citation', () => {
+  expect(entry.citation).toBeInstanceOf(Cite)
 })
