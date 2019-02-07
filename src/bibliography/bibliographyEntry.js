71c0db6ca558d59e5999a08788c752beef96232b
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-02-07 08:06:58 +0100

    Introduce BibliographyEntry

---
+++ src/bibliography/bibliographyEntry.js
@@ -0,0 +1,43 @@
+import _ from 'lodash'
+import Cite from 'citation-js'
+
+export default class BibliographyEntry {
+  #cslData
+
+  constructor (cslData) {
+    this.cslData = cslData
+    Object.freeze(this)
+  }
+
+  get id () {
+    return _.get(this.cslData, 'id', '')
+  }
+
+  get author () {
+    const particle = _.get(this.cslData, 'author.0.non-dropping-particle', '')
+    const family = _.get(this.cslData, 'author.0.family', '')
+    return particle
+      ? `${particle} ${family}`
+      : family
+  }
+
+  get year () {
+    return _.get(this.cslData, 'issued.date-parts.0.0', Number.NaN)
+  }
+
+  get title () {
+    return _.get(this.cslData, 'title', '')
+  }
+
+  get link () {
+    const url = this.cslData.URL
+    const doi = this.cslData.DOI
+    return url || (doi
+      ? `https://doi.org/${doi}`
+      : '')
+  }
+
+  get citation () {
+    return new Cite(this.cslData)
+  }
+}
---
+++ src/bibliography/bibliographyEntry.test.js
@@ -0,0 +1,45 @@
+import { factory } from 'factory-girl'
+import _ from 'lodash'
+import Cite from 'citation-js'
+import BibliographyEntry from './bibliographyEntry'
+
+describe('BibliographyEntry', () => {
+  let cslData
+  let entry
+
+  beforeEach(async () => {
+    cslData = await factory.build('bibliographyEntry')
+    entry = new BibliographyEntry(cslData)
+  })
+
+  test.each([
+    ['id', 'id'],
+    ['author', 'author.0.family'],
+    ['year', 'issued.date-parts.0.0'],
+    ['title', 'title'],
+    ['link', 'URL']
+  ])('%s', async (property, path) =>
+    expect(entry[property]).toEqual(_.get(cslData, path))
+  )
+
+  test('non-dropping particle', async () => {
+    cslData = await factory.build('bibliographyEntry', { author: [
+      {
+        'non-dropping-particle': 'von',
+        'family': 'Soden'
+      }
+    ] })
+    entry = new BibliographyEntry(cslData)
+    expect(entry.author).toEqual('von Soden')
+  })
+
+  test('fallback link', async () => {
+    cslData = await factory.build('bibliographyEntry', { URL: null, DOI: 'doi' })
+    entry = new BibliographyEntry(cslData)
+    expect(entry.link).toEqual(`https://doi.org/${cslData.DOI}`)
+  })
+
+  test('citation', () => {
+    expect(entry.citation).toBeInstanceOf(Cite)
+  })
+})
