e1d21ad1314b02cc78c10ec9f5cc527dc1c1b54b
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-02-13 15:38:11 +0100

    Remove duplicate elements
    Fix commit bdffc1913a02238d91680aaff16a8ee3ce47e969

--- src/fragmentarium/search/FragmentariumSearch.js
+++ src/fragmentarium/search/FragmentariumSearch.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import queryString from 'query-string'
 import Breadcrumbs from 'common/Breadcrumbs'
-import NumberSearchFormResults from 'fragmentarium/search/NumberSearchFormResults'
 import NumberSearch from 'fragmentarium/search/NumberSearch'
-import TransliterationSearchFormResults from 'fragmentarium/search/TransliterationSearchFormResults'
 import TransliterationSearch from 'fragmentarium/search/TransliterationSearch'
-import RandomButton from 'fragmentarium/RandomButton'
-import PioneersButton from 'fragmentarium/PioneersButton'
 import SessionContext from 'auth/SessionContext'
+import SearchGroup from 'fragmentarium/SearchGroup'
 
-import './FragmentariumSearch.css'
+import 'fragmentarium/Fragmentarium.css'
 
 class FragmentariumSearch extends Component {
   MainHeader = () => {
@@ -17,22 +14,6 @@ class FragmentariumSearch extends Component {
       <header>
         <Breadcrumbs section='Fragmentarium' active='Search' />
         <h2>Search</h2>
-      </header>
-    )
-  }
-
-  SectionHeader = ({ number, transliteration }) => {
-    return (
-      <header className='FragmentariumSearch-search__header'>
-        <NumberSearchFormResults number={number} />
-        <TransliterationSearchFormResults transliteration={transliteration} />
-        <div className='FragmentariumSearch-search__button-bar'>
-          <RandomButton fragmentService={this.props.fragmentService} method='random'>
-            I'm feeling lucky
-          </RandomButton>
-          {' '}
-          <PioneersButton fragmentService={this.props.fragmentService} />
-        </div>
       </header>
     )
   }
@@ -46,13 +27,15 @@ class FragmentariumSearch extends Component {
         <SessionContext.Consumer>
           {session => session.isAllowedToReadFragments()
             ? (
-              <section className='FragmentariumSearch-search'>
-                <this.SectionHeader number={number} transliteration={transliteration} />
+              <section className='Fragmentarium-search'>
+                <header className='Fragmentarium-search__header'>
+                  <SearchGroup number={number} transliteration={transliteration} />
+                </header>
                 <NumberSearch number={number} fragmentService={this.props.fragmentService} />
                 <TransliterationSearch transliteration={transliteration} fragmentService={this.props.fragmentService} />
               </section>
             )
-            : <p>You do not have the rights to access the fragmentarium.</p>
+            : <p>Please log in to browse the Fragmentarium.</p>
           }
         </SessionContext.Consumer>
       </section>
