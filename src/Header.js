b197af7b8ae07ba15222ef2ac4ff607ac35dafd8
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-02-14 15:34:38 +0100

    Use NavBar in the main header

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import _ from 'lodash'
 
 import User from './auth/User'
 
 import './Header.css'
 
 export default function Header ({ auth }) {
+  const id = _.uniqueId('Header-')
   return (
     <header className='Header'>
-      <h1 className='Header__title'>
-        <span className='Header__title-main'>electronic<br />Babylonian<br />Literature</span>
-        <small className='Header__title-abbreviation'>eBL</small>
-      </h1>
-      <nav>
-        <ul className='Header__nav'>
-          <li className='Header__nav-item'><Link to='/'>Home</Link></li>
-          <li className='Header__nav-item'><Link to='/dictionary'>Dictionary</Link></li>
-          <li className='Header__nav-item'><Link to='/fragmentarium'>Fragmentarium</Link></li>
-          <li className='Header__nav-item'><User auth={auth} /></li>
-        </ul>
-      </nav>
+      <Navbar variant='light' expand='md'>
+        <Navbar.Brand>
+          <h1 className='Header__title'>
+            <span className='Header__title-main'>electronic<br />Babylonian<br />Literature</span>
+            <small className='Header__title-abbreviation'>eBL</small>
+          </h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls={id} />
+        <Navbar.Collapse id={id}>
+          <Nav>
+            <Nav.Item><LinkContainer to='/'><Nav.Link>Home</Nav.Link></LinkContainer></Nav.Item>
+            <Nav.Item><LinkContainer to='/dictionary'><Nav.Link>Dictionary</Nav.Link></LinkContainer></Nav.Item>
+            <Nav.Item><LinkContainer to='/fragmentarium'><Nav.Link>Fragmentarium</Nav.Link></LinkContainer></Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Text>
+          <User auth={auth} />
+        </Navbar.Text>
+      </Navbar>
     </header>
   )
 }
