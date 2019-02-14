22c380bfdc492fc3784b4b4dab40b286ab3a3eaf
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-02-14 13:32:53 +0100

    Use context consumer in the Callback

--- src/auth/Callback.js
+++ src/auth/Callback.js
@@ -1,34 +1,28 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 import { Redirect } from 'react-router-dom'
 import Spinner from 'common/Spinner'
 import ErrorReporterContext from 'ErrorReporterContext'
 
 const redirectTarget = '/'
 
-class Callback extends Component {
-  constructor (props) {
-    super(props)
-    this.location = props.location
-    this.history = props.history
-    this.auth = props.auth
-  }
-
-  static contextType = ErrorReporterContext
-
-  render () {
-    if (/access_token|id_token|error/.test(this.location.hash)) {
-      this.auth.handleAuthentication()
-        .then(() => this.history.replace(redirectTarget))
-        .catch(error => {
-          this.context.captureException(error)
-          this.history.replace(redirectTarget)
-        })
-      return <Spinner />
-    } else {
-      return <Redirect to={redirectTarget} />
+function Callback ({ location, history, auth }) {
+  return <ErrorReporterContext.Consumer>
+    { ErrorReporterContext => {
+      if (/access_token|id_token|error/.test(location.hash)) {
+        auth.handleAuthentication()
+          .then(() => history.replace(redirectTarget))
+          .catch(error => {
+            ErrorReporterContext.captureException(error)
+            history.replace(redirectTarget)
+          })
+        return <Spinner />
+      } else {
+        return <Redirect to={redirectTarget} />
+      }
     }
-  }
+    }
+  </ErrorReporterContext.Consumer>
 }
 
 export default Callback
