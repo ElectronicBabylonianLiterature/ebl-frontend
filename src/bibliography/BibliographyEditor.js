9308d4ee1c464df37c14c85b9627c7fd357bfad0
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-02-15 10:16:56 +0100

    Extract BibliographyEntryFormController

--- src/bibliography/BibliographyEditor.js
+++ src/bibliography/BibliographyEditor.js
@@ -1,59 +1,21 @@
-import React, { Component } from 'react'
-import Promise from 'bluebird'
+import React from 'react'
 
 import Breadcrumbs from 'common/Breadcrumbs'
-import BibliographyEntryForm from './BibliographyEntryForm'
-import Spinner from 'common/Spinner'
-import ErrorAlert from 'common/ErrorAlert'
 import withData from 'http/withData'
-import SessionContext from 'auth/SessionContext'
+import BibliographyEntryFormController from 'bibliography/BibliographyEntryFormController'
 
-class BibliographyEditor extends Component {
-  static contextType = SessionContext
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      error: null,
-      saving: false
-    }
-    this.updatePromise = Promise.resolve()
-  }
-
-  componentWillUnmount () {
-    this.updatePromise.cancel()
-  }
-
-  get disabled () {
-    return this.state.saving || !this.context.isAllowedToWriteBibliography()
-  }
-
-  updateEntry = entry => {
-    this.updatePromise.cancel()
-    this.setState({ error: null, saving: true })
-    this.updatePromise = this.props.bibliographyRepository
-      .update(entry)
-      .then(() => this.setState({ error: null, saving: false }))
-      .catch(error => {
-        this.setState({ error: error, saving: false })
-      })
-  }
-
-  render () {
-    return (
-      <section className='App-content'>
-        <header>
-          <Breadcrumbs section='Bibliography' active={this.props.data.id} />
-          <h2>
-            Edit {this.props.data.id}
-          </h2>
-          <Spinner loading={this.state.saving}>Saving...</Spinner>
-        </header>
-        <BibliographyEntryForm value={this.props.data} onSubmit={this.updateEntry} disabled={this.disabled} />
-        <ErrorAlert error={this.state.error} />
-      </section>
-    )
-  }
+function BibliographyEditor ({ data, bibliographyRepository }) {
+  return (
+    <section className='App-content'>
+      <header>
+        <Breadcrumbs section='Bibliography' active={data.id} />
+        <h2>
+          Edit {data.id}
+        </h2>
+      </header>
+      <BibliographyEntryFormController entry={data} onSubmit={entry => bibliographyRepository.update(entry)} />
+    </section>
+  )
 }
 
 export default withData(
---
+++ src/bibliography/BibliographyEntryFormController.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Promise from 'bluebird'
+
+import BibliographyEntryForm from './BibliographyEntryForm'
+import Spinner from 'common/Spinner'
+import ErrorAlert from 'common/ErrorAlert'
+import SessionContext from 'auth/SessionContext'
+
+export default class BibliographyEntryFormController extends Component {
+  static contextType = SessionContext
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      error: null,
+      saving: false
+    }
+    this.updatePromise = Promise.resolve()
+  }
+  componentWillUnmount () {
+    this.updatePromise.cancel()
+  }
+
+  get disabled () {
+    return this.state.saving || !this.context.isAllowedToWriteBibliography()
+  }
+
+  handleSubmit = entry => {
+    this.updatePromise.cancel()
+    this.setState({ error: null, saving: true })
+    this.updatePromise = this.props.onSubmit(entry)
+      .then(() => this.setState({ error: null, saving: false }))
+      .catch(error => {
+        this.setState({ error: error, saving: false })
+      })
+  }
+
+  render () {
+    return <>
+      <BibliographyEntryForm value={this.props.entry} onSubmit={this.handleSubmit} disabled={this.disabled} />
+      <Spinner loading={this.state.saving}>Saving...</Spinner>
+      <ErrorAlert error={this.state.error} />
+    </>
+  }
+}
