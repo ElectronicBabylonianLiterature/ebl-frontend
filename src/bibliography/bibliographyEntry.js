15afe383de17fc54a75576823faf3113957b808a
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-02-25 10:44:11 +0100

    Fix private fields

--- src/bibliography/bibliographyEntry.js
+++ src/bibliography/bibliographyEntry.js
@@ -5,44 +5,44 @@ export default class BibliographyEntry {
   #cslData
 
   constructor (cslData) {
-    this.cslData = _.cloneDeep(cslData)
+    this.#cslData = _.cloneDeep(cslData)
     Object.freeze(this)
   }
 
   get id () {
-    return _.get(this.cslData, 'id', '')
+    return _.get(this.#cslData, 'id', '')
   }
 
   get author () {
-    const particle = _.get(this.cslData, 'author.0.non-dropping-particle', '')
-    const family = _.get(this.cslData, 'author.0.family', '')
+    const particle = _.get(this.#cslData, 'author.0.non-dropping-particle', '')
+    const family = _.get(this.#cslData, 'author.0.family', '')
     return particle
       ? `${particle} ${family}`
       : family
   }
 
   get year () {
-    const start = _.get(this.cslData, 'issued.date-parts.0.0', '')
-    const end = _.get(this.cslData, 'issued.date-parts.1.0', '')
+    const start = _.get(this.#cslData, 'issued.date-parts.0.0', '')
+    const end = _.get(this.#cslData, 'issued.date-parts.1.0', '')
     return end
       ? `${start}–${end}`
       : String(start)
   }
 
   get title () {
-    return _.get(this.cslData, 'title', '')
+    return _.get(this.#cslData, 'title', '')
   }
 
   get link () {
-    const url = this.cslData.URL
-    const doi = this.cslData.DOI
+    const url = this.#cslData.URL
+    const doi = this.#cslData.DOI
     return url || (doi
       ? `https://doi.org/${doi}`
       : '')
   }
 
   toHtml () {
-    return new Cite(this.cslData).format('bibliography', {
+    return new Cite(this.#cslData).format('bibliography', {
       format: 'html',
       template: 'citation-apa',
       lang: 'de-DE'
@@ -50,7 +50,7 @@ export default class BibliographyEntry {
   }
 
   toBibtex () {
-    return new Cite(this.cslData).get({
+    return new Cite(this.#cslData).get({
       format: 'string',
       type: 'string',
       style: 'bibtex'
@@ -58,7 +58,7 @@ export default class BibliographyEntry {
   }
 
   toJson () {
-    return _.cloneDeep(this.cslData)
+    return _.cloneDeep(this.#cslData)
   }
 }
 
