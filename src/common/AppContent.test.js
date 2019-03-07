1469ed780aaad13d385a1cd6cfe2f725fddf5633
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-03-07 08:50:58 +0100

    Add test for AppContent

---
+++ src/common/AppContent.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import { MemoryRouter } from 'react-router-dom'
+import AppContent from './AppContent'
+
+let element
+
+test('Title', () => {
+  element = render(<MemoryRouter><AppContent section='Dictionary' active='Active' title='Title' /></MemoryRouter>)
+  expect(element.container).toHaveTextContent('eBLDictionaryActiveTitle')
+})
+
+test('Section and active', () => {
+  element = render(<MemoryRouter><AppContent section='Dictionary' active='Active' /></MemoryRouter>)
+  expect(element.container).toHaveTextContent('eBLDictionaryActive')
+})
+
+test('Section', () => {
+  element = render(<MemoryRouter><AppContent section='The Section' /></MemoryRouter>)
+  expect(element.container).toHaveTextContent('eBLThe Section')
+})
+
+test('No props', () => {
+  element = render(<MemoryRouter><AppContent /></MemoryRouter>)
+  expect(element.container).toHaveTextContent('eBL')
+})
+
+test('Children', () => {
+  element = render(<MemoryRouter><AppContent>Children</AppContent></MemoryRouter>)
+  expect(element.container).toHaveTextContent('eBLChildren')
+})
