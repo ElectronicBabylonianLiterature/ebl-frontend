15294522d02db119de79d427d80994221e80ca3d
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-03-13 10:00:10 +0100

    Improve naming
    Fix commit eae2505814e0d0ac33e292eb20a61381c7845565

--- src/corpus/Corpus.js
+++ src/corpus/Corpus.js
@@ -3,23 +3,23 @@ import { Container, Row, Col } from 'react-bootstrap'
 import Image from 'fragmentarium/Image'
 import AppContent from 'common/AppContent'
 import ReactMarkdown from 'react-markdown'
-import CorpusTexts from './CorpusTexts.json'
+import corpusTexts from './CorpusTexts.json'
 
 class Corpus extends Component {
-  CreateRow (text, index) {
+  createRow (text, index) {
     return <Row as='li' key={index}> <Col md={8}> <ReactMarkdown source={text.text} /> </Col> <Col md={4}> {text.verses} </Col> </Row>
   }
 
-  Texts = () => {
+  texts = () => {
     return (
-      CorpusTexts.map((block, index) =>
+      corpusTexts.map((block, index) =>
         <Fragment key={index}>
           {block.hasOwnProperty('genre')
             ? <h3> {block.genre} </h3>
             : ''}
           <Container fluid as='ol'>
             {block.texts.map((text, index) =>
-              this.CreateRow(text, index)
+              this.createRow(text, index)
             )}
           </Container>
         </Fragment>
@@ -33,7 +33,7 @@ class Corpus extends Component {
         <Container fluid>
           <Row>
             <Col md={5}>
-              <this.Texts />
+              <this.texts />
             </Col>
             <Col md={7}>
               <Image fragmentService={this.props.fragmentService} fileName='LibraryCropped.svg' />
