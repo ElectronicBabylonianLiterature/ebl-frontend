009c861cc7c8d1d8c2714ee25e89d3f78a95d4d6
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-03-15 09:13:46 +0100

    Add help popover to uncurated references

--- src/fragmentarium/view/UncuratedReferences.js
+++ src/fragmentarium/view/UncuratedReferences.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { Popover } from 'react-bootstrap'
+import _ from 'lodash'
+import HelpTrigger from 'common/HelpTrigger'
+
+function UncuratedReferencesteHelp () {
+  return (
+    <Popover id={_.uniqueId('TemplateHelp-')}>
+      <p>
+        <dfn>Uncurated references</dfn> have been obtained by performing a search on a collection of PDFs.
+        The names of the references are the filenames of the PDFs.
+        Uncurated references should be transformed into proper bibliographical references manually.
+      </p>
+    </Popover>
+  )
+}
 
 export default function UncuratedReferences ({ uncuratedReferences }) {
-  return <p>{uncuratedReferences.count()} uncurated references</p>
+  return <p><HelpTrigger overlay={UncuratedReferencesteHelp()} /> {uncuratedReferences.count()} uncurated references</p>
 }
