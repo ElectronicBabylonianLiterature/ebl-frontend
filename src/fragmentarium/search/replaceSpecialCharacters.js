f0748d55144a64dea1b5c3394fbf899485650112
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-03-27 13:13:51 +0100

    Build regexp from characters
    Fix commit 0c680cb869b0b7392a70c8d74714e38e97ca08c4

---
+++ src/fragmentarium/search/replaceSpecialCharacters.js
@@ -0,0 +1,33 @@
+import _ from 'lodash'
+
+const characters = {
+  'c': 'š',
+  'sz': 'š',
+  's,': 'ṣ',
+  'ş': 'ṣ',
+  't,': 'ṭ',
+  'ḫ': 'h',
+  'j': 'g',
+  'ŋ': 'g',
+  'ĝ': 'g',
+  'g̃': 'g',
+  'C': 'Š',
+  'SZ': 'Š',
+  'S,': 'Ṣ',
+  'Ş': 'Ṣ',
+  'T,': 'Ṭ',
+  'Ḫ': 'H',
+  'J': 'G',
+  'Ŋ': 'G',
+  'G̃': 'G',
+  'Ĝ': 'G'
+}
+
+export default function replaceSpecialCharacters (userInput) {
+  const specialCharacters = Object.keys(characters)
+    .map(character => {
+      return _.escapeRegExp(character)
+    })
+  const regExp = new RegExp(specialCharacters.join('|'), 'g')
+  return userInput.replace(regExp, match => characters[match] || match)
+}
