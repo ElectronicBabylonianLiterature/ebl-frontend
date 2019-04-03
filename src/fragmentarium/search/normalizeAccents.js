fd3b3a1bf35b32ca138da4f5862f5e6638128075
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-04-03 20:15:30 +0200

    Extract configuration

--- src/fragmentarium/search/normalizeAccents.js
+++ src/fragmentarium/search/normalizeAccents.js
@@ -1,23 +1,5 @@
 import _ from 'lodash'
-
-const charactersWithAccents = {
-  'á': { 'letter': 'a', 'index': '₂' },
-  'à': { 'letter': 'a', 'index': '₃' },
-  'é': { 'letter': 'e', 'index': '₂' },
-  'è': { 'letter': 'e', 'index': '₃' },
-  'í': { 'letter': 'i', 'index': '₂' },
-  'ì': { 'letter': 'i', 'index': '₃' },
-  'ú': { 'letter': 'u', 'index': '₂' },
-  'ù': { 'letter': 'u', 'index': '₃' },
-  'Á': { 'letter': 'A', 'index': '₂' },
-  'À': { 'letter': 'A', 'index': '₃' },
-  'É': { 'letter': 'E', 'index': '₂' },
-  'È': { 'letter': 'E', 'index': '₃' },
-  'Í': { 'letter': 'I', 'index': '₂' },
-  'Ì': { 'letter': 'I', 'index': '₃' },
-  'Ú': { 'letter': 'U', 'index': '₂' },
-  'Ù': { 'letter': 'U', 'index': '₃' }
-}
+import charactersWithAccents from './charactersWithAccents.json'
 
 export default function normalizeAccents (userInput) {
   const specialCharacters = Object.keys(charactersWithAccents)
