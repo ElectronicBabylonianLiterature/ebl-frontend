17c6263a65c28bfe8797f27cd384701973bb4940
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-04-05 15:30:43 +0200

    Improve test cases

--- src/fragmentarium/search/normalizeNumbers.test.js
+++ src/fragmentarium/search/normalizeNumbers.test.js
@@ -6,8 +6,8 @@ test.each([
   ['ar14', 'ar₁₄'],
   ['pak44', 'pak₄₄'],
   ['ar2-ka10-nu33', 'ar₂-ka₁₀-nu₃₃'],
-  ['5 KUR', '5 KUR'],
-  ['1: ar-mu', '1: ar-mu'],
+  ['5 KUR LA3.PA2', '5 KUR LA₃.PA₂'],
+  ['1. ar-mu', '1. ar-mu'],
   ['9(ILIMMU5)', '9(ILIMMU₅)'],
   ['NAGA@180', 'NAGA@180'],
   ['|3×AN|', '|3×AN|']
