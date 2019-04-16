12fc083922eafd4ac0779ed5e455409fc2631c49
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-04-16 14:33:52 +0200

    Extract ManuscriptForm

--- src/corpus/ChapterView.js
+++ src/corpus/ChapterView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Button, Col, Alert, Badge, Nav, InputGroup } from 'react-bootstrap'
+import { Form, Button, Col, Alert, Badge, Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import ReactMarkdown from 'react-markdown'
 import _ from 'lodash'
@@ -9,8 +9,8 @@ import ListForm from 'common/List'
 import withData from 'http/withData'
 import Spinner from 'common/Spinner'
 import ErrorAlert from 'common/ErrorAlert'
-import { Manuscript, periodModifiers, periods, provenances, types } from './text'
-import ReferencesForm from 'bibliography/ReferencesForm'
+import { Manuscript } from './text'
+import ManuscriptForm from './ManuscriptForm'
 
 function DetailsRow ({ chapter }) {
   return (
@@ -33,88 +33,6 @@ function DetailsRow ({ chapter }) {
       </Form.Group>
     </Form.Row>
   )
-}
-
-function ManuscriptForm ({ manuscript, onChange, searchBibliography }) {
-  const handleChange = property => event => onChange(manuscript.set(property, event.target.value))
-  const handelRecordChange = (property, values) => event => onChange(manuscript.set(property, values.get(event.target.value)))
-
-  return <>
-    <Form.Row>
-      <Form.Group as={Col} controlId={_.uniqueId('manuscript-')}>
-        <Form.Label>Siglum</Form.Label>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>
-              {manuscript.provenance.abbreviation}
-              {manuscript.period.abbreviation}
-              {manuscript.type.abbreviation}
-            </InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control value={manuscript.siglumDisambiguator} onChange={handleChange('siglumDisambiguator')} />
-        </InputGroup>
-      </Form.Group>
-      <Form.Group as={Col} controlId={_.uniqueId('manuscript-')}>
-        <Form.Label>Museum Number</Form.Label>
-        <Form.Control value={manuscript.museumNumber} onChange={handleChange('museumNumber')} />
-      </Form.Group>
-      <Form.Group as={Col} controlId={_.uniqueId('manuscript-')}>
-        <Form.Label>Accession</Form.Label>
-        <Form.Control value={manuscript.accession} onChange={handleChange('accession')} />
-      </Form.Group>
-    </Form.Row>
-    <Form.Row>
-      <Form.Group as={Col} controlId={_.uniqueId('manuscript-')}>
-        <Form.Label>Provenance</Form.Label>
-        <Form.Control as='select' value={manuscript.provenance.name} onChange={handelRecordChange('provenance', provenances)}>
-          {provenances.toIndexedSeq().map(provenance =>
-            _.isNil(provenance.parent)
-              ? <option key={provenance.name} value={provenance.name}>
-                {provenance.name}
-              </option>
-              : <option key={provenance.name} value={provenance.name}>
-                &nbsp;&nbsp;&nbsp;&nbsp;{provenance.name}
-              </option>
-          )}
-        </Form.Control>
-      </Form.Group>
-      <Form.Group as={Col}>
-        <label>Period</label>
-        <InputGroup>
-          <Form.Control as='select' aria-label='Period modifier' value={manuscript.periodModifier.name} onChange={handelRecordChange('periodModifier', periodModifiers)} >
-            {periodModifiers.toIndexedSeq().map(modifier => <option key={modifier.name} value={modifier.name}>{modifier.displayName}</option>)}
-          </Form.Control>
-          <Form.Control as='select' aria-label='Period' value={manuscript.period.name} onChange={handelRecordChange('period', periods)}>
-            {periods.toIndexedSeq().map(period =>
-              _.isNil(period.parent)
-                ? <option key={period.name} value={period.name}>
-                  {period.name} {period.description}
-                </option>
-                : <option key={period.name} value={period.name}>
-                  &nbsp;&nbsp;&nbsp;&nbsp;{period.name} {period.description}
-                </option>
-            )}
-          </Form.Control>
-        </InputGroup>
-      </Form.Group>
-      <Form.Group as={Col} controlId={_.uniqueId('manuscript-')}>
-        <Form.Label>Type</Form.Label>
-        <Form.Control as='select' value={manuscript.type.name} onChange={handelRecordChange('type', types)}>
-          {types.toIndexedSeq().map(type => <option key={type.name} value={type.name}>{type.name}</option>)}
-        </Form.Control>
-      </Form.Group>
-    </Form.Row>
-    <Form.Group controlId={_.uniqueId('manuscript-')}>
-      <Form.Label>Notes</Form.Label>
-      <Form.Control value={manuscript.notes} onChange={handleChange('notes')} />
-    </Form.Group>
-    <ReferencesForm
-      value={manuscript.references}
-      label='References'
-      onChange={value => onChange(manuscript.set('references', value))}
-      searchBibliography={searchBibliography}
-      collapsed />
-  </>
 }
 
 function ChapterView ({ text, stage, name, onChange, onSubmit, searchBibliography, disabled }) {
