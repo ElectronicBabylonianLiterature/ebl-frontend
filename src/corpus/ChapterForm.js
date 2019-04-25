67d83bf955250f1e9c592b33d667c79970630b49
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-04-25 11:39:58 +0200

    Extract ChapterManuscripts

--- src/corpus/ChapterForm.js
+++ src/corpus/ChapterForm.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Form, Button, Col } from 'react-bootstrap'
 import _ from 'lodash'
-import { Seq, Range } from 'immutable'
-import ListForm from 'common/List'
-import { createManuscript } from './text'
-import ManuscriptForm from './ManuscriptForm'
+import ChapterManuscripts from './ChapterManuscripts'
 
 function ChapterDetails ({ chapter }) {
   return (
@@ -27,35 +24,6 @@ function ChapterDetails ({ chapter }) {
       </Form.Group>
     </Form.Row>
   )
-}
-
-function populateIds (manuscripts) {
-  const existingIds = manuscripts.map(manuscript => manuscript.id)
-  if (existingIds.includes(null)) {
-    const ids = Range((existingIds.max() || 0) + 1)
-    const map_ = (manuscripts, ids) => {
-      if (manuscripts.isEmpty()) {
-        return Seq.Indexed()
-      } else {
-        const manuscript = manuscripts.first()
-        return _.isNil(manuscript.id)
-          ? Seq.Indexed.of(manuscript.set('id', ids.first())).concat(map_(manuscripts.rest(), ids.rest()))
-          : Seq.Indexed.of(manuscript).concat(map_(manuscripts.rest(), ids))
-      }
-    }
-    return map_(manuscripts, ids).toList()
-  } else {
-    return manuscripts
-  }
-}
-
-function ChapterManuscripts ({ chapter, onChange, searchBibliography }) {
-  const handeManuscriptsChange = manuscripts => onChange(chapter.set('manuscripts', populateIds(manuscripts)))
-  return <ListForm label='Manuscripts' noun='manuscript' default={createManuscript()} value={chapter.manuscripts} onChange={handeManuscriptsChange}>
-    {chapter.manuscripts.map((manuscript, index) =>
-      <ManuscriptForm key={index} manuscript={manuscript} searchBibliography={searchBibliography} />
-    )}
-  </ListForm>
 }
 
 export default function ChapterForm ({ onSubmit, disabled, chapter, searchBibliography, onChange }) {
---
+++ src/corpus/ChapterManuscripts.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import _ from 'lodash'
+import { Seq, Range } from 'immutable'
+import ListForm from 'common/List'
+import { createManuscript } from './text'
+import ManuscriptForm from './ManuscriptForm'
+
+function populateIds (manuscripts) {
+  const existingIds = manuscripts.map(manuscript => manuscript.id)
+  if (existingIds.includes(null)) {
+    const ids = Range((existingIds.max() || 0) + 1)
+    const map_ = (manuscripts, ids) => {
+      if (manuscripts.isEmpty()) {
+        return Seq.Indexed()
+      } else {
+        const manuscript = manuscripts.first()
+        return _.isNil(manuscript.id)
+          ? Seq.Indexed.of(manuscript.set('id', ids.first())).concat(map_(manuscripts.rest(), ids.rest()))
+          : Seq.Indexed.of(manuscript).concat(map_(manuscripts.rest(), ids))
+      }
+    }
+    return map_(manuscripts, ids).toList()
+  } else {
+    return manuscripts
+  }
+}
+
+export default function ChapterManuscripts ({ chapter, onChange, searchBibliography }) {
+  const handeManuscriptsChange = manuscripts => onChange(chapter.set('manuscripts', populateIds(manuscripts)))
+  return <ListForm label='Manuscripts' noun='manuscript' default={createManuscript()} value={chapter.manuscripts} onChange={handeManuscriptsChange}>
+    {chapter.manuscripts.map((manuscript, index) => <ManuscriptForm key={index} manuscript={manuscript} searchBibliography={searchBibliography} />)}
+  </ListForm>
+}
