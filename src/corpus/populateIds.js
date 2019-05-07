6d147be78a30690dc62dbcad5e73ac6332520ff9
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-05-07 10:52:58 +0200

    Refactor populateIds

--- src/corpus/populateIds.js
+++ src/corpus/populateIds.js
@@ -1,23 +1,26 @@
 import _ from 'lodash'
 import { Seq, Range } from 'immutable'
 
+function createIdRange (existingIds) {
+  return Range((existingIds.max() || 0) + 1)
+}
+
+function setIds (manuscripts, ids) {
+  function setId (manuscript, manuscripts) {
+    return _.isNil(manuscript.id)
+      ? Seq.Indexed.of(manuscript.set('id', ids.first())).concat(setIds(manuscripts, ids.rest()))
+      : Seq.Indexed.of(manuscript).concat(setIds(manuscripts, ids))
+  }
+
+  return manuscripts.isEmpty()
+    ? Seq.Indexed()
+    : setId(manuscripts.first(), manuscripts.rest())
+}
+
 export default function populateIds (manuscripts) {
   const existingIds = manuscripts.map(manuscript => manuscript.id)
 
-  if (existingIds.includes(null)) {
-    const ids = Range((existingIds.max() || 0) + 1)
-    const map_ = (manuscripts, ids) => {
-      if (manuscripts.isEmpty()) {
-        return Seq.Indexed()
-      } else {
-        const manuscript = manuscripts.first()
-        return _.isNil(manuscript.id)
-          ? Seq.Indexed.of(manuscript.set('id', ids.first())).concat(map_(manuscripts.rest(), ids.rest()))
-          : Seq.Indexed.of(manuscript).concat(map_(manuscripts.rest(), ids))
-      }
-    }
-    return map_(manuscripts, ids).toList()
-  } else {
-    return manuscripts
-  }
+  return existingIds.includes(null)
+    ? setIds(manuscripts, createIdRange(existingIds)).toList()
+    : manuscripts
 }
