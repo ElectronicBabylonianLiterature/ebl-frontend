2995f3248ee3c8a44189670e44fbd248e11d844f
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-05-15 12:52:17 +0200

    Fix the compare function
    Fix commit 928e3421e4d390165b4b44e4b7383bb18dbccd86

--- src/fragmentarium/compareStrings.js
+++ src/fragmentarium/compareStrings.js
@@ -1,68 +1,56 @@
 import _ from 'lodash'
 import alphabet from './alphabet'
 
-function throwNotAStringError () {
-  throw new Error('The input is not a string')
-}
-
 function isString (word, anotherWord) {
-  return (_.isString(word) && _.isString(anotherWord)) ? true : throwNotAStringError()
-}
-
-function splitWord (word, anotherWord, wordToSplit) {
-  if (isString(word, anotherWord)) {
-    return wordToSplit.split('')
+  let notStrings = [word, anotherWord].filter(word => !_.isString(word))
+  if (!_.isEmpty(notStrings)) {
+    throw new TypeError(`${notStrings} is not a string`)
   }
 }
 
-function isInTheAlphabet (character) {
-  return alphabet.indexOf(character) !== -1
+function replaceIgnoredCharacters (word) {
+  return word.replace(/\]|\?|\[|-|\]|\./g, '')
 }
 
-function checkForInvalidCharacters (wordAsArray, anotherWordAsArray) {
-  if (!wordAsArray.every(isInTheAlphabet) || !anotherWordAsArray.every(isInTheAlphabet)) {
-    throw new Error('Invalid character(s) in the input')
+function checkForInvalidCharacters (word, anotherWord) {
+  let invalidCharacters = word.split('').concat(anotherWord.split('')).filter(character => !alphabet.includes(character))
+  if (!_.isEmpty(invalidCharacters)) {
+   throw new Error(`Invalid character(s) ${invalidCharacters} in the input`)
   }
 }
 
-function hasHigherAlphabetIndex (wordAsArray, anotherWordAsArray, indexOfCharacter) {
-  return alphabet.indexOf(wordAsArray[indexOfCharacter]) > alphabet.indexOf(anotherWordAsArray[indexOfCharacter])
+function hasHigherAlphabetIndex (word, anotherWord, indexOfCharacter) {
+  return alphabet.indexOf(word[indexOfCharacter]) > alphabet.indexOf(anotherWord[indexOfCharacter]) 
 }
 
-function hasLowerAlphabetIndex (wordAsArray, anotherWordAsArray, indexOfCharacter) {
-  return alphabet.indexOf(wordAsArray[indexOfCharacter]) < alphabet.indexOf(anotherWordAsArray[indexOfCharacter])
+function hasLowerAlphabetIndex (word, anotherWord, indexOfCharacter) {
+  return alphabet.indexOf(word[indexOfCharacter]) < alphabet.indexOf(anotherWord[indexOfCharacter])
 }
 
-function areFinalCharacters (wordAsArray, anotherWordAsArray, indexOfCharacter) {
-  return indexOfCharacter === (wordAsArray.length - 1) && indexOfCharacter === (anotherWordAsArray.length - 1)
-}
-
-function haveEqualAlphabetIndices (wordAsArray, anotherWordAsArray, indexOfCharacter) {
-  return alphabet.indexOf(wordAsArray[indexOfCharacter]) === alphabet.indexOf(anotherWordAsArray[indexOfCharacter])
-}
-
-function areFinalCharactersAndHaveEqualAlphabetIndices (wordAsArray, anotherWordAsArray, indexOfCharacter) {
-  return areFinalCharacters(wordAsArray, anotherWordAsArray, indexOfCharacter) && haveEqualAlphabetIndices(wordAsArray, anotherWordAsArray, indexOfCharacter)
-}
-
-function areEmptyArrays (wordAsArray, anotherWordAsArray) {
-  return wordAsArray.length === 0 && anotherWordAsArray.length === 0
+function areFinalCharacters (word, anotherWord, indexOfCharacter) {
+  return indexOfCharacter === (word.length - 1) && indexOfCharacter === (anotherWord.length - 1)
 }
 
 export default function compareStrings (word, anotherWord) {
-  let wordAsArray = splitWord(word, anotherWord, word)
+  isString(word, anotherWord)
+  let replacedWord = replaceIgnoredCharacters(word)
+  let anotherWordReplaced = replaceIgnoredCharacters(anotherWord)
+  checkForInvalidCharacters(replacedWord, anotherWordReplaced)
 
-  let anotherWordAsArray = splitWord(word, anotherWord, anotherWord)
-
-  checkForInvalidCharacters(wordAsArray, anotherWordAsArray)
-
-  for (let indexOfCharacter = 0; indexOfCharacter <= wordAsArray.length; indexOfCharacter++) {
-    if (hasHigherAlphabetIndex(wordAsArray, anotherWordAsArray, indexOfCharacter)) {
+  for (let indexOfCharacter = 0; indexOfCharacter < replacedWord.length; indexOfCharacter++) {
+    if (hasHigherAlphabetIndex(replacedWord, anotherWordReplaced, indexOfCharacter)) {
       return 1
-    } else if (hasLowerAlphabetIndex(wordAsArray, anotherWordAsArray, indexOfCharacter)) {
+    } else if (hasLowerAlphabetIndex(replacedWord, anotherWordReplaced, indexOfCharacter)) {
       return -1
-    } else if (areFinalCharactersAndHaveEqualAlphabetIndices(wordAsArray, anotherWordAsArray, indexOfCharacter) || areEmptyArrays(wordAsArray, anotherWordAsArray)) {
+    } else if (areFinalCharacters(replacedWord, anotherWordReplaced, indexOfCharacter)) {
+      return 0
+    } 
+}
+    if (replacedWord.length > anotherWordReplaced.length) {
+      return 1
+    } else if (replacedWord.length < anotherWordReplaced.length) {
+      return -1
+    } else {
       return 0
     }
-  }
 }
--- src/fragmentarium/compareStrings.test.js
+++ src/fragmentarium/compareStrings.test.js
@@ -5,63 +5,58 @@ test.each([
   ['abullu', 'Abullu', 1],
   ['Abullu', 'Ābullu', -1],
   ['Ābullu', 'Abullu', 1],
-  ['Ābullu', 'Âbullu', -1],
   ['Âbullu', 'Ābullu', 1],
-  ['abullu', 'abullu₄', -1],
   ['abullu₄', 'abullu', 1],
-  ['aba', 'bba', -1],
   ['bba', 'aba', 1],
   ['šab', 'ṣab', 1],
-  ['ṣab', 'šab', -1],
-  ['abâ', 'arā', -1],
   ['arā', 'abâ', 1],
   ['abu', 'aba', 1],
-  ['aba', 'abu', -1],
   ['abu', 'abu', 0],
-  ['abū', 'abû', -1],
   ['abû', 'abū', 1],
-  ['abu', 'abum', -1],
+  ['abu[', 'abum', -1],
   ['abum', 'abu', 1],
-  ['abu', 'abuma', -1],
   ['abuma', 'abu', 1],
-  ['abama', 'abu', -1],
   ['abu', 'abama', 1],
-  ['', 'abu', -1],
   ['abu', '', 1],
-  ['', '', 0]
+  ['', '', 0],
+  ['KUR.KUR', 'KUR-KUR?',  0],
+  ['Amar-Suen', 'Amar-Suen?', 0],
+  ['[Uruk]', '[Uruk]?', 0]
 ])('compares %s and %s', (word, anotherWord, expected) => {
   const comparedWords = compareStrings(word, anotherWord)
   expect(comparedWords).toBe(expected)
+  const comparedWordsReversed = compareStrings(anotherWord, word)
+  if (expected !== 0) {
+    expect(comparedWordsReversed).toBe(-expected)
+  }
 })
 
 test.each([
-  ['', 2],
-  [53, ''],
-  [29, 98]
-])('throws an error if the %s or %s is not a string', (word, anotherWord) => {
+  ['', 2, '2'],
+  [53, '', '53'],
+  [29, 98, '29,98']
+])('throws an error if the %s or %s is not a string', (word, anotherWord, number) => {
   function compareInvalidWords () {
     compareStrings(word, anotherWord)
   }
-  expect(compareInvalidWords).toThrowError('The input is not a string')
+  expect(compareInvalidWords).toThrowError(`${number} is not a string`)
 })
 
 test.each([
-  ['  ', ' '],
-  ['bälu', 'bitu'],
-  ['ḫarru', 'bitu'],
-  ['belü', 'bytu'],
-  ['ü', '('],
-  ['', 'abalu*'],
-  ['[a]maru', 'bitu'],
-  ['belu\\EN', 'bitu?'],
-  ['\'belu\'', 'bitu'],
-  ['EN=belu', 'amtu'],
-  ['Amar-Suen', 'Hammurabi'],
-  ['matate', 'KUR.KUR'],
-  ['matate;', 'amtu']
-])('throws an error on word(s) with invalid character(s) %s or %s', (word, anotherWord) => {
+  ['', 'abalu*', '*'],
+  ['a&maru', 'bitu', '&'],
+  ['belu\\EN', 'bitu', '\\'],
+  ['\'belu\'', 'bitu', '\',\''],
+  ['EN=belu', 'amtu', '='],
+  ['Amar|Suen', 'Hammurabi', '|'],
+  ['matate;', 'amtu', ';'],
+  ['  ', '', ' , '],
+  ['bälu!', 'bitu', 'ä,!'],
+  ['ḫarru', 'bjtu', 'ḫ,j'],
+  ['belü!', 'byt(u)', 'ü,!,(,)']
+])('throws an error on word(s) with invalid character(s) %s or %s', (word, anotherWord, invalidCharacters) => {
   function compareInvalidWords () {
     compareStrings(word, anotherWord)
   }
-  expect(compareInvalidWords).toThrowError('Invalid character(s) in the input')
+  expect(compareInvalidWords).toThrowError(`Invalid character(s) ${invalidCharacters} in the input`)
 })
