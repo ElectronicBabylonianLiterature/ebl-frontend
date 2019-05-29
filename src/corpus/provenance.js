a1591f1f26f930e7faaed51358688de5806a6ed9
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-05-29 09:31:33 +0200

    Remove non-ASCII identifiers

--- src/corpus/provenance.js
+++ src/corpus/provenance.js
@@ -8,140 +8,145 @@ const createProvenance: RecordFactory<ProvenanceProps> = Record({
   parent: null
 })
 export type Provenance = RecordOf<ProvenanceProps>
-export const provenances: OrderedMap<string, Provenance> = OrderedMap({
-  Assyria: createProvenance({ name: 'Assyria', abbreviation: 'Assa' }),
-  Aššur: createProvenance({
-    name: 'Aššur',
-    abbreviation: 'Ašš',
-    parent: 'Assyria'
-  }),
-  Ḫuzirina: createProvenance({
-    name: 'Ḫuzirina',
-    abbreviation: 'Huz',
-    parent: 'Assyria'
-  }),
-  Kalḫu: createProvenance({
-    name: 'Kalḫu',
-    abbreviation: 'Kal',
-    parent: 'Assyria'
-  }),
-  Khorsabad: createProvenance({
-    name: 'Khorsabad',
-    abbreviation: 'Kho',
-    parent: 'Assyria'
-  }),
-  Nineveh: createProvenance({
-    name: 'Nineveh',
-    abbreviation: 'Nin',
-    parent: 'Assyria'
-  }),
-  Tarbiṣu: createProvenance({
-    name: 'Tarbiṣu',
-    abbreviation: 'Tar',
-    parent: 'Assyria'
-  }),
-  Babylonia: createProvenance({ name: 'Babylonia', abbreviation: 'Baba' }),
-  Babylon: createProvenance({
-    name: 'Babylon',
-    abbreviation: 'Bab',
-    parent: 'Babylonia'
-  }),
-  Borsippa: createProvenance({
-    name: 'Borsippa',
-    abbreviation: 'Bor',
-    parent: 'Babylonia'
-  }),
-  Cutha: createProvenance({
-    name: 'Cutha',
-    abbreviation: 'Cut',
-    parent: 'Babylonia'
-  }),
-  Isin: createProvenance({
-    name: 'Isin',
-    abbreviation: 'Isn',
-    parent: 'Babylonia'
-  }),
-  Kiš: createProvenance({
-    name: 'Kiš',
-    abbreviation: 'Kiš',
-    parent: 'Babylonia'
-  }),
-  Larsa: createProvenance({
-    name: 'Larsa',
-    abbreviation: 'Lar',
-    parent: 'Babylonia'
-  }),
-  Meturan: createProvenance({
-    name: 'Meturan',
-    abbreviation: 'Met',
-    parent: 'Babylonia'
-  }),
-  Nērebtum: createProvenance({
-    name: 'Nērebtum',
-    abbreviation: 'Nēr',
-    parent: 'Babylonia'
-  }),
-  Nippur: createProvenance({
-    name: 'Nippur',
-    abbreviation: 'Nip',
-    parent: 'Babylonia'
-  }),
-  Sippar: createProvenance({
-    name: 'Sippar',
-    abbreviation: 'Sip',
-    parent: 'Babylonia'
-  }),
-  Šaduppûm: createProvenance({
-    name: 'Šaduppûm',
-    abbreviation: 'Šad',
-    parent: 'Babylonia'
-  }),
-  Ur: createProvenance({ name: 'Ur', abbreviation: 'Ur', parent: 'Babylonia' }),
-  Uruk: createProvenance({
-    name: 'Uruk',
-    abbreviation: 'Urk',
-    parent: 'Babylonia'
-  }),
-  Periphery: createProvenance({ name: 'Periphery', abbreviation: '' }),
-  Alalakh: createProvenance({
-    name: 'Alalakh',
-    abbreviation: 'Ala',
-    parent: 'Periphery'
-  }),
-  'Tell el-Amarna': createProvenance({
-    name: 'Tell el-Amarna',
-    abbreviation: 'Ama',
-    parent: 'Periphery'
-  }),
-  Emar: createProvenance({
-    name: 'Emar',
-    abbreviation: 'Emr',
-    parent: 'Periphery'
-  }),
-  Ḫattuša: createProvenance({
-    name: 'Ḫattuša',
-    abbreviation: 'Hat',
-    parent: 'Periphery'
-  }),
-  Mari: createProvenance({
-    name: 'Mari',
-    abbreviation: 'Mar',
-    parent: 'Periphery'
-  }),
-  Megiddo: createProvenance({
-    name: 'Megiddo',
-    abbreviation: 'Meg',
-    parent: 'Periphery'
-  }),
-  Susa: createProvenance({
-    name: 'Susa',
-    abbreviation: 'Sus',
-    parent: 'Periphery'
-  }),
-  Ugarit: createProvenance({
-    name: 'Ugarit',
-    abbreviation: 'Uga',
-    parent: 'Periphery'
-  }),
-  Uncertain: createProvenance({ name: 'Uncertain', abbreviation: 'Unc' })
-})
+export const provenances: OrderedMap<
+  string,
+  Provenance
+> = OrderedMap().withMutations(map =>
+  [
+    createProvenance({ name: 'Assyria', abbreviation: 'Assa' }),
+    createProvenance({
+      name: 'Aššur',
+      abbreviation: 'Ašš',
+      parent: 'Assyria'
+    }),
+    createProvenance({
+      name: 'Ḫuzirina',
+      abbreviation: 'Huz',
+      parent: 'Assyria'
+    }),
+    createProvenance({
+      name: 'Kalḫu',
+      abbreviation: 'Kal',
+      parent: 'Assyria'
+    }),
+    createProvenance({
+      name: 'Khorsabad',
+      abbreviation: 'Kho',
+      parent: 'Assyria'
+    }),
+    createProvenance({
+      name: 'Nineveh',
+      abbreviation: 'Nin',
+      parent: 'Assyria'
+    }),
+    createProvenance({
+      name: 'Tarbiṣu',
+      abbreviation: 'Tar',
+      parent: 'Assyria'
+    }),
+    createProvenance({ name: 'Babylonia', abbreviation: 'Baba' }),
+    createProvenance({
+      name: 'Babylon',
+      abbreviation: 'Bab',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Borsippa',
+      abbreviation: 'Bor',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Cutha',
+      abbreviation: 'Cut',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Isin',
+      abbreviation: 'Isn',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Kiš',
+      abbreviation: 'Kiš',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Larsa',
+      abbreviation: 'Lar',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Meturan',
+      abbreviation: 'Met',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Nērebtum',
+      abbreviation: 'Nēr',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Nippur',
+      abbreviation: 'Nip',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Sippar',
+      abbreviation: 'Sip',
+      parent: 'Babylonia'
+    }),
+    createProvenance({
+      name: 'Šaduppûm',
+      abbreviation: 'Šad',
+      parent: 'Babylonia'
+    }),
+    createProvenance({ name: 'Ur', abbreviation: 'Ur', parent: 'Babylonia' }),
+    createProvenance({
+      name: 'Uruk',
+      abbreviation: 'Urk',
+      parent: 'Babylonia'
+    }),
+    createProvenance({ name: 'Periphery', abbreviation: '' }),
+    createProvenance({
+      name: 'Alalakh',
+      abbreviation: 'Ala',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Tell el-Amarna',
+      abbreviation: 'Ama',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Emar',
+      abbreviation: 'Emr',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Ḫattuša',
+      abbreviation: 'Hat',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Mari',
+      abbreviation: 'Mar',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Megiddo',
+      abbreviation: 'Meg',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Susa',
+      abbreviation: 'Sus',
+      parent: 'Periphery'
+    }),
+    createProvenance({
+      name: 'Ugarit',
+      abbreviation: 'Uga',
+      parent: 'Periphery'
+    }),
+    createProvenance({ name: 'Uncertain', abbreviation: 'Unc' })
+  ].forEach(provenance => map.set(provenance.name, provenance))
+)
