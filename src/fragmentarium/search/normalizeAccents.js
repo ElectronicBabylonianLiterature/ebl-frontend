ed2d1f6f23df9eb76005400a0662db7b25c8bceb
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-06-05 11:40:08 +0200

    Move constants to top level

--- src/fragmentarium/search/normalizeAccents.js
+++ src/fragmentarium/search/normalizeAccents.js
@@ -2,21 +2,21 @@ import charactersWithAccents from './charactersWithAccents.json'
 import specialTransliterationCharacters from './specialTransliterationCharacters.json'
 import escapeRegExp from './escapeRegExp.js'
 
+const charactersWithAccentsAsString = escapeRegExp(
+  Object.keys(charactersWithAccents)
+)
+
+const specialTransliterationCharactersAsString = escapeRegExp(
+  specialTransliterationCharacters
+)
+
+const accentExpression = new RegExp(
+  `(${charactersWithAccentsAsString})(${specialTransliterationCharactersAsString}|)\\w*`,
+  'g'
+)
+
 export default function normalizeAccents (userInput) {
-  const charactersWithAccentsAsString = escapeRegExp(
-    Object.keys(charactersWithAccents)
-  )
-
-  const specialTransliterationCharactersAsString = escapeRegExp(
-    specialTransliterationCharacters
-  )
-
-  const regExp = new RegExp(
-    `(${charactersWithAccentsAsString})(${specialTransliterationCharactersAsString}|)\\w*`,
-    'g'
-  )
-
-  return userInput.replace(regExp, match => {
+  return userInput.replace(accentExpression, match => {
     const subindex = match
       .split('')
       .map(character =>
