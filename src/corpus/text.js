cb89b5db821d8f8ee1ddb266b207f5be48bf772a
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-06-13 13:41:00 +0200

    Fiy type

--- src/corpus/text.js
+++ src/corpus/text.js
@@ -64,7 +64,7 @@ export type ManuscriptLine = {|
   labels: Array<string>,
   number: string,
   atf: string,
-  atfTokens?: AtfToken[]
+  atfTokens: AtfToken[]
 |}
 export const createManuscriptLine: (
   $Shape<ManuscriptLine>
