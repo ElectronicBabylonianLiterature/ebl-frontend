45dc2f46887f0e92cea5fe24557e3f1e45e2d208
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-06-13 14:08:04 +0200

    Extract components

--- src/corpus/ChapterAlignment.js
+++ src/corpus/ChapterAlignment.js
@@ -1,9 +1,7 @@
 // @flow
 import React from 'react'
-import type { Chapter, ManuscriptLine } from './text'
+import type { Chapter, Line, ManuscriptLine } from './text'
 import { Badge, Col, Form, Button } from 'react-bootstrap'
-
-type Props = { chapter: Chapter }
 
 function getSiglum(chapter: Chapter, manuscriptLine: ManuscriptLine) {
   const manuscript = chapter.manuscripts.find(
@@ -16,37 +14,56 @@ function getSiglum(chapter: Chapter, manuscriptLine: ManuscriptLine) {
   }
 }
 
-export default function ChapterAlignment({ chapter }: Props) {
+function Reconstruction(props: { line: Line }) {
+  return (
+    <Form.Row>
+      <Col md={3}>{props.line.number}</Col>
+      <Col md={9}>{props.line.reconstruction}</Col>
+    </Form.Row>
+  )
+}
+
+function ManuscriptAlignment(props: {
+  chapter: Chapter,
+  manuscriptLine: ManuscriptLine
+}) {
+  return (
+    <Form.Row>
+      <Col md={1} />
+      <Col md={1}>{getSiglum(props.chapter, props.manuscriptLine)}</Col>
+      <Col md={1}>
+        {props.manuscriptLine.labels} {props.manuscriptLine.number}
+      </Col>
+      <Col md={9}>
+        {props.manuscriptLine.atfTokens.map((token, index) =>
+          token.lemmatizable ? (
+            <span key={index}>
+              <Button size="sm" variant="outline-dark">
+                {token.value}
+              </Button>{' '}
+            </span>
+          ) : (
+            <span key={index}>{token.value} </span>
+          )
+        )}
+      </Col>
+    </Form.Row>
+  )
+}
+
+export default function ChapterAlignment({ chapter }: { chapter: Chapter }) {
   return (
     <>
       <Badge variant="danger">WIP</Badge>
       {chapter.lines.map((line, index) => (
         <section key={index}>
-          <Form.Row>
-            <Col md={3}>{line.number}</Col>
-            <Col md={9}>{line.reconstruction}</Col>
-          </Form.Row>
+          <Reconstruction line={line} />
           {line.manuscripts.map((manuscript, index) => (
-            <Form.Row key={index}>
-              <Col md={1} />
-              <Col md={1}>{getSiglum(chapter, manuscript)}</Col>
-              <Col md={1}>
-                {manuscript.labels} {manuscript.number}
-              </Col>
-              <Col md={9}>
-                {manuscript.atfTokens.map((token, index) =>
-                  token.lemmatizable ? (
-                    <span key={index}>
-                      <Button size="sm" variant="outline-dark">
-                        {token.value}
-                      </Button>{' '}
-                    </span>
-                  ) : (
-                    <span key={index}>{token.value} </span>
-                  )
-                )}
-              </Col>
-            </Form.Row>
+            <ManuscriptAlignment
+              key={index}
+              chapter={chapter}
+              manuscriptLine={manuscript}
+            />
           ))}
         </section>
       ))}
