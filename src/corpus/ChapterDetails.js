adc31bddadbda94fd527061f3e1e7f2d914e00ad
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-07-22 13:17:14 +0200

    Move ChapterDetails to new file

---
+++ src/corpus/ChapterDetails.js
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react'
+import { Form, Col } from 'react-bootstrap'
+import _ from 'lodash'
+import type { Chapter } from './text'
+
+export default function ChapterDetails({ chapter }: { chapter: Chapter }) {
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
+          <Form.Label>Classification</Form.Label>
+          <Form.Control plaintext readOnly value={chapter.classification} />
+        </Form.Group>
+        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
+          <Form.Label>Stage</Form.Label>
+          <Form.Control plaintext readOnly value={chapter.stage} />
+        </Form.Group>
+        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
+          <Form.Label>Version</Form.Label>
+          <Form.Control plaintext readOnly value={chapter.version} />
+        </Form.Group>
+        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
+          <Form.Label>Name</Form.Label>
+          <Form.Control plaintext readOnly value={chapter.name} />
+        </Form.Group>
+      </Form.Row>
+    </Form>
+  )
+}
--- src/corpus/ChapterEditor.js
+++ src/corpus/ChapterEditor.js
@@ -1,35 +1,11 @@
 import React from 'react'
-import { Form, Col, Tabs, Tab } from 'react-bootstrap'
+import { Tabs, Tab } from 'react-bootstrap'
 import _ from 'lodash'
 import ChapterManuscripts from './manuscripts/ChapterManuscripts'
 import ChapterLines from './lines/ChapterLines'
 import ChapterAlignment from 'corpus/alignment/ChapterAlignment'
 import SessionContext from 'auth/SessionContext'
-
-function ChapterDetails({ chapter }) {
-  return (
-    <Form>
-      <Form.Row>
-        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
-          <Form.Label>Classification</Form.Label>
-          <Form.Control plaintext readOnly value={chapter.classification} />
-        </Form.Group>
-        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
-          <Form.Label>Stage</Form.Label>
-          <Form.Control plaintext readOnly value={chapter.stage} />
-        </Form.Group>
-        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
-          <Form.Label>Version</Form.Label>
-          <Form.Control plaintext readOnly value={chapter.version} />
-        </Form.Group>
-        <Form.Group as={Col} controlId={_.uniqueId('ChapterView-')}>
-          <Form.Label>Name</Form.Label>
-          <Form.Control plaintext readOnly value={chapter.name} />
-        </Form.Group>
-      </Form.Row>
-    </Form>
-  )
-}
+import ChapterDetails from './ChapterDetails'
 
 export default function ChapterEditor({
   onSaveLines,
