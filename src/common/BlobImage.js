ffe440580f3f31ccda0da7851b716f332a8bf45e
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-10-11 10:41:12 +0200

    Only create/revoke object URL if data changes

--- src/common/BlobImage.js
+++ src/common/BlobImage.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image } from 'react-bootstrap'
 import ExternalLink from './ExternalLink'
 
@@ -12,8 +12,12 @@ export default function BlobImage({
   hasLink?: boolean,
   alt?: string
 }) {
-  const objectUrl = URL.createObjectURL(data)
-  useEffect(() => () => URL.revokeObjectURL(objectUrl))
+  const [objectUrl, setObjectUrl] = useState()
+  useEffect(() => {
+    const url = URL.createObjectURL(data)
+    setObjectUrl(url)
+    return () => URL.revokeObjectURL(url)
+  }, [data])
 
   const image = <Image src={objectUrl} alt={alt} fluid />
   return hasLink ? (
