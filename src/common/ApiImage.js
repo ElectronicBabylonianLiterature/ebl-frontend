288a0a5a27648146ee43b00049ea4393cb486b62
Jussi Laasonen <jussi.laasonen@iki.fi>
2019-10-11 14:00:54 +0000

    Remove unused imports

--- src/common/ApiImage.js
+++ src/common/ApiImage.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
-import BlobImage from 'common/BlobImage'
-import withData from 'http/withData'
 import { apiUrl } from 'http/ApiClient'
 
 export default function ApiImage({ fileName }) {
