3f70dbbd5a35ae483e60fe06a24bb12f2feca692
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-10-24 15:08:44 +0200

    Add prop types to TransliterationHeader

--- src/fragmentarium/ui/view/TransliterationHeader.tsx
+++ src/fragmentarium/ui/view/TransliterationHeader.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 import './TransliterationHeader.css'
+import { Fragment } from 'fragmentarium/domain/fragment';
 
-export default function TransliterationHeader({ fragment }) {
+export default function TransliterationHeader({ fragment }: { fragment: Fragment }) {
   return (
     <header>
       {fragment.description.split('\n').map((paragraph, index) => (
