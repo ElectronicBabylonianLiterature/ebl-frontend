f016191e5f7bd447fdedec2d435e9b21d7a57d84
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-10-28 14:15:19 +0100

    Add typesto FragmentPager

--- src/fragmentarium/ui/view/FragmentPager.tsx
+++ src/fragmentarium/ui/view/FragmentPager.tsx
@@ -1,15 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, FunctionComponent } from 'react'
 import classNames from 'classnames'
 import FragmentLink from 'fragmentarium/ui/FragmentLink'
 
 const numberRegexp = /^([^\d]*)(\d+)$/
 
-export default function FragmentPager({ number, children }) {
+type LinkProps = {
+  offset: number
+  label: string
+}
+type Props = {
+  number: string
+}
+const FragmentPager: FunctionComponent<Props> = ({ number, children }) => {
   const match = numberRegexp.exec(number)
   if (match) {
     const prefix = match[1]
     const current = Number(match[2] || 0)
-    const PagerLink = ({ offset, label }) => (
+    const PagerLink = ({ offset, label }: LinkProps) => (
       <FragmentLink number={`${prefix}${current + offset}`} aria-label={label}>
         <i
           className={classNames({
@@ -30,6 +37,7 @@ export default function FragmentPager({ number, children }) {
       </Fragment>
     )
   } else {
-    return children
+    return <>children</>
   }
 }
+export default FragmentPager
