3f2957fac340ebe26bb8577c51a8d3629ace0fc7
yCobanoglu <56864644+yCobanoglu@users.noreply.github.com>
2019-11-13 13:31:07 +0100

    added four different syntax highlight/colours (#7)
    * added three different syntax highlight/colours
    
    * AtfMode to ts & change Atf Token color
    
    * Added Comment explaining token colors

---
+++ src/editor/AtfMode.ts
@@ -0,0 +1,50 @@
+import 'brace/mode/java'
+
+// @ts-ignore
+const acequire = window.ace.acequire
+
+export class AtfHighlightRules extends acequire('ace/mode/text_highlight_rules')
+  .TextHighlightRules {
+  $rules
+
+  constructor() {
+    super()
+    this.$rules = {
+      /*token values are chosen solely based on color and have no meaning
+      This is important when changing the overall theme one could have to
+      change tokens too !
+       */
+      start: [
+        {
+          token: 'variable.parameter',
+          regex: '^@.*$'
+        },
+        {
+          token: 'markup.list',
+          regex: '^\\$.*$'
+        },
+        {
+          token: 'comment.line.number-sign',
+          regex: '^#.*$'
+        },
+        {
+          token: 'string',
+          regex: '[\\[\\]]'
+        },
+        {
+          token: 'string',
+          regex: '\\.\\.\\.'
+        }
+      ]
+    }
+  }
+}
+
+export default class AtfMode extends acequire('ace/mode/java').Mode {
+  HighlightRules
+
+  constructor() {
+    super()
+    this.HighlightRules = AtfHighlightRules
+  }
+}
