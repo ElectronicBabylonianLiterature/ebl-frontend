88156e905f25784f1bcab22635a09ceb7a4bd3db
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-11-20 10:55:29 +0100

    Move Sentry error filtering to beforeSend

--- src/common/SentryErrorReporter.ts
+++ src/common/SentryErrorReporter.ts
@@ -1,36 +1,45 @@
 import * as Sentry from '@sentry/browser'
 import { ErrorReporter } from 'ErrorReporterContext'
 
+function isIgnored(error: string | Error | null | undefined): boolean {
+  if (error) {
+    return ['ApiError', 'AbortError'].includes(error['name'])
+  } else {
+    return false
+  }
+}
+
 class SentryErrorReporter implements ErrorReporter {
-  static init(dsn, environment) {
+  static init(dsn: string, environment: string): void {
     Sentry.init({
       dsn,
-      environment
+      environment,
+      beforeSend(event, hint) {
+        return hint && isIgnored(hint.originalException) ? null : event
+      }
     })
   }
 
-  captureException(error, errorInfo = {}) {
-    if (!['ApiError', 'AbortError'].includes(error.name)) {
-      Sentry.withScope(scope => {
-        Object.keys(errorInfo).forEach(key => {
-          scope.setExtra(key, errorInfo[key])
-        })
-        Sentry.captureException(error)
+  captureException(error: Error, errorInfo = {}): void {
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key])
       })
-    }
+      Sentry.captureException(error)
+    })
   }
 
-  showReportDialog() {
+  showReportDialog(): void {
     Sentry.showReportDialog()
   }
 
-  setUser(id, username, eblName) {
+  setUser(id, username, eblName): void {
     Sentry.configureScope(scope => {
       scope.setUser({ id, username, eblName })
     })
   }
 
-  clearScope() {
+  clearScope(): void {
     Sentry.configureScope(scope => {
       scope.clear()
     })
