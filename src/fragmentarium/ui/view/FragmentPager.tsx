9f66b8a3afd919d7b063e5a8dd192556e33dd1cc
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2019-11-20 14:04:03 +0100

    FragmentPager Arrows reverted and fix failed tests

--- src/fragmentarium/ui/view/FragmentPager.tsx
+++ src/fragmentarium/ui/view/FragmentPager.tsx
@@ -9,9 +9,6 @@ type Props = {
   children
 }
 const FragmentPager: FunctionComponent<Props> = ({ data, children }) => {
-  //data_dict = {data[direction].fragmentNumber
-  console.log(data)
-  console.log(children)
   const PagerLinkNext = ({ nextFragmentNumber }) => (
     <FragmentLink number={nextFragmentNumber}>
       <i
@@ -38,10 +35,10 @@ const FragmentPager: FunctionComponent<Props> = ({ data, children }) => {
   return (
     <Fragment>
       <PagerLinkPrevious
-        previousFragmentNumber={data['next']['fragmentNumber']}
+        previousFragmentNumber={data['previous']['fragmentNumber']}
       />
       {children}
-      <PagerLinkNext nextFragmentNumber={data['previous']['fragmentNumber']} />
+      <PagerLinkNext nextFragmentNumber={data['next']['fragmentNumber']} />
     </Fragment>
   )
 }
@@ -51,9 +48,8 @@ export default withData<
   any
 >(
   ({ data, ...props }) => <FragmentPager data={data} {...props} />,
-  props =>
-    props.fragmentService.fragmentPager(props.children, props.fragmentNumber),
+  props => props.fragmentService.fragmentPager(props.fragmentNumber),
   {
-    watch: props => [props.children, props.fragmentNumber]
+    watch: props => [props.fragmentNumber]
   }
 )
