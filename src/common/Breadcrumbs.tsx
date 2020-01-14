bebc8f3dcaabe4778321b0985451b09b0c724a38
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-01-14 15:38:13 +0100

    Add model for bread crumbs

--- src/common/Breadcrumbs.tsx
+++ src/common/Breadcrumbs.tsx
@@ -3,17 +3,46 @@ import { Breadcrumb } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import _ from 'lodash'
 
-const sections = {
-  eBL: '/',
-  Bibliography: '/bibliography',
-  Corpus: '/corpus',
-  Dictionary: '/dictionary',
-  Fragmentarium: '/fragmentarium'
+export interface Crumb {
+  readonly text: string
+  readonly link: string | null | undefined
 }
 
-function SectionCrumb({ section }) {
-  const sectionLink = sections[section]
-  const sectionItem = <Breadcrumb.Item>{section}</Breadcrumb.Item>
+export class SectionCrumb implements Crumb {
+  static readonly SECTIONS: ReadonlyMap<string, string> = new Map([
+    ['eBL', '/'],
+    ['Bibliography', '/bibliography'],
+    ['Corpus', '/corpus'],
+    ['Dictionary', '/dictionary'],
+    ['Fragmentarium', '/fragmentarium']
+  ])
+
+  readonly text: string
+
+  constructor(section: string) {
+    this.text = section
+  }
+
+  get link(): string | undefined {
+    return SectionCrumb.SECTIONS.get(this.text)
+  }
+}
+
+export class TextCrumb implements Crumb {
+  readonly text: string
+
+  constructor(text: string) {
+    this.text = text
+  }
+
+  get link(): null {
+    return null
+  }
+}
+
+function CrumbComponent({ crumb }: { crumb: Crumb }): JSX.Element {
+  const sectionLink = crumb.link
+  const sectionItem = <Breadcrumb.Item>{crumb.text}</Breadcrumb.Item>
   return sectionLink ? (
     <LinkContainer to={sectionLink}>{sectionItem}</LinkContainer>
   ) : (
@@ -25,13 +54,15 @@ export default function Breadcrumbs({
   crumbs
 }: {
   crumbs: ReadonlyArray<string>
-}) {
-  const initial = ['eBL', ..._.initial(crumbs)]
+}): JSX.Element {
+  const initial = ['eBL', ..._.initial(crumbs)].map(
+    section => new SectionCrumb(section)
+  )
   const last = _.last(crumbs)
   return (
     <Breadcrumb>
-      {initial.map((section, index) => (
-        <SectionCrumb key={index} section={section} />
+      {initial.map((crumb, index) => (
+        <CrumbComponent key={index} crumb={crumb} />
       ))}
       {last && <Breadcrumb.Item active>{last}</Breadcrumb.Item>}
     </Breadcrumb>
