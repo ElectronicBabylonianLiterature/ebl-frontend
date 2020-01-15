f91f122a269423b9dd6c44d0f06b4d718804fcc7
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-01-15 15:39:32 +0100

    Add types to test utils

--- src/test-helpers/utils.ts
+++ src/test-helpers/utils.ts
@@ -1,71 +1,124 @@
-import { fireEvent, wait, act } from '@testing-library/react'
-import * as bluebird from 'bluebird'
+import {
+  fireEvent,
+  wait,
+  act,
+  RenderResult,
+  Matcher
+} from '@testing-library/react'
+import Bluebird from 'bluebird'
 import _ from 'lodash'
 
-function when(createMatcher) {
+interface ExpectResult<T> {
+  toHaveBeenCalledWith(args: any): T
+}
+interface WhenResult<T> {
+  expect(func: Function): ExpectResult<T>
+}
+
+type MatcherFactory<T> = (func: Function) => (args: any) => T
+
+function when<T>(createMatcher: MatcherFactory<T>): WhenResult<T> {
   return {
-    expect: onChange => ({
+    expect: (onChange: Function): ExpectResult<T> => ({
       toHaveBeenCalledWith: createMatcher(onChange)
     })
   }
 }
 
-export function changeValue(input, newValue) {
+export function changeValue<T>(input: Element, newValue: T): void {
   act(() => {
     fireEvent.change(input, { target: { value: newValue } })
   })
 }
 
-export function clickNth(element, text, n = 0) {
+export function clickNth(element: RenderResult, text: Matcher, n = 0): void {
   const clickable = element.getAllByText(text)[n]
   act(() => {
     fireEvent.click(clickable)
   })
 }
 
-export function changeValueByValue(element, value, newValue, n = 0) {
+type Changer<T> = (
+  element: RenderResult,
+  selector: Matcher,
+  newValue: T,
+  n?: number
+) => void
+
+export function changeValueByValue<T>(
+  element: RenderResult,
+  value: Matcher,
+  newValue: T,
+  n = 0
+): void {
   changeValue(element.getAllByDisplayValue(value)[n], newValue)
 }
 
-export function changeValueByLabel(element, label, newValue, n = 0) {
+export function changeValueByLabel<T>(
+  element: RenderResult,
+  label: Matcher,
+  newValue: T,
+  n = 0
+): void {
   changeValue(element.getAllByLabelText(label)[n], newValue)
 }
 
-export function whenClicked(element, text, n = 0) {
-  return when(onChange => async (...expectedChange) => {
+export function whenClicked(
+  element: RenderResult,
+  text: Matcher,
+  n = 0
+): WhenResult<Promise<void>> {
+  return when(onChange => async (...expectedChange): Promise<void> => {
     clickNth(element, text, n)
     await wait(() => expect(onChange).toHaveBeenCalledWith(...expectedChange))
   })
 }
 
-function whenChangedBy(element, selector, newValue, changer) {
-  return when(onChange => expectedChangeFactory => {
+function whenChangedBy<T>(
+  element: RenderResult,
+  selector: Matcher,
+  newValue: T,
+  changer: Changer<T>
+): WhenResult<void> {
+  return when(onChange => (expectedChangeFactory): void => {
     changer(element, selector, newValue)
     expect(onChange).toHaveBeenCalledWith(expectedChangeFactory(newValue))
   })
 }
 
-export function whenChangedByValue(element, value, newValue) {
+export function whenChangedByValue<T>(
+  element: RenderResult,
+  value: Matcher,
+  newValue: T
+): WhenResult<void> {
   return whenChangedBy(element, value, newValue, changeValueByValue)
 }
 
-export function whenChangedByLabel(element, label, newValue) {
+export function whenChangedByLabel<T>(
+  element: RenderResult,
+  label: Matcher,
+  newValue: T
+): WhenResult<void> {
   return whenChangedBy(element, label, newValue, changeValueByLabel)
 }
 
-export function submitForm(element, query) {
+export function submitForm(element: RenderResult, query: string): void {
   act(() => {
-    fireEvent.submit(element.container.querySelector(query))
+    const result = element.container.querySelector(query)
+    result && fireEvent.submit(result)
   })
 }
 
-export function submitFormByTestId(element, testId) {
+export function submitFormByTestId(
+  element: RenderResult,
+  testId: Matcher
+): void {
   act(() => {
     fireEvent.submit(element.getByTestId(testId))
   })
 }
 
-export type TestData = [string, any[], jest.Mock, any, any?, any?]
+export type TestData = [string, any[], jest.Mock, any, (any[] | null)?, any?]
 export function testDelegation(
   object: any,
   testData: readonly TestData[]
@@ -73,7 +126,7 @@ export function testDelegation(
   describe.each(testData)(
     '%s',
     (method, params, target, expectedResult, expectedParams, targetResult) => {
-      let result
+      let result: any
 
       beforeEach(() => {
         jest.clearAllMocks()
@@ -86,7 +139,7 @@ export function testDelegation(
       })
 
       it(`Returns`, async () => {
-        if (result instanceof bluebird.Promise || result instanceof Promise) {
+        if (result instanceof Bluebird || result instanceof Promise) {
           await expect(result).resolves.toEqual(expectedResult)
         } else {
           expect(result).toEqual(expectedResult)
