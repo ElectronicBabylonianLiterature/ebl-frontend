4405f759c4766459dff2a2f09d04a8a9ae745525
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-01-27 12:33:40 +0100

    Extract Glossary

--- src/fragmentarium/ui/display/Display.test.tsx
+++ src/fragmentarium/ui/display/Display.test.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import { factory } from 'factory-girl'
+import { Fragment } from 'fragmentarium/domain/fragment'
 
 import Display from './Display'
 
-let fragment
-let element
-let container
+let fragment: Fragment
+let element: RenderResult
+let container: HTMLElement
 
 beforeEach(async () => {
-  fragment = await factory.build('fragment', { atf: '1. ku' })
+  fragment = await factory.build('fragment', {
+    atf: '1. ku',
+    publication: 'Guod cigipli epibif odepuwu.',
+    description:
+      'Balbodduh lifuseb wuuk nasu hulwajo ho hiskuk riwa eldat ivu jandara nosrina abike befukiz ravsus.\nZut uzzejum ub mil ika roppar zewize ipifac vut eci avimez cewmikjov kiwso zamli jecja now.'
+  })
   element = render(<Display fragment={fragment} />)
   container = element.container
 })
 
-it(`Renders header`, () => {
+test(`Renders header`, () => {
   expect(container).toHaveTextContent(fragment.publication)
 })
 
-it('Renders transliteration', () => {
+test('Renders transliteration', () => {
   expect(container).toHaveTextContent(fragment.atf)
+})
+
+test('Snapshot', () => {
+  expect(container).toMatchSnapshot()
 })
--- src/fragmentarium/ui/display/Display.tsx
+++ src/fragmentarium/ui/display/Display.tsx
@@ -1,15 +1,19 @@
-import React, { Fragment } from 'react'
-import _ from 'lodash'
-import { Badge } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from 'react'
 import TransliterationHeader from 'fragmentarium/ui/view/TransliterationHeader'
 import SessionContext from 'auth/SessionContext'
+import Session from 'auth/Session'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import { Glossary } from './Glossary'
 
 import './Display.css'
 
-function Display({ fragment }) {
+interface Props {
+  fragment: Fragment
+}
+
+function Display({ fragment }: Props): JSX.Element {
   return (
-    <Fragment>
+    <>
       <TransliterationHeader fragment={fragment} />
       <ol className="Display__lines">
         {fragment.atf.split('\n').map((line, index) => (
@@ -17,60 +21,11 @@ function Display({ fragment }) {
         ))}
       </ol>
       <SessionContext.Consumer>
-        {session =>
-          session.hasBetaAccess() && (
-            <section>
-              <h4>
-                Glossary{' '}
-                <small>
-                  <Badge variant="warning">Beta</Badge>
-                </small>
-              </h4>
-              {_(fragment.text.lines)
-                .flatMap(line =>
-                  line.content
-                    .filter(token => token.lemmatizable)
-                    .map(token => ({
-                      number: line.prefix,
-                      value: token.value,
-                      uniqueLemma: token.uniqueLemma
-                    }))
-                )
-                .reject(token => _.isEmpty(token.uniqueLemma))
-                .groupBy(token => token.uniqueLemma)
-                .toPairs()
-                .map(([lemma, tokensByLemma]) => [
-                  tokensByLemma[0].uniqueLemma,
-                  tokensByLemma
-                ])
-                .sortBy(([lemma, tokensByLemma]) => lemma[0])
-                .map(([lemma, tokensByLemma]) => (
-                  <div key={lemma.join(' ')}>
-                    {lemma.map((l, index) => (
-                      <span key={index}>
-                        {' '}
-                        <Link to={`/dictionary/${l}`}>{l}</Link>
-                      </span>
-                    ))}
-                    {': '}
-                    {_(tokensByLemma)
-                      .groupBy(token => token.value)
-                      .map(
-                        (tokensByValue, value) =>
-                          value +
-                          ' (' +
-                          tokensByValue.map(token => token.number).join(', ') +
-                          ')'
-                      )
-                      .join(', ')}
-                  </div>
-                ))
-                .value()}
-            </section>
-          )
+        {(session: Session): React.ReactNode =>
+          session.hasBetaAccess() && <Glossary fragment={fragment} />
         }
       </SessionContext.Consumer>
-    </Fragment>
+    </>
   )
 }
 
