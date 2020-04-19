fb5090b9827ee90926edd2c97374972baaf8a6a3
Yunus Cobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-04-19 18:12:45 +0200

    split Transliteration, extract LineAccumulator

--- src/fragmentarium/ui/display/Transliteration.tsx
+++ src/fragmentarium/ui/display/Transliteration.tsx
@@ -1,9 +1,4 @@
-import React, {
-  FunctionComponent,
-  PropsWithChildren,
-  ReactElement,
-  ReactNode
-} from 'react'
+import React, { FunctionComponent } from 'react'
 import classNames from 'classnames'
 import {
   Line,
@@ -14,109 +9,8 @@ import {
   LineNumber,
   LineNumberRange
 } from 'fragmentarium/domain/text'
-import DisplayToken, { TokenWrapper } from './DisplayToken'
-import { isEnclosure, isShift, isDocumentOrientedGloss } from './type-guards'
-
+import DisplayLineTokens from './LineAccumulator'
 import './Display.sass'
-import { Shift, Token } from 'fragmentarium/domain/token'
-
-function WordSeparator({
-  modifiers: bemModifiers = []
-}: {
-  modifiers?: readonly string[]
-}): JSX.Element {
-  const element = 'Transliteration__wordSeparator'
-  return (
-    <span
-      className={classNames([
-        element,
-        bemModifiers.map(flag => `${element}--${flag}`)
-      ])}
-    >
-      {' '}
-    </span>
-  )
-}
-
-function isCloseEnclosure(token: Token): boolean {
-  return isEnclosure(token) && ['CENTER', 'RIGHT'].includes(token.side)
-}
-
-function isOpenEnclosure(token: Token): boolean {
-  return isEnclosure(token) && ['CENTER', 'LEFT'].includes(token.side)
-}
-
-class LineAccumulator {
-  result: React.ReactNode[] = []
-  private inGloss = false
-  private language = 'AKKADIAN'
-  private enclosureOpened = false
-
-  applyLanguage(token: Shift): void {
-    this.language = token.language
-  }
-
-  pushToken(token: Token): void {
-    if (this.requireSeparator(token)) {
-      this.pushSeparator(this.result)
-    }
-
-    const glossWrapper: TokenWrapper = ({
-      children
-    }: PropsWithChildren<{}>): JSX.Element => (
-      <sup className="Transliteration__DocumentOrientedGloss">{children}</sup>
-    )
-
-    this.result.push(
-      this.inGloss && !isEnclosure(token) ? (
-        <DisplayToken
-          key={this.result.length}
-          token={token}
-          bemModifiers={[this.language]}
-          Wrapper={glossWrapper}
-        />
-      ) : (
-        <DisplayToken
-          key={this.result.length}
-          token={token}
-          bemModifiers={[this.language]}
-        />
-      )
-    )
-    this.enclosureOpened = isOpenEnclosure(token)
-  }
-
-  openGloss(): void {
-    this.inGloss = true
-  }
-
-  closeGloss(): void {
-    this.inGloss = false
-  }
-
-  private requireSeparator(token: Token): boolean {
-    const noEnclosure = !isCloseEnclosure(token) && !this.enclosureOpened
-    return this.result.length === 0 || noEnclosure
-  }
-
-  private pushSeparator(target: React.ReactNode[]): void {
-    target.push(
-      this.inGloss ? (
-        <sup
-          key={`${target.length}-separator`}
-          className="Transliteration__DocumentOrientedGloss"
-        >
-          <WordSeparator modifiers={[this.language]} />
-        </sup>
-      ) : (
-        <WordSeparator
-          key={`${target.length}-separator`}
-          modifiers={[this.language]}
-        />
-      )
-    )
-  }
-}
 
 function DisplayLineNumber({
   lineNumber
@@ -185,24 +79,25 @@ function DisplayLine({
   )
 }
 
-function DisplayLineTokens({
-  content
+const rulingsToNumber: ReadonlyMap<string, number> = new Map([
+  ['SINGLE', 1],
+  ['DOUBLE', 2],
+  ['TRIPLE', 3]
+])
+
+function DisplayDollarAndAtLine({
+  line,
+  container = 'div'
 }: {
-  content: ReadonlyArray<Token>
-}): any {
-  const tokens = content.reduce((acc: LineAccumulator, token: Token) => {
-    if (isShift(token)) {
-      acc.applyLanguage(token)
-    } else if (isDocumentOrientedGloss(token)) {
-      token.side === 'LEFT' ? acc.openGloss() : acc.closeGloss()
-    } else {
-      acc.pushToken(token)
-    }
-    return acc
-  }, new LineAccumulator()).result
-  return tokens.map((token: ReactNode) => {
-    return token as JSX.Element
-  })
+  line: Line
+  container?: string
+}): JSX.Element {
+  const dollarAndAtLine = line as DollarAndAtLine
+  return React.createElement(
+    container,
+    { className: 'Transliteration__DollarAndAtLine' },
+    `(${dollarAndAtLine.displayValue})`
+  )
 }
 
 function DisplayDollarAndAtLineWithParenthesis({
@@ -217,21 +112,6 @@ function DisplayDollarAndAtLineWithParenthesis({
     container,
     { className: 'Transliteration__DollarAndAtLineWithParenthesis' },
     dollarAndAtLine.displayValue
-  )
-}
-
-function DisplayDollarAndAtLine({
-  line,
-  container = 'div'
-}: {
-  line: Line
-  container?: string
-}): JSX.Element {
-  const dollarAndAtLine = line as DollarAndAtLine
-  return React.createElement(
-    container,
-    { className: 'Transliteration__DollarAndAtLine' },
-    `(${dollarAndAtLine.displayValue})`
   )
 }
 
@@ -259,12 +139,6 @@ const lineComponents: ReadonlyMap<
   ['CompositeAtLine', DisplayDollarAndAtLine]
 ])
 
-const rulingsToNumber: ReadonlyMap<string, number> = new Map([
-  ['SINGLE', 1],
-  ['DOUBLE', 2],
-  ['TRIPLE', 3]
-])
-
 function DisplayRulingDollarLine({
   line,
   container = 'div'
@@ -283,6 +157,7 @@ function DisplayRulingDollarLine({
     })
   )
 }
+
 function DisplayEachRuling({
   rulingType
 }: {
