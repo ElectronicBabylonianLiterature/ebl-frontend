48c6283cd8b8af62cd53a09c969cc9ec8d18326e
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-04-22 09:19:13 +0200

    Extract methods

--- src/auth/react-auth0-spa.tsx
+++ src/auth/react-auth0-spa.tsx
@@ -11,11 +11,40 @@ import Auth0AuthenticationService from 'auth/Auth0AuthenticationService'
 
 async function createSession(auth0Client: Auth0Client): Promise<Session> {
   const accessToken = await auth0Client.getTokenSilently()
-  return new MemorySession(decode(accessToken).scope.split(' '))
+  return new MemorySession(
+    decode<{ scope: string }>(accessToken).scope.split(' ')
+  )
 }
 
 const DEFAULT_REDIRECT_CALLBACK = (state: unknown): void =>
   window.history.replaceState({}, document.title, window.location.pathname)
+
+function isRedirect(): boolean {
+  return (
+    window.location.search.includes('code=') &&
+    window.location.search.includes('state=')
+  )
+}
+
+async function createAuthenticationService(
+  auth0Client: Auth0Client,
+  returnTo: string
+): Promise<AuthenticationService> {
+  const isAuthenticated = await auth0Client.isAuthenticated()
+  if (isAuthenticated) {
+    const user = await auth0Client.getUser()
+    const session = await createSession(auth0Client)
+    return new Auth0AuthenticationService(
+      auth0Client,
+      returnTo,
+      true,
+      user,
+      session
+    )
+  } else {
+    return new Auth0AuthenticationService(auth0Client, returnTo)
+  }
+}
 
 export const Auth0Provider = ({
   children,
@@ -31,33 +60,16 @@ export const Auth0Provider = ({
     const initAuth0 = async (): Promise<void> => {
       const auth0Client = await createAuth0Client(initOptions)
 
-      if (
-        window.location.search.includes('code=') &&
-        window.location.search.includes('state=')
-      ) {
+      if (isRedirect()) {
         const { appState } = await auth0Client.handleRedirectCallback()
         onRedirectCallback(appState)
       }
 
-      const isAuthenticated = await auth0Client.isAuthenticated()
-
-      if (isAuthenticated) {
-        const user = await auth0Client.getUser()
-        const session = await createSession(auth0Client)
-        setAuthenticationService(
-          new Auth0AuthenticationService(
-            auth0Client,
-            returnTo,
-            true,
-            user,
-            session
-          )
-        )
-      } else {
-        setAuthenticationService(
-          new Auth0AuthenticationService(auth0Client, initOptions.returnTo)
-        )
-      }
+      const authenticationService = await createAuthenticationService(
+        auth0Client,
+        returnTo
+      )
+      setAuthenticationService(authenticationService)
     }
     initAuth0()
     // eslint-disable-next-line
