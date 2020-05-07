c63df870a914fb582038a57c0efbc0f75ad6c767
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-05-07 16:34:11 +0200

    Make Bibliograhy test more robust

---
+++ src/test-helpers/createAuthorRexExp.ts
@@ -0,0 +1,6 @@
+import _ from 'lodash'
+import BibliographyEntry from 'bibliography/domain/BibliographyEntry'
+
+export default function createAuthorRegExp(entry: BibliographyEntry): RegExp {
+  return new RegExp(_.escapeRegExp(entry.primaryAuthor.replace("'", '’')))
+}
