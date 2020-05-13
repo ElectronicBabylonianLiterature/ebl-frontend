4f16be31caae977e85c5816a68db2668b6bf5f57
Jussi Laasonen <jussi.laasonen@iki.fi>
2020-05-13 12:12:00 +0000

    Add test case for notes on the first line

--- src/transliteration/ui/TransliterationLines.test.tsx
+++ src/transliteration/ui/TransliterationLines.test.tsx
@@ -6,61 +6,83 @@ import { singleRuling } from 'test-helpers/lines/dollar'
 import TransliterationLines from './TransliterationLines'
 import { Text } from 'transliteration/domain/text'
 
-let element: RenderResult
-let lines: HTMLElement[]
-
-beforeEach(() => {
-  const text = new Text({
-    lines: [
-      singleRuling,
-      noteLine,
-      noteLine,
-      singleRuling,
-      singleRuling,
-      noteLine,
-    ],
-  })
-  element = render(<TransliterationLines text={text} />)
-  lines = element.getAllByRole('listitem')
+const textWithNormalFirstLine = new Text({
+  lines: [
+    singleRuling,
+    noteLine,
+    noteLine,
+    singleRuling,
+    singleRuling,
+    noteLine,
+  ],
 })
-
-test('Snapshot', () => {
-  expect(element.container).toMatchSnapshot()
-})
-
-test('Shows all lines', () => {
-  expect(lines.length).toEqual(3)
-})
-
-describe.each([
+const normaFirsLineCases: [number, number[]][] = [
   [1, [1, 2]],
   [2, []],
   [3, [3]],
-])('Line %s with notes %p', (lineNumber, noteNumbers) => {
-  let line: HTMLElement
-  let links: HTMLElement[]
+]
 
+const textWithNoteOnFirstLine = new Text({
+  lines: [noteLine, noteLine, singleRuling],
+})
+const noteFirstLineCases: [number, number[]][] = [
+  [0, [1, 2]],
+  [1, []],
+]
+
+let element: RenderResult
+let lines: HTMLElement[]
+
+describe.each([
+  [textWithNormalFirstLine, false, normaFirsLineCases],
+  [textWithNoteOnFirstLine, true, noteFirstLineCases],
+])('%#', (text, noteOnFirstLine, lineCases: [number, number[]][]) => {
   beforeEach(() => {
-    line = lines[lineNumber - 1]
-    links = within(line).queryAllByRole('link')
+    element = render(<TransliterationLines text={text} />)
+    lines = element.getAllByRole('listitem')
   })
 
-  test('ID', () => {
-    expect(line).toHaveAttribute('id', `line-${lineNumber}`)
+  test('Snapshot', () => {
+    expect(element.container).toMatchSnapshot()
   })
 
-  test('Shows all links', () => {
-    expect(links.length).toEqual(noteNumbers.length)
+  test('Shows all lines', () => {
+    expect(lines.length).toEqual(lineCases.length)
   })
 
-  if (!_.isEmpty(noteNumbers)) {
-    test.each(_.range(1, noteNumbers.length + 1))('%s. link', (linkNumber) => {
-      const index = linkNumber - 1
-      const noteNumber = noteNumbers[index]
-      const link = links[index]
+  describe.each(lineCases)(
+    'Line %s with notes %p',
+    (lineNumber, noteNumbers) => {
+      let line: HTMLElement
+      let links: HTMLElement[]
 
-      expect(link).toHaveAttribute('href', `#note-${noteNumber}`)
-      expect(link).toHaveTextContent(String(noteNumber))
-    })
-  }
+      beforeEach(() => {
+        const index = noteOnFirstLine ? lineNumber : lineNumber - 1
+        line = lines[index]
+        links = within(line).queryAllByRole('link')
+      })
+
+      test('ID', () => {
+        expect(line).toHaveAttribute('id', `line-${lineNumber}`)
+      })
+
+      test('Shows all links', () => {
+        expect(links.length).toEqual(noteNumbers.length)
+      })
+
+      if (!_.isEmpty(noteNumbers)) {
+        test.each(_.range(1, noteNumbers.length + 1))(
+          '%s. link',
+          (linkNumber) => {
+            const index = linkNumber - 1
+            const noteNumber = noteNumbers[index]
+            const link = links[index]
+
+            expect(link).toHaveAttribute('href', `#note-${noteNumber}`)
+            expect(link).toHaveTextContent(String(noteNumber))
+          }
+        )
+      }
+    }
+  )
 })
