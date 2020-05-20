f5e22d089d66f9a9d96c1b8dd7a8c0ad76bbbfa1
Jussi Laasonen <jussi.laasonen@iki.fi>
2020-05-20 09:21:38 +0000

    Move lineNumberToString to domain

--- src/transliteration/ui/LinePrefix.tsx
+++ src/transliteration/ui/LinePrefix.tsx
@@ -1,7 +1,7 @@
 import { Line } from 'transliteration/domain/line'
 import React from 'react'
-import { isTextLine } from '../domain/type-guards'
-import lineNumberToString from './lineNumberToString'
+import { isTextLine } from 'transliteration/domain/type-guards'
+import lineNumberToString from 'transliteration/domain/lineNumberToString'
 
 export function LinePrefix({ line }: { line: Line }): JSX.Element {
   return isTextLine(line) ? (
