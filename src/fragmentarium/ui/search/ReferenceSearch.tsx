53f9c8cd004ce9c816e5440293a2fca5456eb573
Yunus Cobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-06-08 11:31:32 +0200

    display references

--- src/fragmentarium/ui/search/ReferenceSearch.tsx
+++ src/fragmentarium/ui/search/ReferenceSearch.tsx
@@ -5,6 +5,43 @@ import FragmentList from 'fragmentarium/ui/FragmentList'
 import withData from 'http/withData'
 import { FragmentInfo } from 'fragmentarium/domain/fragment'
 
+function References({
+  references,
+  referencesKey,
+}: {
+  references: ReadonlyArray<any>
+  referencesKey: string
+}) {
+  return (
+    <ul className="list-unstyled">
+      {references.map((reference, index) => (
+        <ul key={index}>{reference[referencesKey]}</ul>
+      ))}
+    </ul>
+  )
+}
+
+function ReferenceSearchResult({ data }) {
+  function makeReferences(fragment, key) {
+    return (
+      <References
+        references={fragment.references}
+        referencesKey={key}
+      ></References>
+    )
+  }
+  return (
+    <FragmentList
+      fragments={data}
+      columns={{
+        'References.Id': (data) => makeReferences(data, 'id'),
+        'References.Pages': (data) => makeReferences(data, 'pages'),
+        Description: 'description',
+      }}
+    />
+  )
+}
+
 export default withData<
   {
     id: string | null | undefined
@@ -14,16 +51,7 @@ export default withData<
   readonly FragmentInfo[]
 >(
   ({ id, pages, data }) =>
-    id || pages ? (
-      <FragmentList
-        fragments={data}
-        columns={{
-          Accession: 'Accession',
-          'CDLI Number': 'cdliNumber',
-          Description: 'description',
-        }}
-      />
-    ) : null,
+    id || pages ? <ReferenceSearchResult data={data} /> : null,
   (props) => props.fragmentSearchService.searchReference(props.id, props.pages),
   {
     watch: (props) => [props.id, props.pages],
