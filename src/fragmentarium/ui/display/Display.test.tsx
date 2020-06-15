4254e2750f6c9adb9884f54a4cc0606287c3f311
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-06-15 12:22:16 +0200

    Show Glossary to all users

--- src/fragmentarium/ui/display/Display.test.tsx
+++ src/fragmentarium/ui/display/Display.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { act, render, RenderResult } from '@testing-library/react'
 import { factory } from 'factory-girl'
 import { Fragment } from 'fragmentarium/domain/fragment'
 import complexText from 'test-helpers/complexTestText'
 import WordService from 'dictionary/application/WordService'
 import Display from './Display'
+import { wordDto } from 'test-helpers/test-word'
+import { MemoryRouter } from 'react-router-dom'
 
 jest.mock('dictionary/application/WordService')
 
@@ -15,13 +17,22 @@ let container: HTMLElement
 
 beforeEach(async () => {
   wordService = new WordService(null)
+  jest
+    .spyOn(wordService, 'find')
+    .mockImplementation(() => Promise.resolve(wordDto))
   fragment = await factory.build('fragment', {
     publication: 'Guod cigipli epibif odepuwu.',
     text: complexText,
     description:
       'Balbodduh lifuseb wuuk nasu hulwajo ho hiskuk riwa eldat ivu jandara nosrina abike befukiz ravsus.\nZut uzzejum ub mil ika roppar zewize ipifac vut eci avimez cewmikjov kiwso zamli jecja now.',
   })
-  element = render(<Display fragment={fragment} wordService={wordService} />)
+  await act(async () => {
+    element = render(
+      <MemoryRouter>
+        <Display fragment={fragment} wordService={wordService} />
+      </MemoryRouter>
+    )
+  })
   container = element.container
 })
 
