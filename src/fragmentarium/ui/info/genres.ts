2d3ca430a42e72c9e57bf2b6d9a246250a5a5c26
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-08-14 18:21:56 +0200

    adding more genres

--- src/fragmentarium/ui/info/genres.ts
+++ src/fragmentarium/ui/info/genres.ts
@@ -18,6 +18,66 @@ export const genres: ReadonlyArray<Tree> = [
             value: 'Inventories',
             nodes: [{ value: 'Balanced Accounts' }, { value: 'Basket Labels' }],
           },
+          {
+            value: 'Receipts',
+            nodes: [{ value: 'Income' }],
+          },
+          {
+            value: 'Expenditure',
+            nodes: [{ value: 'Messenger Text' }],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'ARCHIVAL',
+    nodes: [
+      {
+        value: 'Administrative',
+        nodes: [
+          {
+            value: 'Lists',
+            nodes: [{ value: 'One Entry' }, { value: 'Multientry' }],
+          },
+          { value: 'Memos' },
+          {
+            value: 'Inventories',
+            nodes: [{ value: 'Balanced Accounts' }, { value: 'Basket Labels' }],
+          },
+          {
+            value: 'Receipts',
+            nodes: [{ value: 'Income' }],
+          },
+          {
+            value: 'Expenditure',
+            nodes: [{ value: 'Messenger Text' }],
+          },
+        ],
+      },
+      {
+        value: 'Legal',
+        nodes: [
+          {
+            value: 'Debt Note, Loan',
+          },
+          { value: 'Decree' },
+          {
+            value: 'Judicial',
+            nodes: [{ value: 'Court Case' }],
+          },
+          {
+            value: 'Sale',
+          },
+          {
+            value: 'Transfer of Properties, Other',
+            nodes: [
+              { value: 'Inheritance' },
+              { value: 'Adoption' },
+              { value: 'Dowry' },
+              { value: 'Exchange' },
+            ],
+          },
         ],
       },
     ],
