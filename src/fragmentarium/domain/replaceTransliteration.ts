277eb5456518f1e241a7fdc35dbadf65f14accfc
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-09-02 12:03:59 +0000

    Remove alternative damage brackets from transliteration search

--- src/fragmentarium/domain/replaceTransliteration.ts
+++ src/fragmentarium/domain/replaceTransliteration.ts
@@ -3,10 +3,17 @@ import normalizeNumbers from './normalizeNumbers'
 import normalizeAccents from './normalizeAccents'
 import _ from 'lodash'
 
-export default function replaceTransliteration(transliteration) {
+function replaceAlternativeDamage(input: string): string {
+  return input.replace(/[⸢⸣]/g, '')
+}
+
+export default function replaceTransliteration(
+  transliteration: string
+): string {
   return _(transliteration)
     .thru(replaceSpecialCharacters)
     .thru(normalizeNumbers)
     .thru(normalizeAccents)
+    .thru(replaceAlternativeDamage)
     .value()
 }
