e806b412577e97cc9b5dd9979f67c4391ce4ea2c
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-09-16 13:32:39 +0300

    add generics

--- src/common/usePrevious.ts
+++ src/common/usePrevious.ts
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react'
 
-export function usePrevious(value: any) {
-  const ref = useRef()
+export function usePrevious<T>(value: T) {
+  const ref = useRef<T>()
   useEffect(() => {
     ref.current = value
   })
-  return ref.current
+  return ref.current as T
 }
