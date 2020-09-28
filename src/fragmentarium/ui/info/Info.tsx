8a9252966c26d6886c8eaaead3ff0f6d1348a1c5
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-09-28 15:18:19 +0200

    init

--- src/fragmentarium/ui/info/Info.tsx
+++ src/fragmentarium/ui/info/Info.tsx
@@ -6,6 +6,9 @@ import OrganizationLinks from 'fragmentarium/ui/info/OrganizationLinks'
 import UncuratedReferences from 'fragmentarium/ui/info/UncuratedReferences'
 import { Fragment, UncuratedReference } from 'fragmentarium/domain/fragment'
 import FragmentService from 'fragmentarium/application/FragmentService'
+import HelpTrigger from 'common/HelpTrigger'
+import _ from 'lodash'
+import { Popover, Row } from 'react-bootstrap'
 interface Props {
   fragment: Fragment
   fragmentService: FragmentService
@@ -18,6 +21,17 @@ export default function Info({
 }: Props): JSX.Element {
   const updateGenres = (genres) =>
     onSave(fragmentService.updateGenres(fragment.number, genres))
+  const ReferencesHelp = () => (
+    <Popover id={_.uniqueId('ReferenceshHelp-')} title="References">
+      <Popover.Content>
+        <code>C</code> = Copy <br />
+        <code>P</code> = Photograph <br />
+        <code>E</code> = Edition <br />
+        <code>D</code> = Discussion <br />
+      </Popover.Content>
+    </Popover>
+  )
+
   return (
     <>
       <Details
@@ -26,7 +40,20 @@ export default function Info({
         fragmentService={fragmentService}
       />
       <section>
-        <h3>References</h3>
+        <Row>
+          <h3>References</h3>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginBottom: '.5rem',
+              marginLeft: '.5rem',
+            }}
+          >
+            <HelpTrigger overlay={ReferencesHelp()} />
+          </div>
+        </Row>
         <ReferenceList references={fragment.references} />
         {fragment.hasUncuratedReferences && (
           <UncuratedReferences
