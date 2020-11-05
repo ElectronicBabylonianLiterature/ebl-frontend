2307a529b3330aff9c945ca615106f7cecd60577
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-11-05 09:42:12 +0000

    Remove unused import

--- src/fragmentarium/ui/lemmatization/Lemmatizer.test.tsx
+++ src/fragmentarium/ui/lemmatization/Lemmatizer.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { Promise } from 'bluebird'
 import { factory } from 'factory-girl'
 
