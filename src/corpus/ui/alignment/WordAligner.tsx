bf89405b9aa8cc386f97f3e5116144bbf1d91bd8
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-12-02 12:57:48 +0000

    Move DropdownButton to own file

--- src/corpus/ui/alignment/WordAligner.tsx
+++ src/corpus/ui/alignment/WordAligner.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Dropdown, Button } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 import _ from 'lodash'
 import Word from './Word'
 
@@ -8,75 +8,7 @@ import produce, { Draft } from 'immer'
 
 import { Token } from 'transliteration/domain/token'
 import { AlignmentToken } from 'corpus/domain/alignment'
-
-function divMenu(content: React.ReactNode) {
-  return React.forwardRef<HTMLDivElement, unknown>(function menu(
-    {
-      style,
-      className,
-      'aria-labelledby': labeledBy,
-    }: {
-      children?: React.ReactNode
-      style?: React.CSSProperties
-      className?: string
-      'aria-labelledby'?: string
-    },
-    ref
-  ) {
-    return (
-      <div
-        ref={ref}
-        style={style}
-        className={className}
-        aria-labelledby={labeledBy}
-      >
-        {content}
-      </div>
-    )
-  })
-}
-
-function buttonToggle(content: React.ReactNode, active: boolean) {
-  return React.forwardRef<HTMLButtonElement & Button, unknown>(function toggle(
-    props,
-    ref
-  ) {
-    return (
-      <Button
-        ref={ref}
-        size="sm"
-        variant="outline-dark"
-        active={active}
-        {...props}
-      >
-        {content}
-      </Button>
-    )
-  })
-}
-
-function DropdownButton({
-  menu,
-  toggle,
-  onToggle,
-  show,
-}: {
-  menu: React.ReactNode
-  toggle: React.ReactNode
-  onToggle: (show: boolean) => void
-  show: boolean
-}): JSX.Element {
-  return (
-    <Dropdown as="span" onToggle={onToggle} show={show}>
-      <Dropdown.Toggle
-        as={buttonToggle(toggle, show)}
-        id={_.uniqueId('DropdownPopOver-')}
-        bsPrefix="DropdownButton__toggle"
-      />
-      <Dropdown.Menu as={divMenu(menu)} />
-    </Dropdown>
-  )
-}
+import DropdownButton from './DropdownButton'
 
 interface FormProps {
   readonly token: AlignmentToken
