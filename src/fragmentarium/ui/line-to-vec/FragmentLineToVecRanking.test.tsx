9104431638497475811aa6bb87e9524d95bd401f
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2020-12-03 17:33:04 +0100

    finished FragmentLineToVecRankingTest

--- src/fragmentarium/ui/line-to-vec/FragmentLineToVecRanking.test.tsx
+++ src/fragmentarium/ui/line-to-vec/FragmentLineToVecRanking.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FragmentLineToVecRanking from './FragmentLineToVecRanking'
+import { MemoryRouter } from 'react-router-dom'
 import {
   render,
   screen,
@@ -7,8 +8,13 @@ import {
 } from '@testing-library/react'
 import Promise from 'bluebird'
 import { LineToVecRanking } from 'fragmentarium/domain/lineToVecRanking'
+import SessionContext from 'auth/SessionContext'
+import { Session } from 'auth/Session'
 
 beforeEach(async () => {
+  const session = {
+    isAllowedToReadFragments: jest.fn().mockReturnValue(true),
+  }
   const lineToVecRankingsResults: LineToVecRanking = {
     score: [
       ['X.1', 10],
@@ -26,14 +32,18 @@ beforeEach(async () => {
     Promise.resolve(lineToVecRankingsResults)
   )
   render(
-    <FragmentLineToVecRanking
-      number={'X.0'}
-      fragmentService={fragmentService}
-    />
+    <MemoryRouter>
+      <SessionContext.Provider value={(session as unknown) as Session}>
+        <FragmentLineToVecRanking
+          number={'X.0'}
+          fragmentService={fragmentService}
+        />
+      </SessionContext.Provider>
+    </MemoryRouter>
   )
   await waitForElementToBeRemoved(() => screen.getByLabelText('Spinner'))
 })
 
 it('Shows the number of transliterated tablets', async () => {
-  expect(screen.getByText('X.1')).toBeVisible()
+  expect(screen.getByText('X.1:10')).toBeVisible()
 })
