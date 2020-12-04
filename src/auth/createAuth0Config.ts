3c9dcf4333a6b107deea0c224828b2ff3a8bfe54
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-12-04 08:49:54 +0000

    Add return type to createAuth0Config

--- src/auth/createAuth0Config.ts
+++ src/auth/createAuth0Config.ts
@@ -1,9 +1,17 @@
-export default function createAuth0Config() {
+interface Auth0Config {
+  readonly domain: string
+  readonly clientID: string
+  readonly redirectUri: string
+  readonly returnTo: string
+  readonly audience: string
+}
+
+export default function createAuth0Config(): Auth0Config {
   return {
-    domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-    redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
-    returnTo: process.env.REACT_APP_AUTH0_RETURN_TO,
-    audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN ?? '',
+    clientID: process.env.REACT_APP_AUTH0_CLIENT_ID ?? '',
+    redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URI ?? '',
+    returnTo: process.env.REACT_APP_AUTH0_RETURN_TO ?? '',
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE ?? '',
   }
 }
