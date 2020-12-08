a9ca0f31daaf5cad679acb5cd8b67875b3077bd5
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2020-12-08 11:46:39 +0000

    Move OmittedWordsSelect to new file

--- src/corpus/ui/alignment/ChapterAligner.tsx
+++ src/corpus/ui/alignment/ChapterAligner.tsx
@@ -1,23 +1,15 @@
-import React, { useState } from 'react'
-import _ from 'lodash'
-import { Chapter, Line, ManuscriptLine } from 'corpus/domain/text'
-import { Badge, Button, Col, Container, Row } from 'react-bootstrap'
-import Select, { OptionsType, ValueType } from 'react-select'
-import WordAligner from './WordAligner'
-import produce, { castDraft, Draft } from 'immer'
-import Reconstruction from 'corpus/ui/Reconstruction'
 import {
-  ChapterAlignment,
   AlignmentToken,
+  ChapterAlignment,
   ManuscriptAlignment,
 } from 'corpus/domain/alignment'
-import { isAnyWord } from 'transliteration/domain/type-guards'
-import { Token } from 'transliteration/domain/token'
-
-interface OmittedWordOption {
-  value: number
-  label: string
-}
+import { Chapter, Line, ManuscriptLine } from 'corpus/domain/text'
+import Reconstruction from 'corpus/ui/Reconstruction'
+import produce, { castDraft, Draft } from 'immer'
+import React, { useState } from 'react'
+import { Badge, Button, Col, Container, Row } from 'react-bootstrap'
+import OmittedWordsSelect from './OmittedWordsSelect'
+import WordAligner from './WordAligner'
 
 const setAlignment = produce(
   (draft: Draft<ManuscriptAlignment>, index: number, token: AlignmentToken) => {
@@ -30,52 +22,6 @@ const setOmittedWords = produce(
     draft.omittedWords = value
   }
 )
-
-function createOmittedWordOptions(
-  reconstructionTokens: readonly Token[]
-): OptionsType<OmittedWordOption> {
-  return _(reconstructionTokens)
-    .map((reconstructionToken, index) =>
-      isAnyWord(reconstructionToken)
-        ? {
-            value: index,
-            label: reconstructionToken.value,
-          }
-        : null
-    )
-    .reject(_.isNull)
-    .value() as OmittedWordOption[]
-}
-
-function OmittedWordsSelect(props: {
-  label: string
-  reconstructionTokens: readonly Token[]
-  value: readonly number[]
-  onChange: (value: number[]) => void
-}): JSX.Element {
-  const handleChange = (value: ValueType<OmittedWordOption>) => {
-    props.onChange(_.isArray(value) ? value.map((option) => option.value) : [])
-  }
-
-  const options: OptionsType<OmittedWordOption> = createOmittedWordOptions(
-    props.reconstructionTokens
-  )
-
-  return (
-    <Select
-      aria-label={props.label}
-      placeholder={props.label}
-      options={options}
-      value={
-        props.value
-          .map((index) => options.find((option) => option.value === index))
-          .filter((option) => option) as OmittedWordOption[]
-      }
-      isMulti
-      onChange={handleChange}
-    />
-  )
-}
 
 function ManuscriptAligner(props: {
   chapter: Chapter
