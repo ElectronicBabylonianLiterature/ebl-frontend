c4def85f22758d2c60646fdec0b73b0304efe52d
David Englmeier <david.englmeier@ifi.lmu.de>
2020-12-21 01:06:24 +0100

    added wordbutton component

---
+++ src/fragmentarium/ui/fragment/WordDownloadButton.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { wordExport } from './WordExport'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import WordService from 'dictionary/application/WordService'
+import { Dropdown } from 'react-bootstrap'
+import { saveAs } from 'file-saver'
+import Spinner from 'common/Spinner'
+
+type Props = {
+  children?: React.ReactNode
+  fragment?: Fragment
+  wordService?: WordService
+}
+
+export default function WordDownloadButton({
+  fragment,
+  wordService,
+  children,
+}: Props): JSX.Element {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = (event) => {
+    setIsLoading(true)
+    saveWordBlob(fragment, wordService).then((blob) => {
+      setIsLoading(false)
+    })
+  }
+
+  return (
+    <>
+      <Dropdown.Item onClick={handleClick}>
+        {isLoading ? <Spinner /> : children}
+      </Dropdown.Item>
+    </>
+  )
+}
+
+async function saveWordBlob(fragment, wordService) {
+  const wordBlob = await wordExport(fragment, wordService)
+  saveAs(wordBlob, `${fragment.number}.docx`)
+}
