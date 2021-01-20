c130b6a897b260fec9cb146ce7363afead638c40
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-01-20 09:36:58 +0000

    Show suggested alignments in red

--- src/corpus/ui/alignment/Word.tsx
+++ src/corpus/ui/alignment/Word.tsx
@@ -4,6 +4,7 @@ import _ from 'lodash'
 import './Word.css'
 import { Token } from 'transliteration/domain/token'
 import { AlignmentToken } from 'corpus/domain/alignment'
+import classNames from 'classnames'
 
 interface Props {
   token: AlignmentToken
@@ -12,14 +13,20 @@ interface Props {
 
 function Word({ token, reconstructionTokens }: Props): JSX.Element {
   return (
-    <>
+    <span
+      className={classNames({
+        Word: true,
+        'Word--with-alignment': !_.isNil(token.alignment),
+        'Word--suggestion': token.suggested,
+      })}
+    >
       {token.value}
       {_.isNumber(token.alignment) && (
         <span className="Word__alignment">
           {reconstructionTokens[Number(token.alignment)].value}
         </span>
       )}
-    </>
+    </span>
   )
 }
 
