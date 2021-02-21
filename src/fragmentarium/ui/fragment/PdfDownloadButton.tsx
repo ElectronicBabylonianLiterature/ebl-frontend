b78895ac9237fae9bb0355d7a6b3c432ede10670
Hamster3001 <David.Englmeier@hotmail.de>
2021-02-21 21:44:43 +0100

    Pdf export (#84)
    * first implemetation TEI-Export
    
    * rm old get content function
    
    * use clean value + updated export function
    
    * code refactor according to tests / comments
    
    * function rename
    
    * added function for bibliography maybe not needed anyway (commented out)
    
    * update download test
    
    * first ts conversion (probably not finished)
    
    * rm filter map (slow) + shorter functions
    
    * some code simplification...
    
    * further simplification...
    
    * small refactor  + changed tsx to ts
    
    * add types to function params and minor code refactoring
    
    * rm comment
    
    * change test to getByRole (from getByTestId)
    
    * camelCase
    
    * first commit tei export test + added dependency for "fast-xml-parser"
    
    * test update
    
    * squash merge of TEIExport brah
    Squashed commit of the following:
    
    commit 2ec861665e8805bec98747c82aeaeb30ad51060f
    Author: David Englmeier <david.englmeier@ifi.lmu.de>
    Date:   Mon Oct 5 13:00:37 2020 +0200
    
        ts export update
    
    * word parsing mostly fiinished
    
    * trigger WordExport on button click
    
    * added wordbutton component
    
    * rm ?undefined from props
    
    * fix previous download test by adding mock for wordservice
    
    * add new WordService
    
    * basic commit wordExport Test
    
    * rm unused module
    
    * updated yarn lock
    
    * possible preparation for PDF export
    
    * use jquery ref div + some cc issues
    
    * jquery ref substitute
    
    * rm bluebird test
    
    * package.json for WE
    
    * Packages for WE
    
    * back to pre merge
    
    * rm package-lock
    
    * commit yarn lock?
    
    * bluebird promise for wordDownloadButton
    
    * return types for bluebird promises
    
    * cancel Bluebird Promises
    
    * update cancel promises
    
    * small-caps to all-small-caps
    
    * pdf first implementation finished
    
    * pdf export update
    
    * types for pdf export + test
    
    * dependencies for pdfExport
    
    Co-authored-by: David Englmeier <david.englmeier@ifi.lmu.de>

---
+++ src/fragmentarium/ui/fragment/PdfDownloadButton.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { pdfExport } from './PdfExport'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import WordService from 'dictionary/application/WordService'
+import { Dropdown } from 'react-bootstrap'
+import Spinner from 'common/Spinner'
+import $ from 'jquery'
+import Promise from 'bluebird'
+import usePromiseEffect from 'common/usePromiseEffect'
+import { jsPDF } from 'jspdf'
+
+type Props = {
+  children: React.ReactNode
+  fragment: Fragment
+  wordService: WordService
+}
+
+export default function PdfDownloadButton({
+  fragment,
+  wordService,
+  children,
+}: Props): JSX.Element {
+  const [isLoading, setIsLoading] = useState(false)
+  const [setPromise, cancelPromise] = usePromiseEffect()
+
+  const handleClick = (event) => {
+    const jQueryRef = $('#jQueryContainer')
+    setIsLoading(true)
+    cancelPromise()
+
+    setPromise(
+      getPdfDoc(fragment, wordService, jQueryRef).then((doc) => {
+        doc.save(fragment.number + '.pdf')
+        setIsLoading(false)
+      })
+    )
+  }
+
+  return (
+    <>
+      <Dropdown.Item onClick={handleClick}>
+        {isLoading ? <Spinner /> : children}
+      </Dropdown.Item>
+      <div id="jQueryContainer" style={{ display: 'none' }}></div>
+    </>
+  )
+}
+
+function getPdfDoc(
+  fragment: Fragment,
+  wordService: WordService,
+  jQueryRef: JQuery
+): Promise<jsPDF> {
+  return new Promise(function (resolve) {
+    resolve(pdfExport(fragment, wordService, jQueryRef))
+  })
+}
---
+++ src/fragmentarium/ui/fragment/PdfExport.test.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useRef, RefObject } from 'react'
+import $ from 'jquery'
+import { act, render } from '@testing-library/react'
+import { factory } from 'factory-girl'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import complexText from 'test-support/complexTestText'
+import WordService from 'dictionary/application/WordService'
+import { wordDto } from 'test-support/test-word'
+import { pdfExport } from './PdfExport'
+import WordDownloadButton from './WordDownloadButton'
+import { jsPDF } from 'jspdf'
+
+jest.mock('dictionary/application/WordService')
+
+let wordService
+let fragment: Fragment
+let pdfDoc: jsPDF
+
+beforeEach(async () => {
+  wordService = new (WordService as jest.Mock<jest.Mocked<WordService>>)()
+  jest
+    .spyOn(wordService, 'find')
+    .mockImplementation(() => Promise.resolve(wordDto))
+  fragment = await factory.build('fragment', {
+    publication: 'Guod cigipli epibif odepuwu.',
+    text: complexText,
+    description:
+      'Balbodduh lifuseb wuuk nasu hulwajo ho hiskuk riwa eldat ivu jandara nosrina abike befukiz ravsus.\nZut uzzejum ub mil ika roppar zewize ipifac vut eci avimez cewmikjov kiwso zamli jecja now.',
+  })
+
+  pdfDoc = await pdfExport(fragment, wordService, $('#jQueryContainer')).then(
+    (doc) => {
+      return doc
+    }
+  )
+})
+
+test('outputType', () => {
+  expect(typeof pdfDoc).toBe('object')
+})
