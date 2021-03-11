c5624e7bba33a6b53bb30aafa78b4610e8e3aba3
yCobanoglu <yunus.ebabylon@gmail.com>
2021-03-11 13:28:42 +0100

    remove console log

--- src/dictionary/ui/display/WordDisplay.tsx
+++ src/dictionary/ui/display/WordDisplay.tsx
@@ -95,7 +95,6 @@ function WordDisplay({ word }: { word: Word }): JSX.Element {
 }
 
 function WordDisplayDetails({ word }: { word: Word }): JSX.Element {
-  console.log(word)
   return (
     <Row>
       <Col>
