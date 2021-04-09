dad48b50568efb5a9d191312bd69133e5afa346a
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-04-09 11:53:46 +0000

    Change LemmaInput back to class component
    See: https://stackoverflow.com/questions/42573017/in-react-es6-why-does-the-input-field-lose-focus-after-typing-a-character

--- src/dictionary/ui/editor/LemmaInput.tsx
+++ src/dictionary/ui/editor/LemmaInput.tsx
@@ -1,53 +1,61 @@
-import React, { ChangeEvent } from 'react'
-import { FormControl, FormGroup, FormLabel, InputGroup } from 'react-bootstrap'
+import React, { ChangeEvent, Component } from 'react'
+import { FormGroup, FormLabel, FormControl, InputGroup } from 'react-bootstrap'
 import _ from 'lodash'
 
-export default function LemmaInput({
-  value,
-  onChange,
-}: {
+class LemmaInput extends Component<{
   value: { lemma: readonly string[]; attested?: boolean }
   onChange: (lemma) => void
-}): JSX.Element {
-  const lemmaChanged = (event: ChangeEvent<HTMLTextAreaElement>): void => {
-    onChange({
-      ...value,
+}> {
+  get hasAttested(): boolean {
+    return _.has(this.props.value, 'attested')
+  }
+
+  lemmaFormControl = (): JSX.Element => {
+    return (
+      <FormControl
+        type="text"
+        value={this.props.value.lemma.join(' ')}
+        onChange={this.lemmaChanged}
+      />
+    )
+  }
+
+  lemmaChanged = (event: ChangeEvent<HTMLTextAreaElement>): void => {
+    this.props.onChange({
+      ...this.props.value,
       lemma: event.target.value.split(' '),
     })
   }
 
-  const LemmaFormControl = (): JSX.Element => (
-    <FormControl
-      type="text"
-      value={value.lemma.join(' ')}
-      onChange={lemmaChanged}
-    />
-  )
-
-  const attestedChanged = (event: ChangeEvent<HTMLInputElement>): void => {
-    onChange({
-      ...value,
+  attestedChanged = (event: ChangeEvent<HTMLInputElement>): void => {
+    this.props.onChange({
+      ...this.props.value,
       attested: event.target.checked,
     })
   }
-  return (
-    <FormGroup controlId={_.uniqueId('LemmaInput-')}>
-      <FormLabel>Lemma</FormLabel>
-      {_.has(value, 'attested') ? (
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Checkbox
-              type="checkbox"
-              aria-label="attested"
-              checked={value.attested}
-              onChange={attestedChanged}
-            />
-          </InputGroup.Prepend>
-          <LemmaFormControl />
-        </InputGroup>
-      ) : (
-        <LemmaFormControl />
-      )}
-    </FormGroup>
-  )
+
+  render(): JSX.Element {
+    return (
+      <FormGroup controlId={_.uniqueId('LemmaInput-')}>
+        <FormLabel>Lemma</FormLabel>
+        {_.has(this.props.value, 'attested') ? (
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Checkbox
+                type="checkbox"
+                aria-label="attested"
+                checked={this.props.value.attested}
+                onChange={this.attestedChanged}
+              />
+            </InputGroup.Prepend>
+            <this.lemmaFormControl />
+          </InputGroup>
+        ) : (
+          <this.lemmaFormControl />
+        )}
+      </FormGroup>
+    )
+  }
 }
+
+export default LemmaInput
