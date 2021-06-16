2feb42601cd684e9a07def4d8cfb909c1de9a9f1
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2021-06-16 14:15:28 +0200

    rename already defined

--- src/signs/ui/display/Word.tsx
+++ src/signs/ui/display/Word.tsx
@@ -1,8 +1,27 @@
 import withData, { WithoutData } from 'http/withData'
 import Word from 'dictionary/domain/Word'
-import React from 'react'
+import React, { Fragment } from 'react'
 import WordService from 'dictionary/application/WordService'
-import { Lemma } from 'dictionary/ui/search/Word'
+import { Link } from 'react-router-dom'
+
+function Lemma({ word }: { word: Word }): JSX.Element {
+  console.log(word)
+  const attested = word.attested === false ? '*' : ''
+  const lemma = word.lemma.join(' ')
+  return (
+    <Fragment>
+      {word._id ? (
+        <Link to={`/dictionary/${word._id}`}>
+          {attested}
+          {lemma}
+        </Link>
+      ) : (
+        <em>{`${attested}${lemma}`}</em>
+      )}
+      <span>{word.homonym && ` ${word.homonym}`}</span>
+    </Fragment>
+  )
+}
 
 type Props = {
   data: Word
