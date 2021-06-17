4ab332540cc9a5dcacffc91f9c0fdb12f45f382c
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-06-17 12:54:55 +0000

    Remove useless escape

--- src/signs/ui/search/MesZLContent.tsx
+++ src/signs/ui/search/MesZLContent.tsx
@@ -27,7 +27,7 @@ async function convertMarkdownToHtml(markdown: string): Promise<string> {
   // supSuper Issue https://github.com/zestedesavoir/zmarkdown/issues/438
   const subSup = (mesZL: string): string =>
     mesZL
-      .replace(/\^([^\^]*)\^/g, '<sup>$1</sup>')
+      .replace(/\^([^^]*)\^/g, '<sup>$1</sup>')
       .replace(/~([^~]*)~/g, '<sub>$1</sub>')
 
   // remark uses commonMarkdown, that's why we have to parse italic manually ontop of transforming it with remark
