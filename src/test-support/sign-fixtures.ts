ea274cf9a4add3abcd4df8f262bf2bf340290537
yCobanoglu <yunus.cobanoglu@yahoo.de>
2021-07-02 14:43:09 +0200

    extract constant reduce lines of code

--- src/test-support/sign-fixtures.ts
+++ src/test-support/sign-fixtures.ts
@@ -4,6 +4,19 @@ import { Factory } from 'fishery'
 
 const chance = new Chance()
 
+const mesZL = `123	**ALSK13**	𒁇𒍴
+Lorem ipsum dolor *sit* amet, consetetur <span style="color: #00610F;">*sadipscing*(*l*)*ubasd*</span>sadipscing elitr, sed diam *nonumy*
+eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet c
+lita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+^aba^𒁇𒊩 = BAR-MUNUS = *parratu*, weibliches Lamm. CAD <span style="color: #00610F;">P</span> 192b liest *parsallu*.
+<span style="color: #00610F;">P</span>
+^aba^𒁇𒋝
+^asdg^𒋝
+^ghas^𒁈
+𒁖^asd^
+𒍴^q12asd^
+`
+
 export const signFactory = Factory.define<Sign>(() => {
   return new Sign({
     name: chance.pickone(['BA', 'BAD', 'BAR', 'PI']),
@@ -25,17 +38,6 @@ export const signFactory = Factory.define<Sign>(() => {
           '<sup>giš</sup>BAR-KIN₂; *seḫpu* (Bast, Rinde); ME 69 CD 320a',
       },
     ],
-    mesZl: `123	**ALSK13**	𒁇𒍴
-Lorem ipsum dolor *sit* amet, consetetur <span style="color: #00610F;">*sadipscing*(*l*)*ubasd*</span>sadipscing elitr, sed diam *nonumy*
-eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet c
-lita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-^aba^𒁇𒊩 = BAR-MUNUS = *parratu*, weibliches Lamm. CAD <span style="color: #00610F;">P</span> 192b liest *parsallu*.
-<span style="color: #00610F;">P</span>
-^aba^𒁇𒋝
-^asdg^𒋝
-^ghas^𒁈
-𒁖^asd^
-𒍴^q12asd^
-`,
+    mesZl: mesZL,
   })
 })
