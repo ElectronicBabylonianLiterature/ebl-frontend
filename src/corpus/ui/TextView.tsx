ee533b76d57e4e66dc6a01d9850fc393b945be0e
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-07-16 08:12:38 +0000

    Extract component

--- src/corpus/ui/TextView.tsx
+++ src/corpus/ui/TextView.tsx
@@ -10,7 +10,7 @@ import withData from 'http/withData'
 import ChapterNavigation from './ChapterNavigation'
 
 import './TextView.sass'
-import { groupReferences } from 'bibliography/domain/Reference'
+import Reference, { groupReferences } from 'bibliography/domain/Reference'
 import referencePopover from 'bibliography/ui/referencePopover'
 
 const Citation = referencePopover(({ reference }) => (
@@ -18,6 +18,32 @@ const Citation = referencePopover(({ reference }) => (
     {reference.primaryAuthor} {reference.year}
   </>
 ))
+
+function References({
+  references,
+}: {
+  references: readonly Reference[]
+}): JSX.Element {
+  return (
+    <section className="text-view__references">
+      <h4>References</h4>
+      {groupReferences(references).map(([type, group]) => (
+        <p key={type} className="text-view__reference-group">
+          <b className="text-view__reference-group-title">
+            {_.startCase(type.toLowerCase())}
+          </b>
+          :{' '}
+          {group.map((reference, index) => (
+            <React.Fragment key={index}>
+              {index > 0 && ', '}
+              <Citation reference={reference} />
+            </React.Fragment>
+          ))}
+        </p>
+      ))}
+    </section>
+  )
+}
 
 function TextView({ text }: { text: Text }): JSX.Element {
   const title = <InlineMarkdown source={text.title} />
@@ -28,23 +54,7 @@ function TextView({ text }: { text: Text }): JSX.Element {
         <h3>Introduction</h3>
         <ReactMarkdown className="text-view__markdown" source={text.intro} />
         {!_.isEmpty(text.references) && (
-          <section className="text-view__references">
-            <h4>References</h4>
-            {groupReferences(text.references).map(([type, group]) => (
-              <p key={type} className="text-view__reference-group">
-                <b className="text-view__reference-group-title">
-                  {_.startCase(type.toLowerCase())}
-                </b>
-                :{' '}
-                {group.map((reference, index) => (
-                  <React.Fragment key={index}>
-                    {index > 0 && ', '}
-                    <Citation reference={reference} />
-                  </React.Fragment>
-                ))}
-              </p>
-            ))}
-          </section>
+          <References references={text.references} />
         )}
       </section>
       <section className="text-view__chapter-list">
