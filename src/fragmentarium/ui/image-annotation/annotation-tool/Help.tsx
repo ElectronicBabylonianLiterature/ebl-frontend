304e92ac95272a6465443f12193c0c041db04b17
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2021-10-13 14:29:20 +0200

    help for autmatic selection

--- src/fragmentarium/ui/image-annotation/annotation-tool/Help.tsx
+++ src/fragmentarium/ui/image-annotation/annotation-tool/Help.tsx
@@ -11,11 +11,23 @@ export default function Help(): JSX.Element {
             <b>Esc</b> Reset
           </li>
           <li>
-            <b>Ctrl + Selecting Annotation:</b> Change Reading on existing
+            <b>Ctrl + Select Annotation:</b> Change Reading on existing
             Annotation
           </li>
           <li>
             <b>Shift + Panning:</b> Disable Selection and perform Panning
+          </li>
+          <li>
+            <b>Shift + Panning:</b> Disable Selection and perform Panning
+          </li>
+          <li>
+            <b>Automatic Selection</b> To perform Automatic Selection annotate
+            one Sign with it&apos;s corresponding reading from the left and then
+            annotate right neighbouring signs with <b>blank</b>. Then click on
+            Automatic Selection and select the first annotated Sign (the one
+            which has a proper reading not blank) neighbouring annotations with
+            blank will be automatically linked to their corresponding readings
+            on the left.
           </li>
         </ul>
       </Popover.Content>
