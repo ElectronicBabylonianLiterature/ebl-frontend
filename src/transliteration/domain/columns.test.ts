8f00a667999f91bce9312d1f7408c1c2aa4e872b
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2021-12-14 10:06:46 +0000

    Extract column creation to new file

---
+++ src/transliteration/domain/columns.test.ts
@@ -0,0 +1,102 @@
+import {
+  emptyFirstColumn,
+  firstColumnSpan,
+  implicitFirstColumn,
+  span,
+} from 'test-support/lines/text-columns'
+import { lemmatized } from 'test-support/lines/text-lemmatization'
+import { containsColumns, createColumns, numberOfColumns } from './columns'
+
+test.each([
+  [lemmatized[0], false],
+  [emptyFirstColumn, true],
+  [firstColumnSpan, true],
+  [implicitFirstColumn, true],
+  [span, true],
+])('hasColumns', (line, expected) => {
+  expect(containsColumns(line.content)).toEqual(expected)
+})
+
+test.each([
+  [
+    emptyFirstColumn,
+    [
+      {
+        span: 1,
+        content: [],
+      },
+      {
+        span: 1,
+        content: [emptyFirstColumn.content[1]],
+      },
+      {
+        span: 1,
+        content: [emptyFirstColumn.content[3]],
+      },
+      {
+        span: 1,
+        content: [emptyFirstColumn.content[5]],
+      },
+    ],
+  ],
+  [
+    implicitFirstColumn,
+    [
+      {
+        span: 1,
+        content: [implicitFirstColumn.content[0]],
+      },
+      {
+        span: 1,
+        content: [implicitFirstColumn.content[2]],
+      },
+    ],
+  ],
+  [
+    firstColumnSpan,
+    [
+      {
+        span: 2,
+        content: [firstColumnSpan.content[1]],
+      },
+      {
+        span: 1,
+        content: [firstColumnSpan.content[3]],
+      },
+    ],
+  ],
+  [
+    span,
+    [
+      {
+        span: 1,
+        content: [span.content[0]],
+      },
+      {
+        span: 2,
+        content: [span.content[2]],
+      },
+    ],
+  ],
+  [
+    lemmatized[0],
+    [
+      {
+        span: null,
+        content: lemmatized[0].content,
+      },
+    ],
+  ],
+])('columns', (line, expected) => {
+  expect(createColumns(line.content)).toEqual(expected)
+})
+
+test.each([
+  [lemmatized[0], 1],
+  [emptyFirstColumn, 4],
+  [implicitFirstColumn, 2],
+  [firstColumnSpan, 3],
+  [span, 3],
+])('numberOfColumns', (line, expected) => {
+  expect(numberOfColumns(createColumns(line.content))).toEqual(expected)
+})
--- src/transliteration/domain/text-line.ts
+++ src/transliteration/domain/text-line.ts
@@ -1,19 +1,11 @@
-import _ from 'lodash'
-import { Token } from './token'
 import { LineNumber, LineNumberRange } from './line-number'
-import { isColumn } from './type-guards'
 import { AbstractLine, LineBaseDto } from './abstract-line'
+import { TextLineColumn, createColumns, numberOfColumns } from './columns'
 
 export interface TextLineDto extends LineBaseDto {
   readonly type: 'TextLine'
   readonly lineNumber: LineNumber | LineNumberRange
 }
-
-export interface TextLineColumn {
-  span: number | null
-  content: Token[]
-}
-const defaultSpan = 1
 
 export class TextLine extends AbstractLine {
   readonly type = 'TextLine'
@@ -25,41 +17,10 @@ export class TextLine extends AbstractLine {
   }
 
   get columns(): readonly TextLineColumn[] {
-    return _.reduce<Token, TextLineColumn[]>(
-      this.content,
-      (columns, current) => {
-        if (isColumn(current)) {
-          if (_.isEmpty(columns) && current.number === null) {
-            columns.push({
-              span: defaultSpan,
-              content: [],
-            })
-          }
-          columns.push({
-            span: current.number ?? defaultSpan,
-            content: [],
-          })
-        } else if (_.isEmpty(columns)) {
-          columns.push({
-            span: this.hasColumns ? 1 : null,
-            content: [current],
-          })
-        } else {
-          _.last(columns)?.content.push(current)
-        }
-        return columns
-      },
-      []
-    )
+    return createColumns(this.content)
   }
 
   get numberOfColumns(): number {
-    return _(this.columns)
-      .map((column) => column.span ?? defaultSpan)
-      .sum()
-  }
-
-  get hasColumns(): boolean {
-    return this.content.some(isColumn)
+    return numberOfColumns(this.columns)
   }
 }
