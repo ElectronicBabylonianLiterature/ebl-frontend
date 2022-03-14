917388990a42a0620e76d6785a7d1038027ec33f
Jussi Laasonen <404469+jlaasonen@users.noreply.github.com>
2022-03-14 10:58:15 +0000

    Move Score to new file

--- src/corpus/ui/ChapterViewLine.tsx
+++ src/corpus/ui/ChapterViewLine.tsx
@@ -1,21 +1,18 @@
 import React, { useContext, useEffect, useMemo, useRef } from 'react'
 import _ from 'lodash'
-import { ChapterDisplay, ChapterId, LineDisplay } from 'corpus/domain/chapter'
-import withData from 'http/withData'
+import { ChapterDisplay, LineDisplay } from 'corpus/domain/chapter'
 import { LineColumns } from 'transliteration/ui/line-tokens'
 import Markup from 'transliteration/ui/markup'
 import lineNumberToString from 'transliteration/domain/lineNumberToString'
 import { TextLineColumn } from 'transliteration/domain/columns'
 import classNames from 'classnames'
 import TextService from 'corpus/application/TextService'
-import { LineDetails, ManuscriptLineDisplay } from 'corpus/domain/line-details'
-import classnames from 'classnames'
-import { Collapse, OverlayTrigger, Popover } from 'react-bootstrap'
-import { isTextLine } from 'transliteration/domain/type-guards'
+import { Collapse } from 'react-bootstrap'
 import RowsContext from './RowsContext'
 import TranslationContext from './TranslationContext'
 import { Anchor } from 'transliteration/ui/LineNumber'
 import { parallelLinePrefix } from 'transliteration/domain/parallel-line'
+import Score from './Score'
 
 const lineNumberColumns = 1
 const toggleColumns = 2
@@ -89,103 +86,6 @@ function Translation({
     <td colSpan={translationColumns} />
   )
 }
-
-function Manuscript({
-  manuscript,
-  maxColumns,
-}: {
-  manuscript: ManuscriptLineDisplay
-  maxColumns: number
-}): JSX.Element {
-  return (
-    <tr
-      className={classnames({
-        'chapter-display__manuscript': true,
-        'chapter-display__manuscript--standard': manuscript.isStandardText,
-      })}
-    >
-      <td>
-        <span className="chapter-display__manuscript-siglum">
-          {manuscript.isParallelText && parallelLinePrefix}
-          {manuscript.siglum}
-        </span>
-      </td>
-      <td>
-        <span className="chapter-display__manuscript-labels">
-          {manuscript.labels.join(' ')}{' '}
-          {manuscript.number !== null &&
-            `${lineNumberToString(manuscript.number)}.`}
-        </span>
-      </td>
-      {isTextLine(manuscript.line) ? (
-        <LineColumns
-          columns={manuscript.line.columns}
-          maxColumns={maxColumns}
-        />
-      ) : (
-        <td colSpan={maxColumns}></td>
-      )}
-      <td>
-        <span className="chapter-display__manuscript-paratext">
-          {manuscript.dollarLines
-            .map((dollarLine) => dollarLine.displayValue)
-            .join(' ')}
-        </span>
-      </td>
-      <td>
-        {manuscript.hasNotes && (
-          <OverlayTrigger
-            rootClose
-            overlay={
-              <Popover
-                id={_.uniqueId('ManuscriptLineNotesPopOver-')}
-                className=""
-              >
-                <Popover.Content>
-                  <ol className="chapter-display__manuscript-notes">
-                    {manuscript.noteLines.map((note, index) => (
-                      <Markup key={index} container="li" parts={note.parts} />
-                    ))}
-                  </ol>
-                </Popover.Content>
-              </Popover>
-            }
-            trigger={['click']}
-            placement="top"
-          >
-            <i className="fas fa-book chapter-display__manuscript-info-toggle"></i>
-          </OverlayTrigger>
-        )}
-      </td>
-    </tr>
-  )
-}
-
-const Score = withData<
-  Record<string, unknown>,
-  {
-    id: ChapterId
-    lineNumber: number
-    textService: TextService
-  },
-  LineDetails
->(
-  ({ data: line }): JSX.Element => (
-    <table className="chapter-display__manuscripts">
-      <tbody>
-        {line.sortedManuscripts.map((manuscript, index) => (
-          <Manuscript
-            manuscript={manuscript}
-            key={index}
-            maxColumns={line.numberOfColumns}
-          />
-        ))}
-      </tbody>
-    </table>
-  ),
-  ({ id, lineNumber, textService }) =>
-    textService.findChapterLine(id, lineNumber)
-)
 
 export function ChapterViewLine({
   chapter,
