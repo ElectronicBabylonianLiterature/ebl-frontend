6f181113400bc8e1923f6b01cb2df33366285d5d
Enrique Jiménez <enrique.jimenez@lmu.de>
2022-04-05 13:03:27 +0000

    Correct typo and snapshot

--- src/signs/ui/display/SignImages.tsx
+++ src/signs/ui/display/SignImages.tsx
@@ -72,7 +72,7 @@ function SignImagePagination({
     <Container>
       <Row>
         <Col>
-          <h3>&#8546;. Palaeopgrahy</h3>
+          <h3>&#8546;. Palaeography</h3>
         </Col>
       </Row>
       <Row>
