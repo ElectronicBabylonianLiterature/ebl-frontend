b6b5592c1a4ac471a8a33c89b71d1562b3a3325e
Yunus Cobanoglu <56864644+yCobanoglu@users.noreply.github.com>
2022-04-28 13:02:50 +0200

    unify searches (#239)

--- src/fragmentarium/application/FragmentSearchService.ts
+++ src/fragmentarium/application/FragmentSearchService.ts
@@ -8,9 +8,12 @@ export type FragmentInfoPromise = Promise<FragmentInfo>
 export interface FragmentInfoRepository {
   random(): FragmentInfosPromise
   interesting(): FragmentInfosPromise
-  searchNumber(number: string): FragmentInfosPromise
-  searchReference(id: string, page: string): FragmentInfosPromise
-  searchTransliteration(number: string): FragmentInfosPromise
+  searchFragmentarium(
+    number: string,
+    transliteration: string,
+    id: string,
+    pages: string
+  ): FragmentInfosPromise
   fetchLatestTransliterations(): FragmentInfosPromise
   fetchNeedsRevision(): FragmentInfosPromise
 }
@@ -48,15 +51,18 @@ export default class FragmentSearchService {
       })
   }
 
-  searchNumber(number: string): FragmentInfosPromise {
-    return this.fragmentRepository.searchNumber(number)
-  }
-  searchReference(id: string, pages: string): FragmentInfosPromise {
-    return this.fragmentRepository.searchReference(id, pages)
-  }
-
-  searchTransliteration(transliteration: string): FragmentInfosPromise {
-    return this.fragmentRepository.searchTransliteration(transliteration)
+  searchFragmentarium(
+    number: string,
+    transliteration: string,
+    bibliographyId: string,
+    pages: string
+  ): FragmentInfosPromise {
+    return this.fragmentRepository.searchFragmentarium(
+      number,
+      transliteration,
+      bibliographyId,
+      pages
+    )
   }
 
   fetchLatestTransliterations(): FragmentInfosPromise {
---
+++ src/fragmentarium/ui/search/FragmentariumSearchResults.test.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import Bluebird from 'bluebird'
+import { fragmentInfoFactory } from 'test-support/fragment-fixtures'
+import { FragmentInfo } from 'fragmentarium/domain/fragment'
+import FragmentariumSearchResults from './FragmentariumSearchResults'
+import FragmentSearchService from 'fragmentarium/application/FragmentSearchService'
+
+jest.mock('fragmentarium/application/FragmentSearchService')
+const number = 'K.003292'
+let fragments: FragmentInfo[]
+const fragmentSearchService = new (FragmentSearchService as jest.Mock<
+  jest.Mocked<FragmentSearchService>
+>)()
+
+function renderFragmentariumSearchResults(
+  number = '',
+  transliteration = '',
+  bibliographyId = '',
+  pages = ''
+) {
+  return render(
+    <MemoryRouter>
+      <FragmentariumSearchResults
+        number={number}
+        transliteration={transliteration}
+        bibliographyId={bibliographyId}
+        pages={pages}
+        fragmentSearchService={fragmentSearchService}
+      />
+    </MemoryRouter>
+  )
+}
+
+describe('search fragmentarium only number', () => {
+  beforeEach(async () => {
+    fragments = fragmentInfoFactory.buildList(2)
+    fragmentSearchService.searchFragmentarium.mockReturnValueOnce(
+      Bluebird.resolve(fragments)
+    )
+    renderFragmentariumSearchResults(number)
+    await screen.findByText('Number')
+  })
+
+  it('Searches for the given parameters', () => {
+    expect(fragmentSearchService.searchFragmentarium).toBeCalledWith(
+      number,
+      '',
+      '',
+      ''
+    )
+  })
+
+  it('Displays and links results', async () => {
+    for (const fragment of fragments) {
+      expect(await screen.findByText(fragment.number)).toHaveAttribute(
+        'href',
+        `/fragmentarium/${fragment.number}`
+      )
+    }
+  })
+})
+
+const transliteration = 'ma i-ra\nka li'
+
+describe('search fragmentarium only transliteration', () => {
+  beforeEach(async () => {
+    fragments = [
+      fragmentInfoFactory.build(
+        {},
+        { associations: { matchingLines: [['line 1', 'line 2']] } }
+      ),
+      fragmentInfoFactory.build(
+        {},
+        { associations: { matchingLines: [['line 3'], ['line 4']] } }
+      ),
+    ]
+    fragmentSearchService.searchFragmentarium.mockReturnValueOnce(
+      Bluebird.resolve(fragments)
+    )
+    renderFragmentariumSearchResults('', transliteration)
+    await screen.findByText(fragments[0].number)
+  })
+
+  it('Queries the API with given parameters', () => {
+    expect(fragmentSearchService.searchFragmentarium).toBeCalledWith(
+      '',
+      transliteration,
+      '',
+      ''
+    )
+  })
+
+  it('Links results', () => {
+    for (const fragment of fragments) {
+      expect(screen.getByText(fragment.number)).toHaveAttribute(
+        'href',
+        `/fragmentarium/${fragment.number}`
+      )
+    }
+  })
+
+  it('Displays script', () => {
+    for (const fragment of fragments) {
+      expect(screen.getAllByText(fragment.script)).not.toEqual([])
+    }
+  })
+
+  it('Displays matching lines', () => {
+    for (const line of fragments.flatMap((fragment) =>
+      fragment.matchingLines.flat()
+    )) {
+      expect(screen.getAllByText(line)).not.toEqual([])
+    }
+  })
+})
