7a6866b949e91ec561aa3dcf30f071b2dc45c787
Enrique Jiménez <enrique.jimenez@lmu.de>
2022-04-29 07:13:46 +0000

    Restructure PR

--- src/dictionary/ui/display/WordDisplay.tsx
+++ src/dictionary/ui/display/WordDisplay.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import Word from 'dictionary/domain/Word'
-import withData, { WithoutData } from 'http/withData'
-import { RouteComponentProps } from 'react-router-dom'
 import AppContent from 'common/AppContent'
 import { SectionCrumb, TextCrumb } from 'common/Breadcrumbs'
 import { Button, Col, Row } from 'react-bootstrap'
 import './wordInformationDisplay.sass'
-import WordService from 'dictionary/application/WordService'
+import withData, { WithoutData } from 'http/withData'
+import { RouteComponentProps } from 'react-router-dom'
 import { LiteratureRedirectBox } from 'common/LiteratureRedirectBox'
-import {
-  AmplifiedMeanings,
-  AmplifiedMeaningsDetails,
-  Derivatives,
-  Join,
-  Logogram,
-  OtherForm,
-  SingleDerivative,
-} from 'dictionary/ui/display/WordDisplayParts'
 import { AGI } from 'dictionary/ui/display/WordDisplayAGI'
+import { WordDisplayDetails } from 'dictionary/ui/display/WordDisplayDetails'
 import { Markdown } from 'common/Markdown'
+import WordService from 'dictionary/application/WordService'
 
 const Heading = ({
   number,
@@ -172,96 +164,6 @@ function WordDisplay({ word }: { word: Word }): JSX.Element {
         </>
       )}
     </AppContent>
-  )
-}
-
-function WordDisplayDetails({ word }: { word: Word }): JSX.Element {
-  return (
-    <Row>
-      <Col>
-        <Row>
-          <Col xs={{ offset: 1 }}>
-            <Row>
-              <Col>
-                {word.forms.length > 0 && (
-                  <>
-                    Other forms:&nbsp;
-                    <Join
-                      list={word.forms}
-                      separator={', '}
-                      Component={OtherForm}
-                    />
-                  </>
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                {word.amplifiedMeanings.length > 0 && (
-                  <AmplifiedMeanings
-                    amplifiedMeanings={word.amplifiedMeanings}
-                    wordId={word._id}
-                  />
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                {word.derived.length > 0 && (
-                  <>
-                    Derivatives:&nbsp;
-                    <Derivatives derivatives={word.derived} />
-                  </>
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                {word.derivedFrom && (
-                  <>
-                    Derived from:&nbsp;
-                    <SingleDerivative {...word.derivedFrom} />
-                  </>
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                {word.logograms.length > 0 && (
-                  <>
-                    Logograms:&nbsp;
-                    <Join
-                      list={word.logograms}
-                      separator={', '}
-                      Component={Logogram}
-                    />
-                  </>
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col>{word.roots && `Roots: ${word.roots.join(', ')}`}</Col>
-            </Row>
-            <Row>
-              <Col>
-                <br />
-                {word.meaning && <Markdown text={word.meaning} />}
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <AmplifiedMeaningsDetails
-              amplifiedMeanings={word.amplifiedMeanings}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col></Col>
-        </Row>
-      </Col>
-    </Row>
   )
 }
 type Props = {
