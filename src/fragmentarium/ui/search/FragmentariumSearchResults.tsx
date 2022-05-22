4e371cb0cf03f3ef1cfe94ac5c5696f959a3d39e
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2022-05-22 20:51:20 +0200

    fix ts errors

--- src/fragmentarium/ui/search/FragmentariumSearchResults.tsx
+++ src/fragmentarium/ui/search/FragmentariumSearchResults.tsx
@@ -1,9 +1,14 @@
-import { FragmentInfo } from 'fragmentarium/domain/fragment'
+import {
+  FragmentInfo,
+  FragmentInfosPagination,
+} from 'fragmentarium/domain/fragment'
 import withData from 'http/withData'
 import _ from 'lodash'
 import React, { useEffect, useState } from 'react'
 import ReferenceList from 'bibliography/ui/ReferenceList'
-import FragmentSearchService from 'fragmentarium/application/FragmentSearchService'
+import FragmentSearchService, {
+  FragmentInfosPaginationPromise,
+} from 'fragmentarium/application/FragmentSearchService'
 import { Col, Pagination, Row } from 'react-bootstrap'
 import FragmentLink from 'fragmentarium/ui/FragmentLink'
 import { Genres } from 'fragmentarium/domain/Genres'
@@ -12,20 +17,188 @@ import { DisplayText } from 'transliteration/ui/TransliterationLines'
 import WordService from 'dictionary/application/WordService'
 import { DictionaryContext } from 'dictionary/ui/dictionary-context'
 
-function GenresDisplay({ genres }: { genres: Genres }): JSX.Element {
-  return (
-    <ul>
-      {genres.genres.map((genreItem) => {
-        const uncertain = genreItem.uncertain ? '(?)' : ''
-        return (
-          <ul key={genreItem.toString}>
-            <small>{`${genreItem.category.join(' ➝ ')} ${uncertain}`}</small>
-          </ul>
+type searchPagination = (index: number) => FragmentInfosPaginationPromise
+
+export default withData<
+  {
+    number: string
+    transliteration: string
+    bibliographyId: string
+    pages: string
+    fragmentSearchService: FragmentSearchService
+    wordService: WordService
+  },
+  { fragmentSearchService: FragmentSearchService },
+  FragmentInfosPagination
+>(
+  ({
+    data,
+    number,
+    transliteration,
+    bibliographyId,
+    pages,
+    fragmentSearchService,
+    wordService,
+  }) => (
+    <FragmentInfos
+      wordService={wordService}
+      fragmentInfos={data.fragmentInfos}
+      totalCount={data.totalCount}
+      searchPagination={(pagination: number) =>
+        fragmentSearchService.searchFragmentarium(
+          number,
+          transliteration,
+          bibliographyId,
+          pages,
+          pagination
         )
-      })}
-    </ul>
+      }
+    />
+  ),
+  (props) =>
+    props.fragmentSearchService.searchFragmentarium(
+      props.number,
+      props.transliteration,
+      props.bibliographyId,
+      props.pages
+    ),
+  {
+    watch: (props) => [
+      props.number,
+      props.transliteration,
+      props.bibliographyId,
+      props.pages,
+    ],
+    filter: (props) =>
+      !_.isEmpty(props.number) ||
+      !_.isEmpty(props.transliteration) ||
+      !_.isEmpty(props.bibliographyId),
+    defaultData: { fragmentInfos: [], totalCount: 0 },
+  }
+)
+
+function FragmentInfos({
+  fragmentInfos,
+  totalCount,
+  searchPagination,
+  wordService,
+}: {
+  fragmentInfos: readonly FragmentInfo[]
+  totalCount: number
+  searchPagination: searchPagination
+  wordService: WordService
+}): JSX.Element {
+  const [activePage, setActivePage] = useState(0)
+  const pages = useState(Math.ceil(totalCount / 100))
+  const [fragmentInfosSaved, setFragmentInfosSaved] = useState<any>([
+    { fragmentInfos: fragmentInfos, index: 0 },
+  ])
+  const items = [...Array(pages).keys()].map((_, index) => {
+    return (
+      <Pagination.Item
+        key={index}
+        active={index === activePage}
+        onClick={() => setActivePage(index)}
+      >
+        {index + 1}
+      </Pagination.Item>
+    )
+  })
+
+  const addFragmentInfoSaved = (
+    fragmentInfos: readonly FragmentInfo[],
+    activePage: number
+  ): void => {
+    const fragmentInfoExists = _.find(fragmentInfosSaved, { index: activePage })
+
+    fragmentInfoExists ||
+      setFragmentInfosSaved((old) => [
+        ...old,
+        { fragmentInfos: fragmentInfos, index: activePage },
+      ])
+  }
+
+  useEffect(() => {
+    const preceedingFragmentInfos = activePage + 1
+    searchPagination(preceedingFragmentInfos).then((fragmentInfos) =>
+      addFragmentInfoSaved(fragmentInfos.fragmentInfos, preceedingFragmentInfos)
+    )
+  })
+
+  return (
+    <>
+      <FragmentInfosPageWithData
+        storeFragmentInfos={addFragmentInfoSaved}
+        wordService={wordService}
+        searchPagination={searchPagination}
+        fragmentInfos={fragmentInfosSaved.find(
+          (fragmentInfo) => fragmentInfo.index === activePage
+        )}
+        activeIndex={activePage}
+      />
+      <Col xs={{ offset: 5 }}>
+        <Pagination>{items}</Pagination>
+      </Col>
+    </>
   )
 }
+
+const FragmentInfosPageWithData = withData<
+  {
+    fragmentInfos: readonly FragmentInfo[] | undefined
+    wordService: WordService
+    searchPagination: searchPagination
+    activeIndex: number
+    storeFragmentInfos: any
+  },
+  { searchPagination: searchPagination },
+  any
+>(
+  ({ data, activeIndex, storeFragmentInfos, wordService }) => (
+    <FragmentInfosPage
+      wordService={wordService}
+      storeFragmentInfos={storeFragmentInfos}
+      fragmentInfos={data?.fragmentInfos}
+      activeIndex={activeIndex}
+    />
+  ),
+  (props) =>
+    props.fragmentInfos
+      ? Bluebird.resolve(props.fragmentInfos)
+      : props.searchPagination(props.activeIndex),
+  {
+    watch: (props) => [props.activeIndex],
+  }
+)
+
+function FragmentInfosPage({
+  fragmentInfos,
+  activeIndex,
+  storeFragmentInfos,
+  wordService,
+}: {
+  wordService: WordService
+  fragmentInfos: readonly FragmentInfo[]
+  activeIndex: number
+  storeFragmentInfos: (
+    fragmentInfos: readonly FragmentInfo[],
+    activeIndex: number
+  ) => void
+}) {
+  storeFragmentInfos(fragmentInfos, activeIndex)
+  return (
+    <>
+      {fragmentInfos.map((fragmentInfo, index) => (
+        <FragmentInfoDisplay
+          wordService={wordService}
+          key={index}
+          fragmentInfo={fragmentInfo}
+        />
+      ))}
+    </>
+  )
+}
+
 function FragmentInfoDisplay({
   fragmentInfo,
   wordService,
@@ -67,167 +240,18 @@ function FragmentInfoDisplay({
     </>
   )
 }
-/*
-function FragmentInfosPage ({ fragmentInfos, activeIndex, savedFetchFragmentInfo }) {
-  savedFetchFragmentInfo(fragmentInfos, activeIndex)
+
+function GenresDisplay({ genres }: { genres: Genres }): JSX.Element {
   return (
-    <>
-      {fragmentInfos.map((fragmentInfo, index) => (
-        <FragmentInfoDisplay key={index} fragmentInfo={fragmentInfo} />
-      ))}
-    </>
-  )
-}
- */
-
-function FragmentInfoPagination({
-  fragmentInfos,
-  totalCount,
-  fragmentariumSearch,
-  wordService,
-}: {
-  fragmentInfos: readonly FragmentInfo[]
-  totalCount: number
-  fragmentariumSearch: any
-  wordService: WordService
-}): JSX.Element {
-  const [activePage, setActivePage] = useState(0)
-  const pages = useState(Math.ceil(totalCount / 100))
-  const [fragmentInfosSaved, setFragmentInfosSaved] = useState<any>([
-    { fragmentInfos: fragmentInfos, index: 0 },
-  ])
-  const items = [...Array(pages).keys()].map((_, index) => {
-    return (
-      <Pagination.Item
-        key={index}
-        active={index === activePage}
-        onClick={() => setActivePage(index)}
-      >
-        {index + 1}
-      </Pagination.Item>
-    )
-  })
-
-  const addFragmentInfoSaved = (
-    fragmentInfos: readonly FragmentInfo[],
-    activePage: number
-  ): void => {
-    const fragmentInfoExists = _.find(fragmentInfosSaved, { index: activePage })
-
-    fragmentInfoExists ||
-      setFragmentInfosSaved((old) => [
-        ...old,
-        { fragmentInfos: fragmentInfos, index: activePage },
-      ])
-  }
-
-  useEffect(() => {
-    fragmentariumSearch(1).then((fragmentInfos) =>
-      addFragmentInfoSaved(fragmentInfos.fragmentInfos, 1)
-    )
-  })
-
-  const FragmentInfosPage = ({ fragmentInfos, activeIndex }) => {
-    addFragmentInfoSaved(fragmentInfos, activeIndex)
-    return (
-      <>
-        {fragmentInfos.map((fragmentInfo, index) => (
-          <FragmentInfoDisplay
-            wordService={wordService}
-            key={index}
-            fragmentInfo={fragmentInfo}
-          />
-        ))}
-      </>
-    )
-  }
-
-  const FragmentInfosPageWithData = withData<any, any, any>(
-    ({ data, activeIndex }) => (
-      <FragmentInfosPage
-        fragmentInfos={data.fragmentInfos}
-        activeIndex={activeIndex}
-      />
-    ),
-    (props) =>
-      props.fragmentInfos
-        ? Bluebird.resolve(props.fragmentInfos)
-        : props.fragmentariumSearch(props.activeIndex),
-    {
-      watch: (props) => [props.activeIndex],
-    }
-  )
-
-  return (
-    <>
-      <FragmentInfosPageWithData
-        fragmentariumSearch={fragmentariumSearch}
-        fragmentInfos={fragmentInfosSaved.find(
-          (fragmentInfo) => fragmentInfo.index === activePage
-        )}
-        activePage={activePage}
-      />
-      <Col xs={{ offset: 5 }}>
-        <Pagination>{items}</Pagination>
-      </Col>
-    </>
-  )
-}
-
-export default withData<
-  {
-    number: string
-    transliteration: string
-    bibliographyId: string
-    pages: string
-    fragmentSearchService: FragmentSearchService
-    wordService: WordService
-  },
-  { fragmentSearchService: FragmentSearchService },
-  { fragmentInfos: readonly FragmentInfo[]; totalCount: number }
->(
-  ({
-    data,
-    number,
-    transliteration,
-    bibliographyId,
-    pages,
-    fragmentSearchService,
-    wordService,
-  }) => (
-    <FragmentInfoPagination
-      wordService={wordService}
-      fragmentInfos={data.fragmentInfos}
-      totalCount={data.totalCount}
-      fragmentariumSearch={(pagination: number) =>
-        fragmentSearchService.searchFragmentarium(
-          number,
-          transliteration,
-          bibliographyId,
-          pages,
-          pagination
+    <ul>
+      {genres.genres.map((genreItem) => {
+        const uncertain = genreItem.uncertain ? '(?)' : ''
+        return (
+          <ul key={genreItem.toString}>
+            <small>{`${genreItem.category.join(' ➝ ')} ${uncertain}`}</small>
+          </ul>
         )
-      }
-    />
-  ),
-  (props) =>
-    props.fragmentSearchService.searchFragmentarium(
-      props.number,
-      props.transliteration,
-      props.bibliographyId,
-      props.pages
-    ),
-  {
-    watch: (props) => [
-      props.number,
-      props.transliteration,
-      props.bibliographyId,
-      props.pages,
-    ],
-    filter: (props) =>
-      !_.isEmpty(props.number) ||
-      !_.isEmpty(props.transliteration) ||
-      !_.isEmpty(props.bibliographyId),
-    defaultData: { fragmentInfos: [], totalCount: 0 },
-  }
-)
+      })}
+    </ul>
+  )
+}
