b1c66f89adb985ec7785228a5f5fb915a41704c6
Ilya Khait <ilya.khait@lmu.de>
2022-05-27 14:32:27 +0000

    Fix more tests

--- src/corpus/ui/GotoButton.test.tsx
+++ src/corpus/ui/GotoButton.test.tsx
@@ -5,7 +5,6 @@ import userEvent from '@testing-library/user-event'
 import { Text } from 'corpus/domain/text'
 import { text } from 'test-support/test-corpus-text'
 import GotoButton from './GotoButton'
-import { Stages } from 'corpus/domain/period'
 
 const title = 'goto'
 
@@ -27,15 +26,11 @@ test('Multiple stages', async () => {
   await openDropdown(multiStageText)
 
   expectLink(
-    `${Stages[multiStageText.chapters[0].stage].abbreviation} ${
-      multiStageText.chapters[0].name
-    }`,
+    `${multiStageText.chapters[0].stage} ${multiStageText.chapters[0].name}`,
     '/corpus/L/1/1/OB/The Only Chapter'
   )
   expectLink(
-    `${Stages[multiStageText.chapters[1].stage].abbreviation} ${
-      multiStageText.chapters[1].name
-    }`,
+    `${multiStageText.chapters[1].stage} ${multiStageText.chapters[1].name}`,
     '/corpus/L/1/1/OA/The Only Chapter'
   )
   expectLink('Introduction', '/corpus/L/1/1')
