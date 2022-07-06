a3710d03b8398d851eef1ebef55705b565b0e474
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2022-07-06 16:51:03 +0200

    Extend wordinfo (#263)
    * add LineGroup class and context
    
    * add stateful alignments
    
    * add basic highlighting
    
    * extract Info component
    
    * add sentenceIndex property to BaseToken
    
    * refactor LineGroupContext
    
    * fix frequent popover refetching
    
    * store lemma for single popover
    
    * differentiate between display within vs. outside lineGroup
    
    * arrange aligned tokens in popover
    
    * fix type readonly Token[]
    
    * add LineLemmasContext
    
    * prepare fetching of all line lemmas
    
    * update Score to use lineGroup
    
    * store lemmas in a map; fix fetching data in Score
    
    * conditional data fetching in Score
    
    * force loading of variant lemmas
    
    * fix popover in fragmentarium (more precise checks for line group containment)
    
    * fix Glossary popovers
    
    * add LineLemmasContext to WordInfo-test
    
    * merge DisplayToken and DisplayLineGroupToken components
    
    * add isInPopover flag to suppress double popover
    
    * refactor .pushToken
    
    * extract functions into individual files, reduce nesting in WordInfo function
    
    * update snapshots
    
    * simplify prop specification
    
    * split WordInfo into popover/non-popover versions
    
    * flatten line accumulator conditions
    
    * Revert "flatten line accumulator conditions"
    
    This reverts commit 6aa55765db4492fde02387786475aa00e20ee896.
    
    * shorten pushToken method
    
    * extract transliteration columns
    
    * replace nested if/else with switch
    
    * Revert "replace nested if/else with switch"
    
    This reverts commit 5bc857e73f114cade1245539bfebec37f5250f83.
    
    * move token processing into LineAccumulator class
    
    * refactor addColumnToken
    
    * extract LineAccumulator class
    
    * move manuscript property checks into class
    
    * pass only necessary props; don't pass lineGroup as context in Score as it's available locally
    
    * remove unnecessary check
    
    * Remove separate lemmaKeys value from LineLemmasContext; add isLemma type guard; don't try to fetch ManuscriptLine lemmas
    
    * refactor const trigger
    
    * Revert "Remove separate lemmaKeys value from LineLemmasContext; add isLemma type guard; don't try to fetch ManuscriptLine lemmas"
    
    This reverts commit a39161ba84ab20323af7696483efaddc91740a6e.
    
    * add isLemma type guard
    
    * Discard extra lemmaKeys attribute in LineLemmasContext
    
    * refactoring, revert switch case to add column tokens in line accumulator
    
    * simplified switch case for addColumnToken method; replace empty <></> with null
    
    * fix LineLemmasContext
    
    * add LineGroup tests; remove unnecessary methods
    
    * add multiple tests for popover alignments
    
    * add condition to filter out undefined reconstruction
    
    * update snapshots
    
    * sort by (non-)variant
    
    * re-arrange popover info
    
    * remove extra reconstruction check
    
    * merge Score and ScoreWithData
    
    * merge Info and InfoWithData
    
    * merge AlignedTokens and AlignedTokensWithData
    
    * setSentenceIndices function is only applied to line variants
    
    * add comment

---
+++ src/test-support/word-info-fixtures.ts
@@ -0,0 +1,171 @@
+import { wordFactory } from 'test-support/word-fixtures'
+import { TextLine, TextLineDto } from 'transliteration/domain/text-line'
+import { Word } from 'transliteration/domain/token'
+import { alignedManuscriptToken } from './line-group-fixtures'
+import { Provenances } from 'corpus/domain/provenance'
+import { Periods } from 'corpus/domain/period'
+import { ManuscriptTypes } from 'corpus/domain/manuscript'
+
+export const dictionaryWord = wordFactory.homonymNotI().build()
+export const word: Word = {
+  enclosureType: [],
+  cleanValue: '|KUR.KUR|',
+  value: '|KUR.KUR|',
+  language: 'AKKADIAN',
+  normalized: false,
+  lemmatizable: true,
+  alignable: true,
+  uniqueLemma: [dictionaryWord._id],
+  erasure: 'NONE',
+  alignment: null,
+  variant: null,
+  parts: [
+    {
+      enclosureType: [],
+      cleanValue: '|KUR.KUR|',
+      value: '|KUR.KUR|',
+      type: 'CompoundGrapheme',
+    },
+  ],
+  type: 'Word',
+  hasVariantAlignment: false,
+}
+
+export const variantTokenDto: Word = {
+  value: 'u₂-šah-maṭ',
+  cleanValue: 'variant-u₂-šah-maṭ',
+  enclosureType: [],
+  erasure: 'NONE',
+  parts: [
+    {
+      value: 'u₂',
+      cleanValue: 'u₂',
+      enclosureType: [],
+      erasure: 'NONE',
+      name: 'u',
+      nameParts: [
+        {
+          value: 'u',
+          cleanValue: 'u',
+          enclosureType: [],
+          erasure: 'NONE',
+          type: 'ValueToken',
+        },
+      ],
+      subIndex: 2,
+      modifiers: [],
+      flags: [],
+      sign: null,
+      type: 'Reading',
+    },
+    {
+      value: '-',
+      cleanValue: '-',
+      enclosureType: [],
+      erasure: 'NONE',
+      type: 'Joiner',
+    },
+    {
+      value: 'šah',
+      cleanValue: 'šah',
+      enclosureType: [],
+      erasure: 'NONE',
+      name: 'šah',
+      nameParts: [
+        {
+          value: 'šah',
+          cleanValue: 'šah',
+          enclosureType: [],
+          erasure: 'NONE',
+          type: 'ValueToken',
+        },
+      ],
+      subIndex: 1,
+      modifiers: [],
+      flags: [],
+      sign: null,
+      type: 'Reading',
+    },
+    {
+      value: '-',
+      cleanValue: '-',
+      enclosureType: [],
+      erasure: 'NONE',
+      type: 'Joiner',
+    },
+    {
+      value: 'maṭ',
+      cleanValue: 'maṭ',
+      enclosureType: [],
+      erasure: 'NONE',
+      name: 'maṭ',
+      nameParts: [
+        {
+          value: 'maṭ',
+          cleanValue: 'maṭ',
+          enclosureType: [],
+          erasure: 'NONE',
+          type: 'ValueToken',
+        },
+      ],
+      subIndex: 1,
+      modifiers: [],
+      flags: [],
+      sign: null,
+      type: 'Reading',
+    },
+  ],
+  language: 'AKKADIAN',
+  normalized: false,
+  lemmatizable: true,
+  alignable: true,
+  uniqueLemma: ['hamāṭu I'],
+  alignment: 1,
+  variant: {
+    value: 'ušahmaṭ',
+    cleanValue: 'ušahmaṭ',
+    enclosureType: [],
+    erasure: 'NONE',
+    parts: [
+      {
+        value: 'ušahmaṭ',
+        cleanValue: 'ušahmaṭ',
+        enclosureType: [],
+        erasure: 'NONE',
+        type: 'ValueToken',
+      },
+    ],
+    language: 'AKKADIAN',
+    normalized: true,
+    lemmatizable: true,
+    alignable: true,
+    uniqueLemma: [],
+    alignment: null,
+    variant: null,
+    hasVariantAlignment: false,
+    modifiers: [],
+    type: 'AkkadianWord',
+  },
+  hasVariantAlignment: false,
+  type: 'Word',
+}
+
+export const alignedManuscriptLineDto: TextLineDto = {
+  type: 'TextLine',
+  lineNumber: {
+    number: 1,
+    hasPrime: false,
+    prefixModifier: null,
+    suffixModifier: null,
+  },
+  prefix: '',
+  content: [alignedManuscriptToken],
+}
+
+export const manuscriptLineDto = {
+  line: new TextLine(alignedManuscriptLineDto),
+  provenance: Provenances.Uruk,
+  period: Periods.Hellenistic,
+  type: ManuscriptTypes.Library,
+  siglumDisambiguator: '1',
+}
