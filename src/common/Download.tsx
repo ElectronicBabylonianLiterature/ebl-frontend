ec427827aff3cb6775840799ab3024d8386ea5c2
Ilya Khait <ilya.khait@lmu.de>
2022-07-11 15:29:36 +0200

    Export corpus editions (JSON) (#277)
    * Move download component to common
    
    * Refactor download, add component to corpus
    
    * Fix PDF export for empty transliteration
    
    * Implement download JSON for Chapter
    
    * Add test

---
+++ src/common/Download.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import { DropdownButton, ButtonGroup, Dropdown } from 'react-bootstrap'
+import _ from 'lodash'
+
+type Props = {
+  pdfDownloadButton: JSX.Element
+  wordDownloadButton: JSX.Element
+  baseFileName: string
+  atfUrl?: string
+  jsonUrl?: string
+  teiUrl?: string
+}
+
+export default function Download({
+  pdfDownloadButton,
+  wordDownloadButton,
+  baseFileName,
+  atfUrl,
+  jsonUrl,
+  teiUrl,
+}: Props): JSX.Element {
+  return (
+    <DropdownButton
+      key="downloadDropdown"
+      as={ButtonGroup}
+      aria-label="Download"
+      title={<i className="fas fa-file-download"></i>}
+      id={_.uniqueId('fragment-download-')}
+      variant="outline-primary"
+    >
+      {[wordDownloadButton, pdfDownloadButton]}
+      {atfUrl ? (
+        <Dropdown.Item
+          eventKey="3"
+          href={atfUrl}
+          download={`${baseFileName}.atf`}
+        >
+          Download as ATF
+        </Dropdown.Item>
+      ) : (
+        ''
+      )}
+      {jsonUrl ? (
+        <Dropdown.Item
+          eventKey="4"
+          href={jsonUrl}
+          download={`${baseFileName}.json`}
+        >
+          Download as JSON File
+        </Dropdown.Item>
+      ) : (
+        ''
+      )}
+      {teiUrl ? (
+        <Dropdown.Item
+          eventKey="5"
+          href={teiUrl}
+          download={`${baseFileName}.xml`}
+        >
+          Download as TEI XML File
+        </Dropdown.Item>
+      ) : (
+        ''
+      )}
+    </DropdownButton>
+  )
+}
