9f16ba98b42a855027d15b6b2ecc9a1b89cccd5f
Ilya Khait <ilya.khait@lmu.de>
2022-07-13 12:09:16 +0000

    Move `FragmentInCorpus` to `CuneiformFragment`

--- src/fragmentarium/ui/edition/Edition.tsx
+++ src/fragmentarium/ui/edition/Edition.tsx
@@ -6,14 +6,6 @@ import PioneersButton from 'fragmentarium/ui/PioneersButton'
 import './Edition.css'
 import TransliterationHeader from 'fragmentarium/ui/fragment/TransliterationHeader'
 import { Fragment } from 'fragmentarium/domain/fragment'
-import { ManuscriptAttestation } from 'corpus/domain/manuscriptAttestation'
-import Breadcrumbs, { TextCrumb } from 'common/Breadcrumbs'
-import GenreCrumb from 'corpus/ui/GenreCrumb'
-import CorpusTextCrumb from 'corpus/ui/CorpusTextCrumb'
-import ChapterCrumb from 'corpus/ui/ChapterCrumb'
-import withData from 'http/withData'
-
-import FragmentService from 'fragmentarium/application/FragmentService'
 import FragmentSearchService from 'fragmentarium/application/FragmentSearchService'
 
 type Props = {
@@ -22,21 +14,18 @@ type Props = {
     transliteration: string,
     notes: string
   ) => Bluebird<Fragment>
-  fragmentService: FragmentService
   fragmentSearchService: FragmentSearchService
   disabled: boolean
 }
 
 function Edition({
   fragment,
-  fragmentService,
   fragmentSearchService,
   updateTransliteration,
   disabled,
 }: Props): JSX.Element {
   return (
     <>
-      <FragmentInCorpus fragment={fragment} fragmentService={fragmentService} />
       <TransliterationHeader fragment={fragment} />
       <TransliteratioForm
         transliteration={fragment.atf}
@@ -52,60 +41,6 @@ function Edition({
 }
 Edition.defaultProps = {
   disabled: false,
-}
-
-const FragmentInCorpus = withData<
-  {
-    fragment: Fragment
-  },
-  { fragmentService },
-  Array<ManuscriptAttestation>
->(
-  ({ data }): JSX.Element => (
-    <FragmentInCorpusDisplay manuscriptAttestations={data} />
-  ),
-  (props) => props.fragmentService.findInCorpus(props.fragment.number)
-)
-
-function FragmentInCorpusDisplay({
-  manuscriptAttestations,
-}: {
-  manuscriptAttestations: ManuscriptAttestation[]
-}): JSX.Element {
-  return (
-    <>
-      {manuscriptAttestations.length > 0 && (
-        <>
-          <p>Edited in Corpus:</p>
-          <div>
-            {manuscriptAttestations.map((manuscriptAttestation, index) => {
-              return (
-                <Breadcrumbs
-                  key={index}
-                  className="manuscript_chapter__breadcrumbs"
-                  crumbs={[
-                    new GenreCrumb(manuscriptAttestation.text.genre, false),
-                    new CorpusTextCrumb(
-                      manuscriptAttestation.chapterId.textId,
-                      manuscriptAttestation.text.name,
-                      false
-                    ),
-                    new ChapterCrumb(
-                      manuscriptAttestation.chapterId,
-                      false,
-                      true
-                    ),
-                    new TextCrumb(manuscriptAttestation.manuscriptSiglum),
-                  ]}
-                  hasFullPath={false}
-                />
-              )
-            })}
-          </div>
-        </>
-      )}
-    </>
-  )
 }
 
 export default Edition
