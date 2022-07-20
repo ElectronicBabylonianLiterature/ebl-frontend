c32ff1a09ec1a4a59a44b16658d2f2b382f87970
Ilya Khait <ilya.khait@lmu.de>
2022-07-20 15:13:21 +0000

    Update tests

--- src/corpus/ui/Download.test.tsx
+++ src/corpus/ui/Download.test.tsx
@@ -11,11 +11,14 @@ import { ChapterDisplay } from 'corpus/domain/chapter'
 import { chapterDisplayFactory } from 'test-support/chapter-fixtures'
 
 const jsonUrl = 'JSON URL mock'
+const atfUrl = 'ATF URL mock'
 let chapter: ChapterDisplay
 let element: RenderResult
 
 beforeEach(async () => {
-  ;(URL.createObjectURL as jest.Mock).mockReturnValueOnce(jsonUrl)
+  ;(URL.createObjectURL as jest.Mock)
+    .mockReturnValueOnce(jsonUrl)
+    .mockReturnValueOnce(atfUrl)
 
   chapter = chapterDisplayFactory.build()
   await act(async () => {
@@ -26,26 +29,27 @@ beforeEach(async () => {
   })
 })
 
-describe.each([['Download as JSON File', 'json', jsonUrl]])(
-  '%s download link',
-  (name, type, url) => {
-    test('href', () => {
-      expect(screen.getByRole('link', { name: `${name}` })).toHaveAttribute(
-        'href',
-        url
-      )
-    })
+describe.each([
+  ['Download as JSON File', 'json', jsonUrl],
+  ['Download as ATF', 'atf', atfUrl],
+])('%s download link', (name, type, url) => {
+  test('href', () => {
+    expect(screen.getByRole('link', { name: `${name}` })).toHaveAttribute(
+      'href',
+      url
+    )
+  })
 
-    test('download', () => {
-      expect(screen.getByRole('link', { name: `${name}` })).toHaveAttribute(
-        'download',
-        `${chapter.uniqueIdentifier}.${type}`
-      )
-    })
-  }
-)
+  test('download', () => {
+    expect(screen.getByRole('link', { name: `${name}` })).toHaveAttribute(
+      'download',
+      `${chapter.uniqueIdentifier}.${type}`
+    )
+  })
+})
 
 test('Revoke object URLs on unmount', () => {
   element.unmount()
   expect(URL.revokeObjectURL).toHaveBeenCalledWith(jsonUrl)
+  expect(URL.revokeObjectURL).toHaveBeenCalledWith(atfUrl)
 })
