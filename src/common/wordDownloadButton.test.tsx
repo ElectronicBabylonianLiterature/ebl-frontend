d7d4cdc1840d1ec3823f8a3d9c1d43022a7625fc
Ilya Khait <ilya.khait@lmu.de>
2022-08-20 17:42:37 +0000

    Update `WordDownloadButton` test

--- src/common/wordDownloadButton.test.tsx
+++ src/common/wordDownloadButton.test.tsx
@@ -7,16 +7,17 @@ jest.mock('corpus/ui/Download')
 const contextMocked = CorpusWordExportContext as jest.Mock<
   jest.Mocked<CorpusWordExportContext>
 >
-it('Do Children render', () => {
-  const children = <span>Download as Word</span>
-  render(
-    <WordDownloadButton
-      context={new contextMocked()}
-      baseFileName={'filename'}
-      getWordDoc={jest.fn()}
-    >
-      {children}
-    </WordDownloadButton>
-  )
+const children = <span>Download as Word</span>
+render(
+  <WordDownloadButton
+    context={new contextMocked()}
+    baseFileName={'filename'}
+    getWordDoc={jest.fn()}
+  >
+    {children}
+  </WordDownloadButton>
+)
+
+it('Shows children', () => {
   expect(screen.getByText('Download as Word')).toBeInTheDocument()
 })
