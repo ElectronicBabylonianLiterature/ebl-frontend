dd772e047347d58eb9a01540cb06386cd377f220
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2022-10-02 19:52:33 +0000

    remove colSpan parameter

--- src/transliteration/ui/markup.tsx
+++ src/transliteration/ui/markup.tsx
@@ -57,16 +57,14 @@ export default function Markup({
   parts,
   container = 'div',
   className,
-  colSpan,
 }: {
   parts: readonly MarkupPart[]
   container?: string
   className?: string
-  colSpan?: number
 }): JSX.Element {
   return React.createElement(
     container,
-    { className, colSpan },
+    { className },
     parts.map((part: MarkupPart, index: number) => {
       if (isLanguagePart(part)) {
         return <DisplayLaguagePart key={index} part={part} />
