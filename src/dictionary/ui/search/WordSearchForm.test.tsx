f9a33c127fe99f856874aabdf5a5d5156feb3e8f
Ilya Khait <ilya.khait@lmu.de>
2022-12-07 12:08:36 +0000

    Update `WordSearchForm` & test

--- src/dictionary/ui/search/WordSearchForm.test.tsx
+++ src/dictionary/ui/search/WordSearchForm.test.tsx
@@ -4,7 +4,7 @@ import { createMemoryHistory } from 'history'
 import { render, screen } from '@testing-library/react'
 import { changeValueByLabel, submitForm } from 'test-support/utils'
 
-import WordSearchForm from './WordSearchForm'
+import WordSearchForm from 'dictionary/ui/search/WordSearchForm'
 
 it('Adds lemma to query string on submit', async () => {
   const history = createMemoryHistory()
@@ -15,8 +15,13 @@ it('Adds lemma to query string on submit', async () => {
     </Router>
   )
 
-  changeValueByLabel(screen, 'Query', 'lemma')
+  changeValueByLabel(screen, 'Word', 'lemma')
+  changeValueByLabel(screen, 'Meaning', 'some meaning')
+  changeValueByLabel(screen, 'Root', 'lmm')
+  changeValueByLabel(screen, 'Vowel class', 'a/a')
   await submitForm(container)
 
-  expect(history.push).toBeCalledWith('?query=lemma')
+  expect(history.push).toBeCalledWith(
+    '?meaning=some%20meaning&root=lmm&vowelClass=a%2Fa&word=lemma'
+  )
 })
--- src/dictionary/ui/search/WordSearchForm.tsx
+++ src/dictionary/ui/search/WordSearchForm.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { stringify } from 'query-string'
 import { Form, FormControl, Button, Row, Col } from 'react-bootstrap'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
-import _ from 'lodash'
 
 type Props = {
   query: string[] | string | null | undefined
@@ -10,45 +9,86 @@ type Props = {
   location
   match
 } & RouteComponentProps
-type State = { query: string }
+type State = {
+  query: { word: string; meaning: string; root: string; vowelClass: string }
+}
 
 class WordSearch extends Component<Props, State> {
   state = {
-    query: _.isArray(this.props.query)
-      ? this.props.query.join(' ')
-      : this.props.query || '',
+    query: { word: '', meaning: '', root: '', vowelClass: '' },
   }
 
   onChange = (event) => {
+    const { id, value } = event.target
     this.setState({
-      query: event.target.value,
+      query: { ...this.state.query, [id]: value },
     })
+    console.log(this.state.query)
   }
 
   submit = (event) => {
     event.preventDefault()
-    this.props.history.push(`?${stringify({ query: this.state.query })}`)
+    this.props.history.push(`?${stringify(this.state.query)}`)
   }
 
   render() {
     return (
       <Form onSubmit={this.submit}>
-        <Form.Group as={Row} controlId="query">
-          <Form.Label column sm={2}>
-            Query
+        <Form.Group as={Row} controlId="word">
+          <Form.Label column sm={3}>
+            Word
           </Form.Label>
           <Col sm={6}>
             <FormControl
               type="text"
-              value={this.state.query}
-              placeholder="lemma or meaning"
+              value={this.state.query.word}
+              placeholder="word"
               onChange={this.onChange}
             />
           </Col>
-          <Col sm={4}>
+          <Col sm={2}>
             <Button type="submit" variant="primary">
               Query
             </Button>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="meaning">
+          <Form.Label column sm={3}>
+            Meaning
+          </Form.Label>
+          <Col sm={6}>
+            <FormControl
+              type="text"
+              value={this.state.query.meaning}
+              placeholder="meaning"
+              onChange={this.onChange}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="root">
+          <Form.Label column sm={3}>
+            Root
+          </Form.Label>
+          <Col sm={6}>
+            <FormControl
+              type="text"
+              value={this.state.query.root}
+              placeholder="root"
+              onChange={this.onChange}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="vowelClass">
+          <Form.Label column sm={3}>
+            Vowel class
+          </Form.Label>
+          <Col sm={6}>
+            <FormControl
+              type="text"
+              value={this.state.query.vowelClass}
+              placeholder="vowel class (verbs)"
+              onChange={this.onChange}
+            />
           </Col>
         </Form.Group>
       </Form>
