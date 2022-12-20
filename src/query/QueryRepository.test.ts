cd161964ac4a83a738b8f1d6ebb7d61aaa4aff4a
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2022-12-20 12:38:15 +0000

    refactoring

--- src/query/QueryRepository.test.ts
+++ src/query/QueryRepository.test.ts
@@ -10,47 +10,24 @@ const queryRepository = new ApiQueryRepository(apiClient)
 const lemmas = 'foo I+bar II'
 const queryResult: QueryResult = { items: [], matchCountTotal: 0 }
 
-const testData: TestData<ApiQueryRepository>[] = [
-  new TestData(
-    'query',
-    [{ lemma: 'foo I' }],
-    apiClient.fetchJson,
-    queryResult,
-    [`/fragments/query?lemma=${encodeURIComponent('foo I')}`, true],
-    Promise.resolve(queryResult)
-  ),
-  new TestData(
-    'query',
-    [{ and: lemmas }],
-    apiClient.fetchJson,
-    queryResult,
-    [`/fragments/query?and=${encodeURIComponent(lemmas)}`, true],
-    Promise.resolve(queryResult)
-  ),
-  new TestData(
-    'query',
-    [{ or: lemmas }],
-    apiClient.fetchJson,
-    queryResult,
-    [`/fragments/query?or=${encodeURIComponent(lemmas)}`, true],
-    Promise.resolve(queryResult)
-  ),
-  new TestData(
-    'query',
-    [{ line: lemmas }],
-    apiClient.fetchJson,
-    queryResult,
-    [`/fragments/query?line=${encodeURIComponent(lemmas)}`, true],
-    Promise.resolve(queryResult)
-  ),
-  new TestData(
-    'query',
-    [{ phrase: lemmas }],
-    apiClient.fetchJson,
-    queryResult,
-    [`/fragments/query?phrase=${encodeURIComponent(lemmas)}`, true],
-    Promise.resolve(queryResult)
-  ),
+const testValues = [
+  { queryOperator: 'lemma', value: 'foo I' },
+  { queryOperator: 'and', value: lemmas },
+  { queryOperator: 'or', value: lemmas },
+  { queryOperator: 'line', value: lemmas },
+  { queryOperator: 'phrase', value: lemmas },
 ]
+
+const testData: TestData<ApiQueryRepository>[] = testValues.map(
+  ({ queryOperator, value }) =>
+    new TestData(
+      'query',
+      [{ [queryOperator]: value }],
+      apiClient.fetchJson,
+      queryResult,
+      [`/fragments/query?${queryOperator}=${encodeURIComponent(value)}`, true],
+      Promise.resolve(queryResult)
+    )
+)
 
 describe('ApiQueryRepository', () => testDelegation(queryRepository, testData))
