2dc3bd14c8c334fc048904438c8f1721eaff40b3
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2022-12-22 11:28:42 +0000

    have updateScript return Promise

--- src/fragmentarium/ui/info/Info.tsx
+++ src/fragmentarium/ui/info/Info.tsx
@@ -24,7 +24,7 @@ export default function Info({
   const updateGenres = (genres) =>
     onSave(fragmentService.updateGenres(fragment.number, genres))
   const updateScript = (script) =>
-    onSave(fragmentService.updateScript(fragment.number, script))
+    fragmentService.updateScript(fragment.number, script)
 
   return (
     <>
