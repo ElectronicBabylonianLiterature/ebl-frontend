80a9860d18179b2513d04788cb6b04d1754fb1db
Ilya Khait <ilya.khait@lmu.de>
2023-01-23 20:40:13 +0000

    Fix after merge

--- src/markup/ui/markup.tsx
+++ src/markup/ui/markup.tsx
@@ -8,7 +8,7 @@ import _ from 'lodash'
 
 type ParagraphMarkupPart = MarkupPart & { paragraph: number }
 
-function createParagraphs(
+export function createParagraphs(
   parts: readonly MarkupPart[]
 ): ParagraphMarkupPart[][] {
   const paragraphs: ParagraphMarkupPart[] = []
