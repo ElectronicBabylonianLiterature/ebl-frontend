9df90b6d079741f58ba720c1383d1d64f6214c50
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-02-27 15:19:47 +0000

    add test for line number anchor

--- src/corpus/ui/LineNumber.test.tsx
+++ src/corpus/ui/LineNumber.test.tsx
@@ -4,6 +4,9 @@ import { lineDisplayFactory } from 'test-support/chapter-fixtures'
 import lineNumberToString from 'transliteration/domain/lineNumberToString'
 import LineNumber from './LineNumber'
 import { oldLineNumberFactory } from 'test-support/linenumber-factory'
+import userEvent from '@testing-library/user-event'
+
+window.HTMLElement.prototype.scrollIntoView = jest.fn()
 
 const LINE_NUMBER = '76a'
 
@@ -20,13 +23,31 @@ const lineDisplay = lineDisplayFactory.build(
     },
   }
 )
+const lineNumberString = lineNumberToString(lineDisplay.number)
+
+function renderLineNumber(): void {
+  render(
+    <table>
+      <tbody>
+        <tr>
+          <LineNumber
+            line={lineDisplay}
+            activeLine={''}
+            showOldLineNumbers={true}
+          />
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 test('LineNumber', () => {
-  render(
-    <LineNumber line={lineDisplay} activeLine={''} showOldLineNumbers={true} />
-  )
-  expect(
-    screen.getByText(`${lineNumberToString(lineDisplay.number)}`)
-  ).toBeVisible()
+  renderLineNumber()
+  expect(screen.getByText(lineNumberString)).toBeVisible()
   expect(screen.getByText(LINE_NUMBER)).toBeVisible()
+})
+test('Clicking on line number sets anchor', () => {
+  renderLineNumber()
+  userEvent.click(screen.getByText(lineNumberString))
+  expect(global.window.location.hash).toEqual(`#${lineNumberString}`)
 })
