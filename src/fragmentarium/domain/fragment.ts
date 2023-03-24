9d920a0f0b4b35a0601adca6676573b2fb413cc5
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-03-24 14:23:53 +0000

    add urOnlineNumber method

--- src/fragmentarium/domain/fragment.ts
+++ src/fragmentarium/domain/fragment.ts
@@ -265,6 +265,9 @@ export class Fragment {
   get bdtnsNumber(): string {
     return this.getExternalNumber('bdtnsNumber')
   }
+  get urOnlineNumber(): string {
+    return this.getExternalNumber('urOnlineNumber')
+  }
 
   get atfHeading(): string {
     const cdliNumber = this.cdliNumber || 'X000001'
