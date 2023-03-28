84d3b15eb736fc7cd35f760d868532d129f1bdff
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-03-28 10:07:15 +0000

    Refactoring

--- src/fragmentarium/ui/info/OrganizationLinks.tsx
+++ src/fragmentarium/ui/info/OrganizationLinks.tsx
@@ -13,6 +13,8 @@ import {
   UrOnlineLogoLink,
 } from './ExternalNumberLogoLink'
 
+type LogoLinkComponent = ({ number }: { number: string }) => JSX.Element
+
 function MuseumLink({ link }: { readonly link: FragmentLink }): JSX.Element {
   return (
     <ExternalLink href={link.url} aria-label={link.label}>
@@ -30,18 +32,18 @@ export default function OrganizationLinks({
 }: {
   readonly fragment: Fragment
 }): JSX.Element {
-  const externalNumbers = [
-    { number: 'cdliNumber', LogoLink: CdliLogoLink },
-    { number: 'bdtnsNumber', LogoLink: BdtnsLogoLink },
-    { number: 'archibabNumber', LogoLink: ArchibabLogoLink },
-    { number: 'urOnlineNumber', LogoLink: UrOnlineLogoLink },
+  const externalNumbers: ReadonlyArray<[string, LogoLinkComponent]> = [
+    [fragment.cdliNumber, CdliLogoLink],
+    [fragment.bdtnsNumber, BdtnsLogoLink],
+    [fragment.archibabNumber, ArchibabLogoLink],
+    [fragment.urOnlineNumber, UrOnlineLogoLink],
   ]
   return (
     <p className="OrganizationLinks">
       {fragment.hasLink && <MuseumLink link={fragment.getLink()} />}
       {externalNumbers.map(
-        ({ number, LogoLink }) =>
-          fragment[number] && <LogoLink number={fragment[number]} />
+        ([number, LogoLink], index) =>
+          number && <LogoLink number={number} key={index} />
       )}
     </p>
   )
