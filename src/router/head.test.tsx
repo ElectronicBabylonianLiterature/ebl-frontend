c96f5e3af3958f888f41677c798c404ba4161fa8
Ilya Khait <ilya.khait@lmu.de>
2023-05-14 20:43:12 +0000

    Add test

--- src/router/head.test.tsx
+++ src/router/head.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { HelmetProvider } from 'react-helmet-async'
-import { helmetContext, HeadTags } from 'router/head' //HeadTagsService
+import { helmetContext, HeadTags, HeadTagsService } from 'router/head'
 
 HelmetProvider.canUseDOM = false
 
@@ -9,6 +9,20 @@ it('should render all metadata', () => {
   render(
     <HelmetProvider context={helmetContext}>
       <HeadTags title={'title'} description={''} />
+    </HelmetProvider>
+  )
+  expect(helmetContext['helmet']['title']['toString']()).toEqual(
+    '<title data-rh="true">title</title>'
+  )
+  expect(helmetContext['helmet']['meta']['toString']()).toEqual(
+    '<meta data-rh="true" name="description" content=""/><meta data-rh="true" property="og:title" content="title"/><meta data-rh="true" property="og:description" content=""/><meta data-rh="true" name="twitter:title" content="title"/><meta data-rh="true" name="twitter:description" content=""/>'
+  )
+})
+
+it('should render all metadata', () => {
+  render(
+    <HelmetProvider context={helmetContext}>
+      <HeadTagsService title={'title'} description={''}></HeadTagsService>
     </HelmetProvider>
   )
   expect(helmetContext['helmet']['title']['toString']()).toEqual(
