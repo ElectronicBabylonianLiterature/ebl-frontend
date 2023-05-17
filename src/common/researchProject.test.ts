fda3549a28b7355a192b223229b018747d5ab05b
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-05-17 16:32:21 +0000

    Add completeness test for ResearchProject

---
+++ src/common/researchProject.test.ts
@@ -0,0 +1,4 @@
+import { testContainsAllValues } from 'test-support/test-values-complete'
+import { ResearchProjects, researchProjects } from './researchProject'
+
+testContainsAllValues(ResearchProjects, researchProjects, 'periods')
