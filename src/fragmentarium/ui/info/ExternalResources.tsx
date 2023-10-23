df1b9ba4267f11b7fdd56f338d1660892bd1e5ab
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-10-23 15:52:18 +0000

    add BmIdLink, YaleBabylonianLink

--- src/fragmentarium/ui/info/ExternalResources.tsx
+++ src/fragmentarium/ui/info/ExternalResources.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-
-import ExternalLink from 'common/ExternalLink'
-
 import { Fragment } from 'fragmentarium/domain/fragment'
-import { FragmentLink } from 'fragmentarium/domain/museum'
 
 import './ExternalResources.sass'
 import {
@@ -13,21 +9,11 @@ import {
   UrOnlineLink,
   HilprechtJenaLink,
   HilprechtHeidelbergLink,
+  BmIdLink,
+  YaleBabylonianLink,
 } from './ExternalNumberLink'
 
 type ExternalLinkComponent = ({ number }: { number: string }) => JSX.Element
-
-function MuseumLink({ link }: { readonly link: FragmentLink }): JSX.Element {
-  return (
-    <ExternalLink href={link.url} aria-label={link.label}>
-      <img
-        className="ExternalResources__image"
-        src={link.logo}
-        alt={link.name}
-      />
-    </ExternalLink>
-  )
-}
 
 export default function ExternalResources({
   fragment,
@@ -35,16 +21,17 @@ export default function ExternalResources({
   readonly fragment: Fragment
 }): JSX.Element {
   const externalNumbers: ReadonlyArray<[string, ExternalLinkComponent]> = [
+    [fragment.bmIdNumber, BmIdLink],
     [fragment.cdliNumber, CdliLink],
     [fragment.bdtnsNumber, BdtnsLink],
     [fragment.archibabNumber, ArchibabLink],
     [fragment.urOnlineNumber, UrOnlineLink],
     [fragment.hilprechtJenaNumber, HilprechtJenaLink],
     [fragment.hilprechtHeidelbergNumber, HilprechtHeidelbergLink],
+    [fragment.accession, YaleBabylonianLink],
   ]
   return (
     <ul className="ExternalResources__items">
-      {fragment.hasLink && <MuseumLink link={fragment.getLink()} />}
       {externalNumbers.map(
         ([number, LinkComponent], index) =>
           number && (
