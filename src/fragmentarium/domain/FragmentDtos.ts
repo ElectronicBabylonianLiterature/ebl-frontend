07d6fd383dcbfbfa22d46103892a7d8552fb4b17
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-10-26 13:59:58 +0000

    add metropolitanNumber; refactor

--- src/fragmentarium/domain/FragmentDtos.ts
+++ src/fragmentarium/domain/FragmentDtos.ts
@@ -55,14 +55,20 @@ type RecordEntryDto = Pick<RecordEntry, 'user' | 'date' | 'type'>
 
 type FolioDto = Pick<Folio, 'name' | 'number'>
 
-export interface ExternalNumbers {
-  cdliNumber: string
-  bmIdNumber: string
-  archibabNumber: string
-  bdtnsNumber: string
-  urOnlineNumber: string
-  hilprechtJenaNumber: string
-  hilprechtHeidelbergNumber: string
+export const ExternalNumberTypes = [
+  'cdliNumber',
+  'bmIdNumber',
+  'archibabNumber',
+  'bdtnsNumber',
+  'urOnlineNumber',
+  'hilprechtJenaNumber',
+  'hilprechtHeidelbergNumber',
+  'metropolitanNumber',
+] as const
+export type ExternalNumber = typeof ExternalNumberTypes[number]
+
+export type ExternalNumbers = {
+  [K in ExternalNumber]: string
 }
 
 export default interface FragmentDto {
--- src/fragmentarium/domain/fragment.ts
+++ src/fragmentarium/domain/fragment.ts
@@ -10,7 +10,11 @@ import { Joins } from './join'
 import { MarkupPart } from 'transliteration/domain/markup'
 import { Period, PeriodModifier } from 'common/period'
 import { Session } from 'auth/Session'
-import { ExternalNumbers } from './FragmentDtos'
+import {
+  ExternalNumber,
+  ExternalNumbers,
+  ExternalNumberTypes,
+} from './FragmentDtos'
 import { RecordEntry } from './RecordEntry'
 import { ResearchProject } from 'research-projects/researchProject'
 import { MesopotamianDate } from 'fragmentarium/domain/Date'
@@ -227,7 +231,7 @@ export class Fragment {
     })
   }
 
-  private getExternalNumber(numberType: keyof ExternalNumbers): string {
+  private getExternalNumber(numberType: ExternalNumber): string {
     return this.externalNumbers[numberType]
   }
 
@@ -252,20 +256,16 @@ export class Fragment {
   get hilprechtHeidelbergNumber(): string {
     return this.getExternalNumber('hilprechtHeidelbergNumber')
   }
+  get metropolitanNumber(): string {
+    return this.getExternalNumber('metropolitanNumber')
+  }
   get yaleBabylonianNumber(): string {
     return this.accession
   }
   get hasExternalResources(): boolean {
-    return _.some([
-      this.cdliNumber,
-      this.bmIdNumber,
-      this.archibabNumber,
-      this.bdtnsNumber,
-      this.urOnlineNumber,
-      this.hilprechtJenaNumber,
-      this.hilprechtHeidelbergNumber,
-      this.yaleBabylonianNumber,
-    ])
+    return _.some(
+      ExternalNumberTypes.map((number) => this.getExternalNumber(number))
+    )
   }
 
   get atfHeading(): string {
