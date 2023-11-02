8c890690d01826f8b2aff23c58593773e0837cc4
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-11-02 12:36:33 +0000

    prepare findspot class display

--- src/fragmentarium/domain/archaeology.ts
+++ src/fragmentarium/domain/archaeology.ts
@@ -3,6 +3,7 @@ import MuseumNumber, { museumNumberToString } from './MuseumNumber'
 import { Provenances } from 'corpus/domain/provenance'
 import _ from 'lodash'
 import { DateRange } from './Date'
+import { immerable } from 'immer'
 
 export const excavationSites = {
   ..._.omit(Provenances, 'Standard Text'),
@@ -28,31 +29,50 @@ export interface ExcavationPlan {
   readonly references: readonly Reference[]
 }
 
-export interface Findspot {
-  readonly site?: ExcavationSite
-  readonly area?: string
-  readonly building?: string
-  readonly buildingType?: BuildingType
-  readonly lavelLayerPhase?: string
-  readonly dateRange?: DateRange & { notes?: string }
-  readonly plans?: readonly ExcavationPlan[]
-  readonly room?: string
-  readonly context?: string
-  readonly primaryContext?: boolean | null
-  readonly notes?: string
+export class Findspot {
+  readonly [immerable] = true
+
+  constructor(
+    readonly site: ExcavationSite = excavationSites[''],
+    readonly area: string = '',
+    readonly building: string = '',
+    readonly buildingType: BuildingType = 'UNKNOWN',
+    readonly levelLayerPhase: string = '',
+    readonly dateRange: (DateRange & { notes?: string }) | null = null,
+    readonly plans: readonly ExcavationPlan[] = [],
+    readonly room: string = '',
+    readonly context: string = '',
+    readonly primaryContext: boolean | null = null,
+    readonly notes: string = ''
+  ) {}
+
+  get buildingTypeValue(): string {
+    return this.buildingType === 'NOT_IN_BUILDING'
+      ? 'Not in building'
+      : _.capitalize(this.buildingType)
+  }
+
+  toString(): string {
+    const area = this.area ? `${this.area} > ` : ''
+    console.log(
+      `${area}${this.building} (${this.buildingTypeValue}), ${this.levelLayerPhase} ().`
+    )
+    return `${area}${this.building} (${this.buildingTypeValue}), ${this.levelLayerPhase} ().`
+  }
 }
 
 export interface Archaeology {
   readonly excavationNumber?: string
   readonly site?: ExcavationSite
   readonly isRegularExcavation?: boolean
-  readonly findspot?: Findspot
+  readonly findspot?: Findspot | null
 }
 
 export interface ArchaeologyDto {
   readonly excavationNumber?: string
   readonly site?: SiteKey
   readonly isRegularExcavation?: boolean
+  readonly findspot?: any
 }
 
 export function createArchaeology(
@@ -66,6 +86,21 @@ export function createArchaeology(
       ? museumNumberToString(dto.excavationNumber)
       : undefined,
     site: excavationSites[dto.site || ''],
+    findspot: _.isNull(dto.findspot)
+      ? null
+      : new Findspot(
+          dto.findspot.site,
+          dto.findspot.area,
+          dto.findspot.building,
+          dto.findspot.buildingType,
+          dto.findspot.levelLayerPhase,
+          dto.findspot.dateRange,
+          dto.findspot.plans,
+          dto.findspot.room,
+          dto.findspot.context,
+          dto.findspot.primaryContext,
+          dto.findspot.notes
+        ),
   }
 }
 
