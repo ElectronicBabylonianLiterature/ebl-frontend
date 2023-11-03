541cbf90a590cd861ec8633898a4ed82a00e5ab1
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-11-03 14:27:52 +0000

    add FindspotService

---
+++ src/fragmentarium/application/FindspotService.ts
@@ -0,0 +1,29 @@
+import Bluebird from 'bluebird'
+import { Findspot, fromFindspotDto } from 'fragmentarium/domain/archaeology'
+
+export interface FindspotRepository {
+  fetchFindspots(): Bluebird<Findspot[]>
+}
+
+export class ApiFindspotRepository implements FindspotRepository {
+  constructor(
+    private readonly apiClient: {
+      fetchJson: (url: string, authorize: boolean) => Bluebird<any>
+      postJson: (url: string, body: Record<string, unknown>) => Bluebird<any>
+    }
+  ) {}
+
+  fetchFindspots(): Bluebird<Findspot[]> {
+    return this.apiClient
+      .fetchJson('/findspots', false)
+      .then((findspots) => findspots.map(fromFindspotDto))
+  }
+}
+
+export class FindspotService {
+  constructor(private readonly findspotRepository: FindspotRepository) {}
+
+  fetchFindspots(): Bluebird<Findspot[]> {
+    return this.findspotRepository.fetchFindspots()
+  }
+}
