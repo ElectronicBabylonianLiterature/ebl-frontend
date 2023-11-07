5cce710256de4dffa4a272456666fdf7e902566a
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-11-07 13:41:10 +0000

    fix typo

--- src/fragmentarium/application/FindspotService.test.ts
+++ src/fragmentarium/application/FindspotService.test.ts
@@ -2,17 +2,17 @@ import { findspotFactory } from 'test-support/fragment-fixtures'
 import { FindspotService } from './FindspotService'
 import { testDelegation, TestData } from 'test-support/utils'
 
-const findspotRespository = {
+const findspotRepository = {
   fetchFindspots: jest.fn(),
 }
-const findspotService = new FindspotService(findspotRespository)
+const findspotService = new FindspotService(findspotRepository)
 const expectedFindspots = findspotFactory.buildList(3)
 
 const testData: TestData<FindspotService>[] = [
   new TestData(
     'fetchFindspots',
     [],
-    findspotRespository.fetchFindspots,
+    findspotRepository.fetchFindspots,
     expectedFindspots,
     null,
     Promise.resolve(expectedFindspots)
