d6c763cb2adc90ed331a621c0a85a2e5d28b1549
fsimonjetz <fabian.simonjetz@rub.de>
2023-11-23 17:37:26 +0100

    Add project search parameter (#411)
    * re-type ResearchProjects
    
    * add internal project filter
    
    * add project to FragmentQuery type
    
    * add types

--- src/query/FragmentQuery.ts
+++ src/query/FragmentQuery.ts
@@ -1,4 +1,5 @@
 import { Periods, PeriodModifiers } from 'common/period'
+import { ResearchProjects } from 'research-projects/researchProject'
 
 export const QueryTypes = ['and', 'or', 'line', 'phrase'] as const
 export type QueryType = typeof QueryTypes[number]
@@ -17,4 +18,5 @@ export type FragmentQuery = Partial<{
   scriptPeriod: PeriodString
   scriptPeriodModifier: PeriodModifierString
   genre: string
+  project: keyof typeof ResearchProjects | null
 }>
