4f6dfc1cd80e4955e4a1c8d888353468110e313c
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-11-27 13:00:19 +0000

    add SearchForm to CAIC subpage

--- src/projects/caic.tsx
+++ src/projects/caic.tsx
@@ -2,8 +2,31 @@ import React from 'react'
 import AppContent from 'common/AppContent'
 import { ResearchProjects } from 'research-projects/researchProject'
 import { TextCrumb } from 'common/Breadcrumbs'
+import SessionContext from 'auth/SessionContext'
+import { Session } from 'auth/Session'
+import { Container } from 'react-bootstrap'
+import SearchForm from 'fragmentarium/ui/SearchForm'
+import BibliographyService from 'bibliography/application/BibliographyService'
+import WordService from 'dictionary/application/WordService'
+import FragmentSearchService from 'fragmentarium/application/FragmentSearchService'
+import FragmentService from 'fragmentarium/application/FragmentService'
+import { FragmentQuery } from 'query/FragmentQuery'
 
-export default function CaicPage(): JSX.Element {
+interface Props {
+  fragmentService: FragmentService
+  fragmentSearchService: FragmentSearchService
+  bibliographyService: BibliographyService
+  fragmentQuery: FragmentQuery
+  wordService: WordService
+}
+
+export default function CaicPage({
+  fragmentService,
+  fragmentSearchService,
+  bibliographyService,
+  fragmentQuery,
+  wordService,
+}: Props): JSX.Element {
   return (
     <AppContent
       title={ResearchProjects.CAIC.name}
@@ -12,15 +35,30 @@ export default function CaicPage(): JSX.Element {
         new TextCrumb(ResearchProjects.CAIC.abbreviation),
       ]}
     >
-      <p>
-        The cuneiform artifacts of the Iraq Museum in Baghdad are a central part
-        of the cultural heritage of Mesopotamia, which is of great importance to
-        all of humanity. As an interdisciplinary academic project, the aim of
-        CAIC is to document, edit, and analyze approximately 17,000 cuneiform
-        tablets both from historical and linguistic perspectives. With the help
-        of the latest digital approaches it will also make them available online
-        to the general public and experts from various disciplines.
-      </p>
+      <SessionContext.Consumer>
+        {(session: Session): JSX.Element => (
+          <Container fluid>
+            <p>
+              The cuneiform artifacts of the Iraq Museum in Baghdad are a
+              central part of the cultural heritage of Mesopotamia, which is of
+              great importance to all of humanity. As an interdisciplinary
+              academic project, the aim of CAIC is to document, edit, and
+              analyze approximately 17,000 cuneiform tablets both from
+              historical and linguistic perspectives. With the help of the
+              latest digital approaches it will also make them available online
+              to the general public and experts from various disciplines.
+            </p>
+            <SearchForm
+              fragmentSearchService={fragmentSearchService}
+              fragmentService={fragmentService}
+              fragmentQuery={fragmentQuery}
+              wordService={wordService}
+              bibliographyService={bibliographyService}
+              project={'CAIC'}
+            />
+          </Container>
+        )}
+      </SessionContext.Consumer>
     </AppContent>
   )
 }
--- src/router/researchProjectRoutes.tsx
+++ src/router/researchProjectRoutes.tsx
@@ -1,26 +1,45 @@
 import React, { ReactNode } from 'react'
 import { Route } from 'react-router-dom'
+import { parse } from 'query-string'
 import { sitemapDefaults } from 'router/sitemap'
 import { HeadTagsService } from 'router/head'
 import { ResearchProjects } from 'research-projects/researchProject'
 import CaicPage from 'projects/caic'
+import BibliographyService from 'bibliography/application/BibliographyService'
+import WordService from 'dictionary/application/WordService'
+import FragmentSearchService from 'fragmentarium/application/FragmentSearchService'
+import FragmentService from 'fragmentarium/application/FragmentService'
 
 export default function ResearchProjectRoutes({
   sitemap,
+  fragmentService,
+  fragmentSearchService,
+  wordService,
+  bibliographyService,
 }: {
   sitemap: boolean
+  fragmentService: FragmentService
+  fragmentSearchService: FragmentSearchService
+  wordService: WordService
+  bibliographyService: BibliographyService
 }): JSX.Element[] {
   return [
     <Route
       key="caic-project"
       exact
       path={`/projects/${ResearchProjects.CAIC.abbreviation}`}
-      render={(): ReactNode => (
+      render={({ location }): ReactNode => (
         <HeadTagsService
           title={`${ResearchProjects.CAIC.abbreviation} in eBL`}
           description={ResearchProjects.CAIC.name}
         >
-          <CaicPage />
+          <CaicPage
+            fragmentService={fragmentService}
+            fragmentSearchService={fragmentSearchService}
+            fragmentQuery={parse(location.search)}
+            wordService={wordService}
+            bibliographyService={bibliographyService}
+          />
         </HeadTagsService>
       )}
       {...(sitemap && sitemapDefaults)}
