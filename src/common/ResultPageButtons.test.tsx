65cde6e96d1d67e24777ba0ad59ad308fbba591a
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2023-12-04 15:36:21 +0000

    refactoring

--- src/common/ResultPageButtons.test.tsx
+++ src/common/ResultPageButtons.test.tsx
@@ -2,31 +2,29 @@ import React from 'react'
 import { render, screen, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { ResultPageButtons } from './ResultPageButtons'
-import { QueryItem } from 'query/QueryResult'
 import { queryItemFactory } from 'test-support/query-item-factory'
 
 const setActive = jest.fn()
-let numberOfPages: number
-let pages: QueryItem[][]
+
+async function renderPages(numberOfPages: number, active = 0) {
+  const pages = Array.from({ length: numberOfPages }, () =>
+    queryItemFactory.buildList(3)
+  )
+  await act(async () => {
+    render(
+      <ResultPageButtons pages={pages} active={active} setActive={setActive} />
+    )
+  })
+}
 
 describe('ResultPageButtons for few pages', () => {
-  beforeEach(async () => {
-    numberOfPages = 3
-    pages = Array.from({ length: numberOfPages }, () =>
-      queryItemFactory.buildList(3)
-    )
-    await act(async () => {
-      render(
-        <ResultPageButtons pages={pages} active={0} setActive={setActive} />
-      )
-    })
-  })
+  beforeEach(() => renderPages(3))
 
   it('renders pagination buttons correctly', () => {
     const pagination = screen.getByLabelText('result-pagination')
     expect(pagination).toBeInTheDocument()
     expect(screen.getByText('1')).toBeVisible()
-    expect(screen.getByText(`${numberOfPages}`)).toBeVisible()
+    expect(screen.getByText('3')).toBeVisible()
     expect(screen.queryByText('…')).not.toBeInTheDocument()
   })
 
@@ -39,31 +37,14 @@ describe('ResultPageButtons for few pages', () => {
 })
 
 describe('ResultPageButtons for many pages', () => {
-  beforeEach(async () => {
-    numberOfPages = 15
-    pages = Array.from({ length: numberOfPages }, () =>
-      queryItemFactory.buildList(3)
-    )
-    await act(async () => {
-      render(
-        <ResultPageButtons pages={pages} active={3} setActive={setActive} />
-      )
-    })
-  })
+  beforeEach(() => renderPages(15, 4))
 
   it('renders pagination buttons correctly', () => {
     const pagination = screen.getByLabelText('result-pagination')
     expect(pagination).toBeInTheDocument()
     expect(screen.getByText('1')).toBeVisible()
-    expect(screen.getByText(`${numberOfPages}`)).toBeVisible()
+    expect(screen.getByText('15')).toBeVisible()
     expect(screen.getByText('…')).toBeVisible()
-  })
-
-  it('triggers setActive when clicking on pagination buttons', () => {
-    const paginationItem = screen.getByText('1')
-    userEvent.click(paginationItem)
-
-    expect(setActive).toHaveBeenCalledWith(0)
   })
 
   it('shows ellipsis when page 7 is active', () => {
