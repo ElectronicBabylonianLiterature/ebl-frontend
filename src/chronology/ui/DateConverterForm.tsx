d04d68bcebc7f7039babff7731e9909bb6dfd176
Ilya Khait <ilya.khait@lmu.de>
2023-12-12 19:29:55 +0000

    Refactor & improve style

--- src/chronology/ui/DateConverterForm.tsx
+++ src/chronology/ui/DateConverterForm.tsx
@@ -1,25 +1,15 @@
 import React, { useState } from 'react'
-import {
-  Form,
-  FormGroup,
-  Row,
-  Col,
-  FormCheck,
-  FormLabel,
-  Popover,
-  Button,
-} from 'react-bootstrap'
-import DateConverter, {
-  weekDayNames,
-  monthNames,
-} from 'chronology/domain/DateConverter'
-import HelpTrigger from 'common/HelpTrigger'
-import { sections, Field } from 'chronology/ui/DateConverterFormFieldData'
+import { Form, Row, Col } from 'react-bootstrap'
+import DateConverter from 'chronology/domain/DateConverter'
+import { sections } from 'chronology/ui/DateConverterFormFieldData'
 import './DateConverterForm.sass'
 import { Markdown } from 'common/Markdown'
 import MarkupService from 'markup/application/MarkupService'
 import Markup from 'markup/ui/markup'
-import { KingField } from './BrinkmanKings'
+import {
+  DateConverterFormControls,
+  DateConverterFormSection,
+} from 'chronology/ui/DateConverterFormParts'
 
 // ToDo:
 // - General range: 29 March 625? BCE - 22 February 76? CE.
@@ -69,7 +59,7 @@ function DateConverterForm(): JSX.Element {
   const [formData, setFormData] = useState(dateConverter.calendar)
   const [scenario, setScenario] = useState('setToGregorianDate')
 
-  const handleChange = (event) => {
+  const handleChange = (event): void => {
     const { name, value } = event.target
     const _formData = {
       ...formData,
@@ -134,181 +124,12 @@ function DateConverterForm(): JSX.Element {
     setFormData({ ...dateConverter.calendar })
   }
 
-  const handleScenarioChange = (_scenario) => {
+  const handleScenarioChange = (_scenario: string) => {
     setScenario(_scenario)
-  }
-
-  const fieldIsActive = (fieldName) => {
-    switch (scenario) {
-      case 'setToGregorianDate':
-        return ['gregorianYear', 'gregorianMonth', 'gregorianDay'].includes(
-          fieldName
-        )
-      case 'setToJulianDate':
-        return ['julianYear', 'julianMonth', 'julianDay'].includes(fieldName)
-      case 'setToSeBabylonianDate':
-        return [
-          'seBabylonianYear',
-          'mesopotamianMonth',
-          'mesopotamianDay',
-        ].includes(fieldName)
-      case 'setToMesopotamianDate':
-        return [
-          'ruler',
-          'regnalYear',
-          'mesopotamianMonth',
-          'mesopotamianDay',
-        ].includes(fieldName)
-      default:
-        return false
-    }
   }
 
   const copyToClipboard = async () => {
     await navigator.clipboard.writeText(JSON.stringify(formData))
-  }
-
-  function getField(field: Field, index: number): JSX.Element {
-    if (field === 'king') {
-      return KingField({ king: ruler, setKing: setFormData })
-    }
-    const isSelect = [
-      'weekDay',
-      'gregorianYear',
-      'gregorianMonth',
-      'gregorianDay',
-      'julianYear',
-      'julianMonth',
-      'julianDay',
-    ].includes(field.name)
-    return (
-      <Col xs={12} sm={12} md={6} lg={6} key={index}>
-        <FormGroup>
-          <FormLabel htmlFor={field.name}>{field.placeholder}</FormLabel>{' '}
-          {getHelpTrigger(field)}
-          <Form.Control
-            {...(isSelect ? { as: 'select', children: getOptions(field) } : {})}
-            id={field.name}
-            name={field.name}
-            value={formData[field.name] || ''}
-            onChange={handleChange}
-            disabled={!fieldIsActive(field.name)}
-            required={field.required && fieldIsActive(field.name)}
-            className={fieldIsActive(field.name) ? 'active-field' : ''}
-          />
-        </FormGroup>
-      </Col>
-    )
-  }
-
-  function getHelpTrigger(field: Field): JSX.Element {
-    return (
-      <HelpTrigger
-        overlay={
-          <Popover id={field.name} title={field.name}>
-            <Popover.Content>{field.help}</Popover.Content>
-          </Popover>
-        }
-      />
-    )
-  }
-
-  function getOptions(field: Field): JSX.Element[] {
-    if (field.name === 'gregorianYear') {
-      return getNumberRangeOptions(-625, 76, getYearOptionLabel) //ToDo: Update to Gregorian dates
-    }
-    if (field.name === 'julianYear') {
-      return getNumberRangeOptions(-625, 76, getYearOptionLabel)
-    }
-    if (['gregorianDay', 'julianDay'].includes(field.name)) {
-      return getNumberRangeOptions(
-        1,
-        dateConverter.getMonthLength(field.name.includes('julian')),
-        (number) => number
-      )
-    }
-    const namesArray = ['gregorianMonth', 'julianMonth'].includes(field.name)
-      ? monthNames
-      : field.name === 'weekDay'
-      ? weekDayNames
-      : []
-    return namesArray.map((name, index) => (
-      <option key={index} value={index + 1}>
-        {name}
-      </option>
-    ))
-  }
-
-  function getNumberRangeOptions(
-    from: number,
-    to: number,
-    labelFormatter: (number) => string
-  ): JSX.Element[] {
-    const numbersArray = Array.from(
-      { length: to - from + 1 },
-      (_, index) => index + from
-    )
-
-    return numbersArray.map((number) => (
-      <option key={number} value={number}>
-        {labelFormatter(number)}
-      </option>
-    ))
-  }
-
-  function getYearOptionLabel(year: number): string {
-    return year < 1 ? `${Math.abs(year) + 1} BCE` : `${year} CE`
-  }
-
-  function getSection(
-    title: string,
-    fields: Field[],
-    index: number
-  ): JSX.Element {
-    return (
-      <Row
-        className={`section-row ${index % 2 === 0 ? 'even' : 'odd'}`}
-        key={index}
-      >
-        <Col md={1} className="section-title">
-          <div>{title}</div>
-        </Col>
-        <Col md={11} className="section-fields">
-          <Row>
-            {fields.map((field, fieldIndex) => getField(field, fieldIndex))}
-          </Row>
-        </Col>
-      </Row>
-    )
-  }
-
-  const scenarioLabels = {
-    setToGregorianDate: 'Modern date',
-    setToJulianDate: 'Julian date',
-    setToSeBabylonianDate: 'Seleucid (Babylonian) date',
-    setToMesopotamianDate: 'Nabonassar date',
-  }
-
-  function getControls(): JSX.Element {
-    return (
-      <aside className="main__sidebar copy-button-container">
-        <FormGroup>
-          <h4>Input</h4>
-          {Object.keys(scenarioLabels).map((_scenario) => (
-            <FormCheck
-              type="radio"
-              label={scenarioLabels[_scenario]}
-              key={_scenario}
-              checked={scenario === _scenario}
-              onChange={() => handleScenarioChange(_scenario)}
-            />
-          ))}
-        </FormGroup>
-        <Button className="copy-button btn btn-light" onClick={copyToClipboard}>
-          Copy JSON
-        </Button>
-      </aside>
-    )
   }
 
   return (
@@ -316,12 +137,27 @@ function DateConverterForm(): JSX.Element {
       <Row className="date_converter" key="date_converter">
         <Col md={8}>
           <Form>
-            {sections.map(({ title, fields }, index) =>
-              getSection(title, fields, index)
-            )}
+            {sections.map(({ title, fields }, index) => (
+              <DateConverterFormSection
+                key={index}
+                title={title}
+                fields={fields}
+                index={index}
+                dateConverter={dateConverter}
+                formData={formData}
+                handleChange={handleChange}
+                scenario={scenario}
+              />
+            ))}
           </Form>
         </Col>
-        <Col md={4}>{getControls()}</Col>
+        <Col md={4}>
+          <DateConverterFormControls
+            scenario={scenario}
+            handleScenarioChange={handleScenarioChange}
+            copyToClipboard={copyToClipboard}
+          />
+        </Col>
       </Row>
     </>
   )
---
+++ src/chronology/ui/DateConverterFormParts.tsx
@@ -0,0 +1,238 @@
+import React from 'react'
+import {
+  Form,
+  FormGroup,
+  FormCheck,
+  FormLabel,
+  Button,
+  Popover,
+  Row,
+  Col,
+} from 'react-bootstrap'
+import DateConverter from 'chronology/domain/DateConverter'
+import HelpTrigger from 'common/HelpTrigger'
+import { Field } from 'chronology/ui/DateConverterFormFieldData'
+import { weekDayNames, monthNames } from 'chronology/domain/DateConverter'
+import { CalendarProps } from 'chronology/domain/DateConverterBase'
+
+const scenarioLabels = {
+  setToGregorianDate: 'Modern date',
+  setToJulianDate: 'Julian date',
+  setToSeBabylonianDate: 'Seleucid (Babylonian) date',
+  setToMesopotamianDate: 'Nabonassar date',
+}
+
+export function DateConverterFormControls({
+  scenario,
+  handleScenarioChange,
+  copyToClipboard,
+}: {
+  scenario: string
+  handleScenarioChange: (_scenario: string) => void
+  copyToClipboard: () => Promise<void>
+}): JSX.Element {
+  return (
+    <aside className="main__sidebar copy-button-container">
+      <FormGroup>
+        <h4>Input</h4>
+        {Object.keys(scenarioLabels).map((_scenario) => (
+          <FormCheck
+            type="radio"
+            label={scenarioLabels[_scenario]}
+            key={_scenario}
+            checked={scenario === _scenario}
+            onChange={() => handleScenarioChange(_scenario)}
+          />
+        ))}
+      </FormGroup>
+      <Button className="copy-button btn btn-light" onClick={copyToClipboard}>
+        Copy JSON
+      </Button>
+    </aside>
+  )
+}
+
+export function DateConverterFormSection({
+  title,
+  fields,
+  index,
+  dateConverter,
+  formData,
+  handleChange,
+  scenario,
+}: {
+  title: string
+  fields: Field[]
+  index: number
+  dateConverter: DateConverter
+  formData: CalendarProps
+  handleChange: (event) => void
+  scenario: string
+}): JSX.Element {
+  return (
+    <Row
+      className={`section-row ${index % 2 === 0 ? 'even' : 'odd'}`}
+      key={index}
+    >
+      <Col md={1} className="section-title">
+        <div>{title}</div>
+      </Col>
+      <Col md={11} className="section-fields">
+        <Row>
+          {fields.map((field, fieldIndex) => (
+            <DateConverterFormField
+              key={fieldIndex}
+              field={field}
+              index={fieldIndex}
+              dateConverter={dateConverter}
+              formData={formData}
+              handleChange={handleChange}
+              scenario={scenario}
+            />
+          ))}
+        </Row>
+      </Col>
+    </Row>
+  )
+}
+
+function DateConverterFormField({
+  field,
+  index,
+  dateConverter,
+  formData,
+  handleChange,
+  scenario,
+}: {
+  field: Field
+  index: number
+  dateConverter: DateConverter
+  formData: CalendarProps
+  handleChange: (event) => void
+  scenario: string
+}): JSX.Element {
+  const isSelect = [
+    'weekDay',
+    'gregorianYear',
+    'gregorianMonth',
+    'gregorianDay',
+    'julianYear',
+    'julianMonth',
+    'julianDay',
+  ].includes(field.name)
+  return (
+    <Col xs={12} sm={12} md={6} lg={6} key={index}>
+      <FormGroup>
+        <FormLabel htmlFor={field.name}>{field.placeholder}</FormLabel>{' '}
+        <DateConverterFormHelpTrigger field={field} />
+        <Form.Control
+          {...(isSelect
+            ? { as: 'select', children: getOptions({ field, dateConverter }) }
+            : {})}
+          id={field.name}
+          name={field.name}
+          value={formData[field.name] || ''}
+          onChange={handleChange}
+          disabled={!fieldIsActive(field.name, scenario)}
+          required={field.required && fieldIsActive(field.name, scenario)}
+          className={fieldIsActive(field.name, scenario) ? 'active-field' : ''}
+        />
+      </FormGroup>
+    </Col>
+  )
+}
+
+function getOptions({
+  field,
+  dateConverter,
+}: {
+  field: Field
+  dateConverter: DateConverter
+}): JSX.Element[] {
+  if (field.name === 'gregorianYear') {
+    return getNumberRangeOptions(-625, 76, getYearOptionLabel) // ToDo: Update to Gregorian dates
+  }
+  if (field.name === 'julianYear') {
+    return getNumberRangeOptions(-625, 76, getYearOptionLabel)
+  }
+  if (['gregorianDay', 'julianDay'].includes(field.name)) {
+    return getNumberRangeOptions(
+      1,
+      dateConverter.getMonthLength(field.name.includes('julian')),
+      (number) => number
+    )
+  }
+  const namesArray = ['gregorianMonth', 'julianMonth'].includes(field.name)
+    ? monthNames
+    : field.name === 'weekDay'
+    ? weekDayNames
+    : []
+  return namesArray.map((name, index) => (
+    <option key={index} value={index + 1}>
+      {name}
+    </option>
+  ))
+}
+
+function getNumberRangeOptions(
+  from: number,
+  to: number,
+  labelFormatter: (number) => string
+): JSX.Element[] {
+  const numbersArray = Array.from(
+    { length: to - from + 1 },
+    (_, index) => index + from
+  )
+
+  return numbersArray.map((number) => (
+    <option key={number} value={number}>
+      {labelFormatter(number)}
+    </option>
+  ))
+}
+
+function getYearOptionLabel(year: number): string {
+  return year < 1 ? `${Math.abs(year) + 1} BCE` : `${year} CE`
+}
+
+const fieldIsActive = (fieldName: string, scenario: string) => {
+  switch (scenario) {
+    case 'setToGregorianDate':
+      return ['gregorianYear', 'gregorianMonth', 'gregorianDay'].includes(
+        fieldName
+      )
+    case 'setToJulianDate':
+      return ['julianYear', 'julianMonth', 'julianDay'].includes(fieldName)
+    case 'setToSeBabylonianDate':
+      return [
+        'seBabylonianYear',
+        'mesopotamianMonth',
+        'mesopotamianDay',
+      ].includes(fieldName)
+    case 'setToMesopotamianDate':
+      return [
+        'ruler',
+        'regnalYear',
+        'mesopotamianMonth',
+        'mesopotamianDay',
+      ].includes(fieldName)
+    default:
+      return false
+  }
+}
+
+function DateConverterFormHelpTrigger({
+  field,
+}: {
+  field: Field
+}): JSX.Element {
+  return (
+    <HelpTrigger
+      overlay={
+        <Popover id={field.name} title={field.name}>
+          <Popover.Content>{field.help}</Popover.Content>
+        </Popover>
+      }
+    />
+  )
+}
