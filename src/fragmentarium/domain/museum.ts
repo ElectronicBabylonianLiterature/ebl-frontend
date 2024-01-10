f3e107db3ed2f32d25aaf695401dd44e69adb481
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-01-10 11:49:14 +0000

    shorter museum keys; reformatting

--- src/fragmentarium/domain/museum.ts
+++ src/fragmentarium/domain/museum.ts
@@ -1,249 +1,190 @@
-import { Fragment } from './fragment'
-import bmLogo from './The_British_Museum.png'
-import ybcLogo from './YBC_small.jpg'
-
-
 export const Museums = {
-  'ISTANBUL_ARKEOLOJI_MUSEUM': {
+  ISTANBUL_ARKEOLOJI_MUSEUM: {
     name: 'İstanbul Arkeoloji Müzeleri',
     city: 'Istanbul',
     country: 'TUR',
     url: 'https://muze.gov.tr/muze-detay?SectionId=IAR01&DistId=IAR',
-    //logo: ,
   },
-  'THE_IRAQ_MUSEUM': {
+  THE_IRAQ_MUSEUM: {
     name: 'The Iraq Museum',
     city: 'Baghdad',
     country: 'IRQ',
     url: 'https://theiraqmuseum.com/',
-    //logo: ,
     copyright:
-    'By Permission of the State Board of Antiquities and Heritage and The Iraq Museum',
-    
+      'By Permission of the State Board of Antiquities and Heritage and The Iraq Museum',
   },
-  'UNIVERSITY_OF_PENNSYLVANIA_MUSEUM_OF_ARCHAEOLOGY_AND_ANTHROPOLOGY': {
+  PENN_MUSEUM: {
     name: 'University of Pennsylvania Museum of Archaeology and Anthropology',
     city: 'Philadelphia',
     country: 'USA',
     url: 'https://www.penn.museum/',
-    //logo: ,
-    
   },
-  'ASHMOLEAN_MUSEUM': {
+  ASHMOLEAN_MUSEUM: {
     name: 'Ashmolean Museum',
     city: 'Oxford',
     country: 'GBR',
     url: 'https://ashmolean.org/',
-    //logo: ,
-    
   },
-  'COLLEGE_OF_PHYSICIANS_OF_PHILADELPHIA': {
+  PHYSICIANS_COLLEGE_PHILADELPHIA: {
     name: 'College of Physicians of Philadelphia',
     city: 'Philadelphia',
     country: 'USA',
     url: 'https://www.collegeofphysicians.org/',
-    //logo: ,
-    
   },
-  'COUVENT_SAINT_ETIENNE': {
+  COUVENT_SAINT_ETIENNE: {
     name: 'Couvent Saint-Étienne',
     city: 'Jerusalem',
     country: 'ISR',
     url: 'https://www.ebaf.edu/couvent/',
-    //logo: ,
-    
   },
-  'REDPATH_MUSEUM_ENTNOLOGICAL_COLLECTIONS': {
+  REDPATH_MUSEUM: {
     name: 'Redpath Museum Ethnological Collections',
     city: 'Montreal',
     country: 'CAN',
     url: 'https://www.mcgill.ca/redpath/collections/ethnology',
-    //logo: ,
-    
   },
-  'FRAU_PROFESSOR_HILPRECHT_COLLECTION_OF_BABYLONIAN_ANTIQUITIES': {
+  HILPRECHT_COLLECTION: {
     name: 'Frau Professor Hilprecht Collection of Babylonian Antiquities',
     city: 'Jena',
     country: 'DEU',
-    url: 'https://www.gw.uni-jena.de/fakultaet/institut-fuer-orientalistik-indogermanistik-ur-und-fruehgeschichtliche-archaeologie/altorientalistik/hilprecht-sammlung',
-    //logo: ,
-    
+    url:
+      'https://www.gw.uni-jena.de/fakultaet/institut-fuer-orientalistik-indogermanistik-ur-und-fruehgeschichtliche-archaeologie/altorientalistik/hilprecht-sammlung',
   },
-  'PHOEBE_A_HEARST_MUSEUM_OF_ANTHROPOLOGY': {
+  HEARST_MUSEUM: {
     name: 'Phoebe A. Hearst Museum of Anthropology',
     city: 'Berkeley',
     country: 'USA',
     url: 'https://hearstmuseum.berkeley.edu/',
-    //logo: ,
-    
   },
-  'JOHN_RYLANDS_RESEARCH_INSTITUTE_AND_LIBRARY': {
+  RYLANDS_INSTITUTE: {
     name: 'John Rylands Research Institute and Library',
     city: 'Manchester',
     country: 'GRB',
     url: 'https://www.library.manchester.ac.uk/rylands/',
-    //logo: ,
-    
   },
-  'KELSEY_MUSEUM_OF_ARCHAEOLOGY': {
+  KELSEY_MUSEUM: {
     name: 'Kelsey Museum of Archaeology',
     city: 'Ann Arbor',
     country: 'USA',
     url: 'https://lsa.umich.edu/kelsey',
-    //logo: ,
-    
   },
-  'KUNSTHISTORISCHES_MUSEUM': {
+  KUNSTHISTORISCHES_MUSEUM: {
     name: 'Kunsthistorisches Museum',
     city: 'Vienna',
     country: 'AUT',
     url: 'https://www.khm.at/',
-    //logo: ,
-    
   },
-  'LOUVRE': {
+  LOUVRE: {
     name: 'Louvre',
     city: 'Paris',
     country: 'FRA',
     url: 'https://www.louvre.fr/',
-    //logo: ,
-    
   },
-  'MUSEE_D_ART_ET_D_HISTOIRE': {
+  MUSEE_D_ART: {
     name: 'Musée d’Art et d’Histoire',
     city: 'Geneva',
     country: 'CHE',
     url: 'https://www.mahmah.ch/',
-    //logo: ,
-    
   },
-  'MUSEES_ROYAUX_D_ART_ET_D_HISTOIRE': {
+  MUSEES_ROYAUX: {
     name: 'Musées royaux d’Art et d’Histoire',
     city: 'Brussels',
     country: 'BEL',
     url: 'https://www.kmkg-mrah.be/',
-    //logo: ,
-    
   },
-  'NATIONALMUSEET': {
+  NATIONALMUSEET: {
     name: 'Nationalmuseet',
     city: 'Copenhagen',
     country: 'DNK',
     url: 'https://en.natmus.dk/',
-    //logo: ,
-    
   },
-  'OAKLAND_MUSEUM_OF_CALIFORNIA': {
+  OAKLAND_MUSEUM: {
     name: 'Oakland Museum of California',
     city: 'Oakland',
     country: 'USA',
     url: 'https://museumca.org/',
-    //logo: ,
-    
   },
-  'INSTITUTE_FOR_THE_STUDY_OF_ANCIENT_CULTURES_WEST_ASIA_AND_NORTH_AFRICA': {
-    name: 'Institute for the Study of Ancient Cultures, West Asia & North Africa',
+  ANCIENT_CULTURES_CHICAGO: {
+    name:
+      'Institute for the Study of Ancient Cultures, West Asia & North Africa',
     city: 'Chicago',
     country: 'USA',
     url: 'https://isac.uchicago.edu/',
-    //logo: ,
-    
   },
-  'PIERPONT_MORGAN_LIBRARY_AND_MUSEUM': {
+  PIERPONT_MORGAN: {
     name: 'Pierpont Morgan Library & Museum',
     city: 'New York',
     country: 'USA',
     url: 'https://www.themorgan.org/',
-    //logo: ,
-    
   },
-  'PONTIFICAL_BIBLICAL_INSTITUTE': {
+  PONTIFICAL_BIBLICAL_INSTITUTE: {
     name: 'Pontifical Biblical Institute',
     city: 'Rome',
     country: 'ITA',
     url: 'http://www.biblico.it/',
-    //logo: ,
-    
   },
-  'ROSICRUCIAN_EGYPTIAN_MUSEUM': {
+  ROSICRUCIAN_EGYPTIAN_MUSEUM: {
     name: 'Rosicrucian Egyptian Museum',
     city: 'San Jose',
     country: 'USA',
     url: 'https://egyptianmuseum.org/',
-    //logo: ,
-    
   },
-  'THE_BRITISH_MUSEUM': {
+  THE_BRITISH_MUSEUM: {
     name: 'The British Museum',
     city: 'London',
     country: 'GBR',
     url: 'https://www.britishmuseum.org/',
-    //logo: ,
     copyright:
-        '© [The Trustees of the British Museum](https://www.britishmuseum.org/about_this_site/terms_of_use/copyright_and_permissions.aspx)',
-    
+      '© [The Trustees of the British Museum](https://www.britishmuseum.org/about_this_site/terms_of_use/copyright_and_permissions.aspx)',
   },
-  'TRINITY_COLLEGE_DUBLIN': {
+  TRINITY_COLLEGE_DUBLIN: {
     name: 'Trinity College Dublin',
     city: 'Dublin',
     country: 'IRL',
     url: 'https://www.tcd.ie/',
-    //logo: ,
-    
   },
-  'VATICAN_MUSEUMS': {
+  VATICAN_MUSEUMS: {
     name: 'Vatican Museums',
     city: 'Vatican City',
     country: 'VAT',
     url: 'http://www.museivaticani.va/',
-    //logo: ,
-    
   },
-  'VORDERASIATISCHES_MUSEUM': {
+  VORDERASIATISCHES_MUSEUM: {
     name: 'Vorderasiatisches Museum',
     city: 'Berlin',
     country: 'DEU',
-    url: 'https://www.smb.museum/en/museums-institutions/vorderasiatisches-museum/home/',
-    //logo: ,
-    
+    url:
+      'https://www.smb.museum/en/museums-institutions/vorderasiatisches-museum/home/',
   },
-  'THE_WALTERS_ART_MUSEUM': {
+  THE_WALTERS_ART_MUSEUM: {
     name: 'The Walters Art Museum',
     city: 'Baltimore',
     country: 'USA',
     url: 'https://thewalters.org/',
-    //logo: ,
-    
   },
-  'YALE_PEABODY_MUSEUM_YALE_BABYLONIAN_COLLECTION': {
+  YALE_PEABODY_COLLECTION: {
     name: 'Yale Peabody Museum, Yale Babylonian Collection',
     city: 'New Haven',
     country: 'USA',
-    url: 'https://peabody.yale.edu/explore/collections/yale-babylonian-collection',
-    //logo: ,
-    
+    url:
+      'https://peabody.yale.edu/explore/collections/yale-babylonian-collection',
   },
-  'ECOLE_PRATIQUE_DES_HAUTES_ETUDES': {
+  ECOLE_PRATIQUE_DES_HAUTES_ETUDES: {
     name: 'École pratique des hautes Études',
     city: 'Paris',
     country: 'FRA',
     url: 'https://www.ephe.psl.eu/',
-    //logo: ,
-    
   },
-  'UNKNOWN': {
+  UNKNOWN: {
     name: '',
     city: '',
     country: '',
     url: '',
-    //logo: ,
-    
   },
-} as const;
+} as const
 
-export type Museum = typeof Museums[keyof typeof Museums];
-export type MuseumKey = keyof typeof Museums;
-
+export type Museum = typeof Museums[keyof typeof Museums]
+export type MuseumKey = keyof typeof Museums
 
 export interface FragmentLink {
   readonly name: string
@@ -260,4 +201,3 @@ export interface MuseumData {
   readonly logo: string
   readonly copyright: string
 }
-
