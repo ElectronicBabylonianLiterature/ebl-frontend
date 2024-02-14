06f8a5effe57bace21cd40591141784a6cba3d37
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2024-02-14 13:33:29 +0100

    Displaying provenance of signs (#449)

--- src/signs/domain/CroppedAnnotation.ts
+++ src/signs/domain/CroppedAnnotation.ts
@@ -5,6 +5,7 @@ type base64String = string
 export interface CroppedAnnotation {
   image: base64String
   fragmentNumber: string
+  provenance?: string
   script: string
   label: string
   date?: MesopotamianDate
