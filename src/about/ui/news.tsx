609b0b3a356ca8cb93e49bce54eb2eb2962ef920
Zsombor Földi <zsombor.foldi@gmail.com>
2024-02-15 21:09:47 +0000

    Update Newsletter 10-14

--- src/about/ui/news.tsx
+++ src/about/ui/news.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import newsletter15 from 'about/ui/newsletter/015.md'
 import newsletter14 from 'about/ui/newsletter/014.md'
+import newsletter13 from 'about/ui/newsletter/013.md'
+import newsletter12 from 'about/ui/newsletter/012.md'
+import newsletter11 from 'about/ui/newsletter/011.md'
+import newsletter10 from 'about/ui/newsletter/010.md'
 import { Nav, Container, Row, Col } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 
@@ -13,15 +17,17 @@ interface Newsletter {
 
 const newsletters: readonly Newsletter[] = [
   { content: newsletter15, date: new Date('02/04/2024'), number: 15 },
-  { content: newsletter14, date: new Date('11/14/2024'), number: 14 },
+  { content: newsletter14, date: new Date('11/06/2023'), number: 14 },
+  { content: newsletter13, date: new Date('06/21/2023'), number: 13 },
+  { content: newsletter12, date: new Date('02/23/2023'), number: 12 },
+  { content: newsletter11, date: new Date('10/28/2022'), number: 11 },
+  { content: newsletter10, date: new Date('10/10/2022'), number: 10 },
 ]
 
 const message = `**Get the most out of eBL!**  
 We will be hosting regular Zoom sessions to showcase its features and tools. 
-These sessions will include a Q&A – please feel free to submit questions in advance. The first session is scheduled 
-for February 29th at 6:00 PM CET. If you would like to attend, please register at the link.
-Important new developments and recently implemented features are regularly summarized in the eBL Newsletters (see below).
-If you wish to receive future eBL Newsletters, send us an [e-mail](mailto:ebl-info@culture.lmu.de).
+These sessions will include a Q&A – please feel free to submit questions in advance per [e-mail](mailto:ebl-info@culture.lmu.de).
+The first session is scheduled for February 29th at 6:00 PM CET. If you would like to attend, please register at the link.
 `
 
 function NewsletterMenu({
