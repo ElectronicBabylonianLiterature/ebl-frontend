ece9e9855a2eea86fac40d53afdcd588d018a8de
Ilya Khait <ilya.khait@lmu.de>
2024-02-27 11:16:16 +0000

    Add `exact` attribute to Fragmentarium Routes

--- src/router/fragmentariumRoutes.tsx
+++ src/router/fragmentariumRoutes.tsx
@@ -78,6 +78,7 @@ export default function FragmentariumRoutes({
     <Route
       key="FragmentariumSearch"
       path="/fragmentarium/search"
+      exact
       render={({ location }): ReactNode => (
         <HeadTagsService
           title="Fragmentarium search: eBL"
@@ -99,6 +100,7 @@ export default function FragmentariumRoutes({
     <Route
       key="FragmentLineToVecRanking"
       path="/fragmentarium/:id/match"
+      exact
       render={({ match }): ReactNode => (
         <HeadTagsService
           title="Fragmentarium line to vector ranking: eBL"
@@ -118,6 +120,7 @@ export default function FragmentariumRoutes({
     <Route
       key="TagSignsView"
       path="/fragmentarium/:id/annotate"
+      exact
       render={({ match }): ReactNode => (
         <TagSignsView
           signService={signService}
@@ -129,6 +132,7 @@ export default function FragmentariumRoutes({
     <Route
       key="FragmentView"
       path="/fragmentarium/:id"
+      exact
       render={({ match, location }): ReactNode => (
         <SessionContext.Consumer>
           {(session) => (
@@ -157,6 +161,7 @@ export default function FragmentariumRoutes({
     <Route
       key="Fragmentarium"
       path="/fragmentarium"
+      exact
       render={(): ReactNode => (
         <HeadTagsService
           title="Fragmentarium: eBL"
@@ -177,7 +182,7 @@ export default function FragmentariumRoutes({
       {...(sitemap && sitemapDefaults)}
     />,
     <Route
-      key="NotFoundFragmentarium"
+      key="FragmentariumNotFound"
       path="/Fragmentarium/*"
       render={(): ReactNode => <NotFoundPage />}
     />,
