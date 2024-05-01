aef1b094cf408d394f84f91730cf0f432d0fd6a0
Zsombor Földi <zsombor.foldi@gmail.com>
2024-05-01 09:57:13 +0000

    update ebl evening correction

--- src/about/ui/news.tsx
+++ src/about/ui/news.tsx
@@ -47,7 +47,7 @@ const message = `**Get the most out of eBL!**
 We will be hosting regular Zoom sessions to showcase its features and tools. 
 These sessions will include a Q&A – please feel free to submit questions in 
 advance per [e-mail](mailto:${process.env.REACT_APP_INFO_EMAIL}).
-The first session is scheduled for May 31st at 6:00 PM CET. This time we shall focus on the Editors' work.
+The second session is scheduled for May 31st at 6:00 PM CET. This time we shall focus on the Editors' work.
 If you would like to attend, please register at the
 [link](https://lmu-munich.zoom-x.de/meeting/register/u5cldemhqDwtGtR-CKF5V1fnntrrTsYbLXEf).
 `
