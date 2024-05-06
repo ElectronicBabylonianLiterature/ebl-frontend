e774981aff2d64421861a47f98611ddfec7e96be
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-05-06 14:58:10 +0000

    redesign project list

--- src/research-projects/ResearchProjectsOverview.tsx
+++ src/research-projects/ResearchProjectsOverview.tsx
@@ -1,29 +1,46 @@
-import AppContent from 'common/AppContent'
-import { TextCrumb } from 'common/Breadcrumbs'
 import React from 'react'
+import AppContent from 'common/AppContent'
+import { SectionCrumb } from 'common/Breadcrumbs'
 import { ResearchProject, ResearchProjects } from './researchProject'
 import { Link } from 'react-router-dom'
+import 'research-projects/ResearchProjects.sass'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function ResearchProjectLink({
   project,
 }: {
   project: ResearchProject
 }): JSX.Element {
-  return <Link to={`/projects/${project.abbreviation}`}>{project.name}</Link>
+  return (
+    <Row>
+      <Col sm={2}>
+        <Link to={`/projects/${project.abbreviation}`}>
+          <img
+            className="project-list__image"
+            src={project.logo}
+            alt={project.name}
+          />
+        </Link>
+      </Col>
+      <Col sm={'auto'}>
+        <Link to={`/projects/${project.abbreviation}`}>
+          {project.displayName || project.name}
+        </Link>
+      </Col>
+    </Row>
+  )
 }
 
 export default function ResearchProjectsOverview(): JSX.Element {
   return (
     <AppContent
       title={'Research Projects in eBL'}
-      crumbs={[new TextCrumb('Projects')]}
+      crumbs={[new SectionCrumb('Projects')]}
     >
       <section>
-        <ul>
-          <li>
-            <ResearchProjectLink project={ResearchProjects.CAIC} />
-          </li>
-        </ul>
+        <Container>
+          <ResearchProjectLink project={ResearchProjects.CAIC} />
+        </Container>
       </section>
     </AppContent>
   )
