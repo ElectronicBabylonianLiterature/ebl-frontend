46440ba4f2907ce5a00dce4f73adfd65fd445290
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-06-17 13:47:43 +0000

    refactor tests - move render into beforeEach - select input field by aria label - match html display by snapshot

--- src/chronology/ui/CuneiformConverter/CuneiformConverterForm.test.tsx
+++ src/chronology/ui/CuneiformConverter/CuneiformConverterForm.test.tsx
@@ -7,8 +7,9 @@ jest.mock('signs/application/SignService')
 const signServiceMock = new (SignService as jest.Mock<
   jest.Mocked<SignService>
 >)()
-const description =
-  'This tool allows to convert transliterations to Unicode cuneiform (ranges U+12000-U+123FF, U+12400-U+1247F, and U+12480-U+1254F), using the mapping from the eBL sign list. Different fonts, developed by S. Vanseveren, can be used to display the cuneiform text.'
+
+let container: HTMLElement
+
 describe('CuneiformConverterForm', () => {
   beforeEach(() => {
     Object.defineProperty(window.navigator, 'clipboard', {
@@ -17,6 +18,8 @@ describe('CuneiformConverterForm', () => {
       },
       writable: true,
     })
+    container = render(<CuneiformConverterForm signService={signServiceMock} />)
+      .container
   })
 
   afterEach(() => {
@@ -24,19 +27,14 @@ describe('CuneiformConverterForm', () => {
   })
 
   it('renders form, options, and scenario panel correctly', () => {
-    render(<CuneiformConverterForm signService={signServiceMock} />)
-    expect(screen.getByLabelText(description)).toBeInTheDocument()
-    expect(screen.getByLabelText('Select Font')).toBeInTheDocument()
-    expect(screen.getByLabelText('Converted Text')).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
   })
   it('handles input change', () => {
-    render(<CuneiformConverterForm signService={signServiceMock} />)
-    const inputTextArea = screen.getByLabelText(description)
+    const inputTextArea = screen.getByLabelText('input-atf')
     fireEvent.change(inputTextArea, { target: { value: 'test text' } })
     expect(inputTextArea).toHaveValue('test text')
   })
   it('handles font change', () => {
-    render(<CuneiformConverterForm signService={signServiceMock} />)
     const fontSelector = screen.getByLabelText('Select Font')
     fireEvent.change(fontSelector, { target: { value: 'Esagil' } })
     expect(fontSelector).toHaveValue('Esagil')
@@ -45,9 +43,8 @@ describe('CuneiformConverterForm', () => {
     signServiceMock.getUnicodeFromAtf.mockResolvedValueOnce([
       { unicode: [73979] },
     ])
-    render(<CuneiformConverterForm signService={signServiceMock} />)
 
-    const inputTextArea = screen.getByLabelText(description)
+    const inputTextArea = screen.getByLabelText('input-atf')
     fireEvent.change(inputTextArea, { target: { value: 'test text' } })
 
     const convertButton = screen.getByText('Convert')
