9200ac45ff790ec49d31062c7fe1a32d8ba0d038
fsimonjetz <fabian.simonjetz@rub.de>
2024-07-15 20:32:12 +0200

    Update info banner (#491)
    * update info banner
    
    * update snapshots

--- src/InfoBanner.tsx
+++ src/InfoBanner.tsx
@@ -3,9 +3,11 @@ import Alert from 'react-bootstrap/Alert'
 import './Header.css'
 import { Container } from 'react-bootstrap'
 import { set as setCookie, get as getCookie } from 'es-cookie'
+import ExternalLink from 'common/ExternalLink'
 
 function InfoBanner(): JSX.Element | null {
-  const initialState = getCookie('eblShowInfoBanner') !== 'false'
+  const key = 'eblShowUpdateBanner'
+  const initialState = getCookie(key) !== 'false'
   const [show, setShow] = useState(initialState)
 
   const midnight = new Date()
@@ -14,20 +16,29 @@ function InfoBanner(): JSX.Element | null {
   return show ? (
     <Container fluid className="Header__info-banner">
       <Alert
-        variant="warning"
+        variant="primary"
         onClose={() => {
           setShow(false)
-          setCookie('eblShowInfoBanner', 'false', { expires: midnight })
+          setCookie(key, 'false', { expires: midnight })
         }}
         dismissible
       >
-        <strong>Scheduled Maintenance Notice:</strong> On{' '}
-        <strong>Monday, July 15, 2024, from 10:00 AM to 12:00 PM CEST</strong>,
-        the eBL servers will be undergoing scheduled maintenance. During this
-        time, you will not be able to log in or make any changes to records. The
-        maintenance may also cause temporary service interruptions. Registered
-        users will receive an email notification once the maintenance is
-        complete.
+        <strong>Update:</strong> On Monday, July 15, the eBL servers were
+        successfully migrated to new infrastructure. From now on, eBL can be
+        accessed either from{' '}
+        <ExternalLink href="https://www.ebl.lmu.de">
+          https://www.ebl.lmu.de
+        </ExternalLink>{' '}
+        or{' '}
+        <ExternalLink href="https://www.ebl.badw.de">
+          https://www.ebl.badw.de
+        </ExternalLink>
+        . Please note that it will not be possible to login at www.ebl.lmu.de
+        until July 16, but you can already access eBL via the new url at
+        <ExternalLink href="https://www.ebl.badw.de">
+          https://www.ebl.badw.de
+        </ExternalLink>
+        .
       </Alert>
     </Container>
   ) : null
