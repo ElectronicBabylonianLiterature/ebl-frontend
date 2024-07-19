d72069e0ea136a820e2d95f15948349b0ac784a5
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2024-07-19 13:25:41 +0000

    add Footer component

---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.sass'
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="main-footer">
+      <div className="breadcrumb">Impressum</div>
+    </footer>
+  )
+}
