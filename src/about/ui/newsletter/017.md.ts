04c797e808d5edf8bd2412485ff80f5270050c33
zsomborfoldi <96750736+zsomborfoldi@users.noreply.github.com>
2024-08-07 16:01:53 +0200

    Newsletter 17 Corrections (#505)

--- src/about/ui/newsletter/017.md.ts
+++ src/about/ui/newsletter/017.md.ts
@@ -13,23 +13,24 @@ const newsletter = `
   ([U.3018](https://www.ebl.lmu.de/fragmentarium/U.3018)) and a variant version
   of the Sumerian King List
   ([U.33271](https://www.ebl.lmu.de/fragmentarium/U.33271)).
--	Folios
-  -	Notebook IX of G. Smith (Bilinguals) has been added
-  -	Folios by Jussi Aro (courtesy of Sanna Aro-Valjus) have been added. They
+- Folios
+  - Notebook IX of G. Smith (Bilinguals) has been added.
+  - Folios by Jussi Aro (courtesy of Sanna Aro-Valjus) have been added. They
     contain finished copies of some 50 extispicy texts, mostly Old Babylonian
     manuscripts and Neo-Assyrian extispicy reports.
--	The new **Permissions** tab in the text editor allows control over who can
+- The new **Permissions** tab in the text editor allows control over who can
   access an edition. For example, checking \`Restrict it to users with CAIC
   permissions\` limits access to CAIC project members. This enables private
   editing until the editor is ready to publish the edition.
--	Editors are kindly requested to add the label \`@colophon\` to their editions,
+- Editors are kindly requested to add the label \`@colophon\` to their editions,
   since this enables the automatic retrieval of all colophons in the database.
 
-### About
+### Tools
 
 - A new display of signs in sign search has been implemented: Now similar
   beginnings and endings are suggested for Neo-Babylonian and Neo-Assyrian
-  scripts. See also the new section “Sign Order” in About > Signs.
+  scripts. See also the new section “Sign Order” in
+  [About > Signs](https://www.ebl.lmu.de/about/signs).
 
 ### Corpus
 
