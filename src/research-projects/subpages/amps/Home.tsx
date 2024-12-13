1d3f5013a7f11db0ac0d66ac4bf166d12c65b2c0
Zsombor Földi <zsombor.foldi@gmail.com>
2024-12-13 11:35:21 +0000

    add AMPS

---
+++ src/research-projects/subpages/amps/Home.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import SearchForm, { SearchFormProps } from 'fragmentarium/ui/SearchForm'
+import { SearchResult } from 'fragmentarium/ui/search/FragmentariumSearchResult'
+import PageContent from 'research-projects/subpages/amps/PageContent'
+import ExternalLink from 'common/ExternalLink'
+
+export default function Home({
+  fragmentService,
+  fragmentSearchService,
+  bibliographyService,
+  wordService,
+  fragmentQuery,
+}: Pick<
+  SearchFormProps,
+  | 'fragmentService'
+  | 'fragmentSearchService'
+  | 'bibliographyService'
+  | 'wordService'
+  | 'fragmentQuery'
+>): JSX.Element {
+  return (
+    <PageContent title={'Introduction'} menuTitle={'Home'}>
+      <p>
+        The scholarly texts of ancient Mesopotamia in the first millennium BCE,
+        specifically commentaries written in Akkadian on cuneiform tablets, were
+        the work of priests who also performed cultic activities in the temple.
+        The proposed project seeks to demonstrate how these scholarly and cultic
+        activities were interrelated and how they shaped the self-identity of
+        the priestly-scholarly community that was in charge of both. The project
+        thus aims to bridge the gap between the study of intellectual history
+        and the study of priesthood in ancient Mesopotamia, which are treated as
+        two separate fields in Assyriology.
+      </p>
+      <p>
+        The project innovatively treats Mesopotamian scholarship and
+        Mesopotamian priesthood as complementary aspects of one phenomenon:
+        “scholasticism.” This concept, which originally referred to the
+        scholarly activities of Catholic priests in the Middle Ages, has
+        recently been applied to the study of non-European communities of
+        priestly scholars with great success. Using the scholastic model to
+        study the priestly-scholarly community of ancient Mesopotamia will
+        reveal the intricate connections between the ritual and textual
+        activities of this community and illuminate the holistic and systematic
+        worldview of its members.
+      </p>
+      <p>
+        Combining traditional philology and the comparative approach, the
+        project investigates how, like other scholastic communities, the
+        scholar-priests of ancient Mesopotamia “internalized” the liturgical
+        texts they studied and performed, how they attributed authority to these
+        texts, and how their study of the liturgical corpus generated new
+        exegetical texts. Key points of comparison between the scholar-priests
+        of ancient Mesopotamia and various ancient and contemporary scholastic
+        communities include their interest in language, textual authority,
+        commentaries, and rituals. By applying the comparative method to the
+        study of cuneiform tablets, the project aims to reconstruct the social,
+        religious, and intellectual reality in which they were written.
+      </p>
+      <p>
+        Search for AMPS texts below or{' '}
+        <ExternalLink href={'https://amps.huji.ac.il/'}>
+          click here
+        </ExternalLink>{' '}
+        to learn more about the project.
+      </p>
+      <div className={'project-page__search'}>
+        <h3>Search in AMPS</h3>
+        <Container>
+          <SearchForm
+            fragmentSearchService={fragmentSearchService}
+            fragmentService={fragmentService}
+            wordService={wordService}
+            bibliographyService={bibliographyService}
+            fragmentQuery={fragmentQuery}
+            project={'AMPS'}
+          />
+        </Container>
+      </div>
+      {fragmentQuery && (
+        <SearchResult
+          fragmentService={fragmentService}
+          fragmentQuery={fragmentQuery}
+        />
+      )}
+    </PageContent>
+  )
+}
---
+++ src/research-projects/subpages/amps/PageContent.tsx
@@ -0,0 +1,39 @@
+import React, { PropsWithChildren } from 'react'
+import AppContent from 'common/AppContent'
+import { ProjectCrumb, SectionCrumb, TextCrumb } from 'common/Breadcrumbs'
+import { Container, Row, Col } from 'react-bootstrap'
+import { ResearchProjects } from 'research-projects/researchProject'
+import TableOfContents from 'research-projects/subpages/amps/TableOfContents'
+
+export default function PageContent({
+  title,
+  menuTitle,
+  children,
+}: { title: string; menuTitle?: string } & PropsWithChildren<
+  unknown
+>): JSX.Element {
+  return (
+    <AppContent
+      title={ResearchProjects.AMPS.name}
+      crumbs={[
+        new SectionCrumb('Projects'),
+        new ProjectCrumb(ResearchProjects.AMPS),
+        new TextCrumb(menuTitle || title),
+      ]}
+    >
+      <Container fluid>
+        <Row>
+          <Col>
+            <h3>{title}</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>{children}</Col>
+          <Col sm={2}>
+            <TableOfContents />
+          </Col>
+        </Row>
+      </Container>
+    </AppContent>
+  )
+}
