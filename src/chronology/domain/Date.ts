39d2306b1034e3cdd596eb3b62dcc0a84ede039e
Ilya Khait <ilya.khait@lmu.de>
2025-01-21 16:32:42 +0000

    Clean up

--- src/chronology/domain/Date.ts
+++ src/chronology/domain/Date.ts
@@ -3,7 +3,6 @@ import { MesopotamianDateString } from 'chronology/domain/DateString'
 
 export class MesopotamianDate extends MesopotamianDateString {
   static fromJson(dateJson: MesopotamianDateDto): MesopotamianDate {
-    console.log(dateJson)
     return new MesopotamianDate({ ...dateJson })
   }
 }
