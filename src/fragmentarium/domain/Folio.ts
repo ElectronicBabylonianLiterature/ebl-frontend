90420f9951bb29a0fbeb99fcb141cc7c59e23edb
Enrique Jiménez <enrique.jimenez@lmu.de>
2025-02-17 11:53:32 +0000

    Add Postgate Folios

--- src/fragmentarium/domain/Folio.ts
+++ src/fragmentarium/domain/Folio.ts
@@ -22,6 +22,7 @@ const folioTypes: {
   ILF: { name: 'Finkel', hasImage: true, isOpen: false },
   JA: { name: 'Aro', hasImage: true, isOpen: true },
   JLP: { name: 'Peterson', hasImage: true, isOpen: false },
+  JNP: { name: 'Postgate', hasImage: true, isOpen: false },
   JS: { name: 'Strassmaier', hasImage: true, isOpen: true },
   JVD: { name: 'van Dijk', hasImage: true, isOpen: false },
   LV: { name: 'Vacín', hasImage: true, isOpen: true },
