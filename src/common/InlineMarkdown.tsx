015910a0e3be5b23a8849a780cef8610c7e36176
Enrique Jiménez <enrique.jimenez@lmu.de>
2025-03-04 15:27:33 +0000

    Add allowParagraphs to props of InlineMarkdown, add tests

--- src/common/InlineMarkdown.tsx
+++ src/common/InlineMarkdown.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import * as remarkSubSuper from 'remark-sub-super'
+import remarkSubSuper from 'remark-sub-super'
+
+interface RenderProps {
+  children: React.ReactNode
+}
 
 export default function InlineMarkdown({
   source,
   className = '',
+  allowParagraphs = false,
 }: {
   source: string
   className?: string
+  allowParagraphs?: boolean
 }): JSX.Element {
   return (
     <ReactMarkdown
       className={className}
       source={source}
       plugins={[remarkSubSuper]}
-      disallowedTypes={['paragraph']}
+      disallowedTypes={allowParagraphs ? [] : ['paragraph']}
       unwrapDisallowed
       renderers={{
-        sub: 'sub',
-        sup: 'sup',
+        sub: ({ children }: RenderProps) => <sub>{children}</sub>,
+        sup: ({ children }: RenderProps) => <sup>{children}</sup>,
       }}
     />
   )
