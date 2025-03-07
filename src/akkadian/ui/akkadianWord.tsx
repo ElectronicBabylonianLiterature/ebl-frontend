d16355a06cca568edac3050115a910b43385c0d8
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-03-07 16:47:09 +0000

    remove WordInfoComponent from AkkadianWordComponent

--- src/akkadian/ui/akkadianWord.tsx
+++ src/akkadian/ui/akkadianWord.tsx
@@ -9,7 +9,6 @@ import { addBreves } from 'transliteration/domain/accents'
 import { isEnclosure } from 'transliteration/domain/type-guards'
 import EnclosureFlags from 'transliteration/ui/EnclosureFlags'
 import Flags from 'transliteration/ui/Flags'
-import WordInfoWithPopover, { WordInfo } from 'transliteration/ui/WordInfo'
 import Meter from 'akkadian/ui/meter'
 import Ipa from 'akkadian/ui/ipa'
 import {
@@ -20,9 +19,6 @@ import {
 export default function AkkadianWordComponent({
   token,
   Wrapper,
-  tokenClasses,
-  lineGroup,
-  isInPopover = false,
   showMeter = false,
   showIpa = false,
   phoneticProps = {},
@@ -30,29 +26,22 @@ export default function AkkadianWordComponent({
   const word = addBreves(token as AkkadianWord)
   const lastParts = _.takeRightWhile(word.parts, isEnclosure)
   const parts = _.dropRight(word.parts, lastParts.length)
-  const WordInfoComponent = isInPopover ? WordInfo : WordInfoWithPopover
 
   return (
     <>
-      <WordInfoComponent
-        word={word}
-        tokenClasses={tokenClasses ?? []}
-        lineGroup={lineGroup}
-      >
-        <DamagedFlag sign={{ flags: word.modifiers }} Wrapper={Wrapper}>
-          <EnclosureFlags token={word}>
-            {parts.map((token, index) => (
-              <DisplayToken key={index} token={token} Wrapper={Wrapper} />
-            ))}
-            <Wrapper>
-              <Flags flags={word.modifiers} />
-            </Wrapper>
-            {lastParts.map((token, index) => (
-              <DisplayToken key={index} token={token} Wrapper={Wrapper} />
-            ))}
-          </EnclosureFlags>
-        </DamagedFlag>
-      </WordInfoComponent>
+      <DamagedFlag sign={{ flags: word.modifiers }} Wrapper={Wrapper}>
+        <EnclosureFlags token={word}>
+          {parts.map((token, index) => (
+            <DisplayToken key={index} token={token} Wrapper={Wrapper} />
+          ))}
+          <Wrapper>
+            <Flags flags={word.modifiers} />
+          </Wrapper>
+          {lastParts.map((token, index) => (
+            <DisplayToken key={index} token={token} Wrapper={Wrapper} />
+          ))}
+        </EnclosureFlags>
+      </DamagedFlag>
       <AkkadianWordAnalysis
         word={word}
         showMeter={showMeter}
