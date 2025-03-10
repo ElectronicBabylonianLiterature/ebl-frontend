94a2dd161e711e7d90f34e300eea9b81f02e78b7
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-03-10 13:58:00 +0000

    add WordInfoPopover to DisplayTextLine for fragment display

--- src/transliteration/ui/text-line.tsx
+++ src/transliteration/ui/text-line.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { PropsWithChildren, useEffect, useRef } from 'react'
 import { Anchor, LineNumber } from './line-number'
 import { LineColumns } from './line-tokens'
 import { TextLine } from 'transliteration/domain/text-line'
@@ -6,10 +6,26 @@ import { LineProps } from './LineProps'
 import { lineNumberToAtf } from 'transliteration/domain/lineNumberToString'
 import TransliterationTd from './TransliterationTd'
 import { Labels, labelsAbbreviation } from 'transliteration/domain/labels'
+import { Token } from 'transliteration/domain/token'
+import { isAnyWord } from 'transliteration/domain/type-guards'
+import WordInfoWithPopover from 'transliteration/ui/WordInfo'
 
 function createId(labels: Labels | undefined, textLine: TextLine) {
   const label = labels ? `${labelsAbbreviation(labels)} ` : ''
   return `${label}${lineNumberToAtf(textLine.lineNumber)}`
+}
+
+function WordInfoPopover({
+  token,
+  children,
+}: PropsWithChildren<{
+  token: Token
+}>): JSX.Element {
+  return isAnyWord(token) ? (
+    <WordInfoWithPopover word={token}>{children}</WordInfoWithPopover>
+  ) : (
+    <>{children}</>
+  )
 }
 
 export default function DisplayTextLine({
@@ -35,7 +51,11 @@ export default function DisplayTextLine({
           <LineNumber line={textLine} />
         </Anchor>
       </TransliterationTd>
-      <LineColumns columns={textLine.columns} maxColumns={columns} />
+      <LineColumns
+        columns={textLine.columns}
+        maxColumns={columns}
+        TokenActionWrapper={WordInfoPopover}
+      />
     </>
   )
 }
