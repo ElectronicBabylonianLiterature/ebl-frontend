1e1c66f1427ad69ffbfeeaa4b6bf4ac8992f7112
Enrique Jiménez <enrique.jimenez@lmu.de>
2025-04-01 12:14:33 +0000

    Fix test file

--- src/fragmentarium/ui/search/FragmentariumSearch.test.tsx
+++ src/fragmentarium/ui/search/FragmentariumSearch.test.tsx
@@ -1,220 +1,251 @@
 import React from 'react'
+import Chance from 'chance'
 import { MemoryRouter, withRouter, RouteComponentProps } from 'react-router-dom'
-import { render, screen, fireEvent, act } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import Promise from 'bluebird'
+import FragmentariumSearch from './FragmentariumSearch'
 import SessionContext from 'auth/SessionContext'
 import FragmentSearchService from 'fragmentarium/application/FragmentSearchService'
 import MemorySession, { Session } from 'auth/Session'
-import FragmentService from 'fragmentarium/application/FragmentService'
-import Fragmentarium from './Fragmentarium'
-import Bluebird from 'bluebird'
-import {
-  fragmentFactory,
-  fragmentInfoFactory,
-} from 'test-support/fragment-fixtures'
-import { statisticsFactory } from 'test-support/fragment-data-fixtures'
-import { Fragment, FragmentInfo } from 'fragmentarium/domain/fragment'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import { fragmentFactory } from 'test-support/fragment-fixtures'
 import WordService from 'dictionary/application/WordService'
-import { queryItemOf } from 'test-support/utils'
 import { DictionaryContext } from 'dictionary/ui/dictionary-context'
-import DossiersService from 'dossiers/application/DossiersService'
+import FragmentService from 'fragmentarium/application/FragmentService'
 import BibliographyService from 'bibliography/application/BibliographyService'
+import { FragmentQuery } from 'query/FragmentQuery'
+import { CorpusQueryResult, QueryResult } from 'query/QueryResult'
+import {
+  corpusQueryItemFactory,
+  queryItemFactory,
+} from 'test-support/query-item-factory'
+import TextService from 'corpus/application/TextService'
+import { ChapterDisplay } from 'corpus/domain/chapter'
+import { chapterDisplayFactory } from 'test-support/chapter-fixtures'
+import userEvent from '@testing-library/user-event'
+import { LineDetails } from 'corpus/domain/line-details'
+import { lineVariantDisplayFactory } from 'test-support/dictionary-line-fixtures'
+import { queryItemOf } from 'test-support/utils'
+import DossiersService from 'dossiers/application/DossiersService'
+
+const chance = new Chance('fragmentarium-search-test')
 
 jest.mock('fragmentarium/application/FragmentSearchService')
-jest.mock('fragmentarium/application/FragmentService')
 jest.mock('dictionary/application/WordService')
-jest.mock('dossiers/application/DossiersService')
+jest.mock('fragmentarium/application/FragmentService')
+jest.mock('corpus/application/TextService')
 jest.mock('bibliography/application/BibliographyService')
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useHistory: () => ({
-    push: jest.fn(),
-    location: { pathname: '/library', state: { isAdvancedSearchOpen: false } },
-  }),
-}))
+jest.mock('dossiers/application/DossiersService')
 
+type FragmentariumSearchProps = {
+  fragmentSearchService: jest.Mocked<FragmentSearchService>
+  fragmentService: jest.Mocked<FragmentService>
+  bibliographyService: jest.Mocked<BibliographyService>
+  dossiersService: jest.Mocked<DossiersService>
+  fragmentQuery: Partial<FragmentQuery>
+  wordService: jest.Mocked<WordService>
+  textService: jest.Mocked<TextService>
+  activeTab: string
+} & RouteComponentProps & {
+    location: Location & { state?: { isAdvancedSearchOpen?: boolean } }
+  }
+
+let wordService: jest.Mocked<WordService>
+let textService: jest.Mocked<TextService>
+let bibliographyService: jest.Mocked<BibliographyService>
+let dossiersService: jest.Mocked<DossiersService>
 const fragmentService = new (FragmentService as jest.Mock<
   jest.Mocked<FragmentService>
 >)()
-const fragmentSearchService = new (FragmentSearchService as jest.Mock<
-  jest.Mocked<FragmentSearchService>
->)()
-const wordService = new (WordService as jest.Mock<jest.Mocked<WordService>>)()
-const dossiersService = new (DossiersService as jest.Mock<
-  jest.Mocked<DossiersService>
->)()
-const bibliographyService = new (BibliographyService as jest.Mock<
-  jest.Mocked<BibliographyService>
->)()
+let fragmentSearchService: jest.Mocked<FragmentSearchService>
+let session: Session
+let container: HTMLElement
 
-type FragmentariumProps = RouteComponentProps & {
-  fragmentService: jest.Mocked<FragmentService>
-  fragmentSearchService: jest.Mocked<FragmentSearchService>
-  wordService: jest.Mocked<WordService>
-  dossiersService: jest.Mocked<DossiersService>
-  bibliographyService: jest.Mocked<BibliographyService>
+const renderFragmentariumSearch = async (
+  waitFor: string,
+  query: Partial<FragmentQuery> = {},
+  activeTab = 'library'
+): Promise<void> => {
+  const FragmentariumSearchWithRouter = withRouter<
+    FragmentariumSearchProps,
+    typeof FragmentariumSearch
+  >(FragmentariumSearch)
+  container = render(
+    <MemoryRouter>
+      <DictionaryContext.Provider value={wordService}>
+        <SessionContext.Provider value={session}>
+          <FragmentariumSearchWithRouter
+            fragmentSearchService={fragmentSearchService}
+            fragmentService={fragmentService}
+            bibliographyService={bibliographyService}
+            dossiersService={dossiersService}
+            fragmentQuery={query}
+            wordService={wordService}
+            textService={textService}
+            activeTab={activeTab}
+          />
+        </SessionContext.Provider>
+      </DictionaryContext.Provider>
+    </MemoryRouter>
+  ).container
+  await screen.findByText(waitFor)
 }
 
-describe('Fragmentarium', () => {
-  const initialStatistics = statisticsFactory.build()
-  let session: Session
-  let container: HTMLElement
+beforeEach(async () => {
+  fragmentSearchService = new (FragmentSearchService as jest.Mock<
+    jest.Mocked<FragmentSearchService>
+  >)()
+  wordService = new (WordService as jest.Mock<jest.Mocked<WordService>>)()
+  textService = new (TextService as jest.Mock<jest.Mocked<TextService>>)()
+  bibliographyService = new (BibliographyService as jest.Mock<
+    jest.Mocked<BibliographyService>
+  >)()
+  dossiersService = new (DossiersService as jest.Mock<
+    jest.Mocked<DossiersService>
+  >)()
+  session = new MemorySession(['read:fragments'])
+  fragmentService.fetchPeriods.mockReturnValueOnce(Promise.resolve([]))
+  fragmentService.fetchGenres.mockReturnValueOnce(Promise.resolve([]))
+  fragmentService.fetchProvenances.mockReturnValueOnce(Promise.resolve([]))
+})
 
-  const renderFragmentarium = async (): Promise<void> => {
-    const FragmentariumWithRouter = withRouter<
-      FragmentariumProps,
-      typeof Fragmentarium
-    >(Fragmentarium)
-    await act(async () => {
-      const { container: renderedContainer } = render(
-        <MemoryRouter initialEntries={['/library']}>
-          <SessionContext.Provider value={session}>
-            <DictionaryContext.Provider value={wordService}>
-              <FragmentariumWithRouter
-                fragmentService={fragmentService}
-                fragmentSearchService={fragmentSearchService}
-                wordService={wordService}
-                dossiersService={dossiersService}
-                bibliographyService={bibliographyService}
-              />
-            </DictionaryContext.Provider>
-          </SessionContext.Provider>
-        </MemoryRouter>
+describe('Search', () => {
+  let fragments: Fragment[]
+
+  describe('Searching fragments by number', () => {
+    const museumNumber = 'K.2'
+
+    beforeEach(async () => {
+      fragments = fragmentFactory.buildList(2, {}, { transient: { chance } })
+      fragmentService.query.mockReturnValueOnce(
+        Promise.resolve({
+          items: fragments.map(queryItemOf),
+          matchCountTotal: 0,
+        })
       )
-      container = renderedContainer
-    })
-    await screen.findByText('Current size of the Library:')
-  }
-
-  const mockAllServiceMethods = (): void => {
-    ;[
-      fragmentService,
-      fragmentSearchService,
-      wordService,
-      dossiersService,
-      bibliographyService,
-    ].forEach((service) => {
-      Object.keys(service).forEach((key) => {
-        if (typeof service[key] === 'function') {
-          service[key].mockReturnValue(Bluebird.resolve())
-        }
+      fragmentService.find
+        .mockReturnValueOnce(Promise.resolve(fragments[0]))
+        .mockReturnValueOnce(Promise.resolve(fragments[1]))
+      wordService.findAll.mockReturnValue(Promise.resolve([]))
+      textService.query.mockReturnValueOnce(
+        Promise.resolve({ items: [], matchCountTotal: 0 })
+      )
+      await renderFragmentariumSearch(fragments[0].number, {
+        number: museumNumber,
       })
     })
-  }
 
-  const testStatisticDisplay = (
-    description: string,
-    statisticValue: number
-  ): void => {
-    it(description, () => {
-      expect(container).toHaveTextContent(statisticValue.toLocaleString())
+    it('Displays result on successful query', async () => {
+      expect(container).toHaveTextContent(fragments[1].number)
     })
-  }
 
-  const testAdvancedSearchInteraction = async (
-    description: string,
-    assertion: () => void
-  ): Promise<void> => {
-    it(description, async () => {
-      expect(screen.getByRole('img')).toBeInTheDocument()
-      await act(async () => {
-        fireEvent.click(screen.getByText('Advanced Search'))
+    it('Fills in search form query', () => {
+      expect(screen.getByLabelText('Number')).toHaveValue(museumNumber)
+    })
+  })
+
+  it('Shows suggestion when entering wrong number format', async () => {
+    fragmentService.query.mockReturnValueOnce(
+      Promise.resolve({
+        items: [],
+        matchCountTotal: 0,
       })
-      assertion()
-    })
-  }
-
-  beforeEach(() => {
-    mockAllServiceMethods()
-    fragmentService.statistics.mockReturnValue(
-      Bluebird.resolve(initialStatistics)
     )
-    wordService.findAll.mockReturnValue(Bluebird.resolve([]))
-    fragmentService.fetchPeriods.mockReturnValue(Bluebird.resolve([]))
-    fragmentService.fetchGenres.mockReturnValue(Bluebird.resolve([]))
-    fragmentService.fetchProvenances.mockReturnValue(Bluebird.resolve([]))
+    wordService.findAll.mockReturnValue(Promise.resolve([]))
+    textService.query.mockReturnValueOnce(
+      Promise.resolve({ items: [], matchCountTotal: 0 })
+    )
+    await renderFragmentariumSearch('K.2', {
+      number: 'K 2',
+    })
+
+    expect(container).toHaveTextContent('Did you mean K.2?')
   })
+})
 
-  describe('Statistics', () => {
-    beforeEach(async () => {
-      session = new MemorySession([])
-      await renderFragmentarium()
-    })
+describe('Searching fragments by transliteration', () => {
+  let result: QueryResult
+  let corpusResult: CorpusQueryResult
+  let fragments: Fragment[]
+  let chapters: ChapterDisplay[]
+  const transliteration = 'LI₂₃ ši₂-ṣa-pel₃-ṭa₃'
 
-    testStatisticDisplay(
-      'Shows the number of indexed tablets',
-      initialStatistics.totalFragments
-    )
-    testStatisticDisplay(
-      'Shows the number of transliterated tablets',
-      initialStatistics.transliteratedFragments
-    )
-
-    it('shows the ApiImage when advanced search is closed', () => {
-      expect(screen.getByRole('img')).toBeInTheDocument()
-    })
-  })
-
-  describe('Fragment lists', () => {
-    let latest: Fragment
-    let needsRevision: FragmentInfo
-
-    beforeEach(async () => {
-      latest = fragmentFactory.build()
-      session = new MemorySession(['read:fragments', 'transliterate:fragments'])
-      fragmentService.queryLatest.mockReturnValue(
-        Bluebird.resolve({ items: [queryItemOf(latest)], matchCountTotal: 0 })
+  beforeEach(async () => {
+    fragments = fragmentFactory.buildList(2, {}, { transient: { chance } })
+    chapters = chapterDisplayFactory.buildList(2, {}, { transient: { chance } })
+    result = {
+      items: fragments.map((fragment) =>
+        queryItemFactory.build({
+          museumNumber: fragment.number,
+        })
+      ),
+      matchCountTotal: 2,
+    }
+    corpusResult = {
+      items: chapters.map((chapter) =>
+        corpusQueryItemFactory.build({
+          textId: chapter.id.textId,
+          stage: chapter.id.stage,
+          name: chapter.id.name,
+        })
+      ),
+      matchCountTotal: 0,
+    }
+    fragmentService.query.mockReturnValueOnce(Promise.resolve(result))
+    textService.query.mockReturnValueOnce(Promise.resolve(corpusResult))
+    fragmentService.find
+      .mockReturnValueOnce(Promise.resolve(fragments[0]))
+      .mockReturnValueOnce(Promise.resolve(fragments[1]))
+    fragmentService.findThumbnail
+      .mockReturnValueOnce(
+        Promise.resolve({
+          blob: new Blob(['imagedata'], { type: 'image/jpeg' }),
+        })
       )
-      fragmentService.find.mockReturnValue(Bluebird.resolve(latest))
-      needsRevision = fragmentInfoFactory.build()
-      fragmentSearchService.fetchNeedsRevision.mockReturnValue(
-        Bluebird.resolve([needsRevision])
+      .mockReturnValueOnce(Promise.resolve({ blob: null }))
+    wordService.findAll.mockReturnValue(Promise.resolve([]))
+    textService.findChapterDisplay
+      .mockReturnValueOnce(Promise.resolve(chapters[0]))
+      .mockReturnValueOnce(Promise.resolve(chapters[1]))
+    textService.findChapterLine.mockReturnValue(
+      Promise.resolve(
+        new LineDetails(
+          [
+            lineVariantDisplayFactory.build({
+              reconstruction: [],
+              manuscripts: [],
+            }),
+          ],
+          0
+        )
       )
-      await renderFragmentarium()
-    })
+    )
 
-    it('shows the latest additions', () => {
-      expect(screen.getByText(latest.number)).toBeInTheDocument()
-    })
-
-    it('shows the fragments needing revision', () => {
-      expect(screen.getByText(needsRevision.number)).toBeInTheDocument()
+    await renderFragmentariumSearch(result.items[0].museumNumber, {
+      transliteration,
     })
   })
 
-  describe('Advanced Search', () => {
-    beforeEach(async () => {
-      session = new MemorySession(['read:fragments'])
-      await renderFragmentarium()
-    })
-
-    testAdvancedSearchInteraction(
-      'hides the ApiImage when advanced search is open',
-      () => {
-        expect(screen.queryByRole('img')).not.toBeInTheDocument()
-      }
-    )
-
-    testAdvancedSearchInteraction(
-      'expands the SearchForm to full width when advanced search is open',
-      () => {
-        expect(screen.queryByRole('img')).not.toBeInTheDocument()
-      }
+  it('Fills in search form query', () => {
+    expect(screen.getByLabelText('Transliteration')).toHaveValue(
+      transliteration
     )
   })
 
-  describe('Conditional Rendering', () => {
-    it('does not render LatestTransliterations when user cannot read fragments', async () => {
-      session = new MemorySession([])
-      await renderFragmentarium()
-      expect(
-        screen.queryByText('Latest Transliterations')
-      ).not.toBeInTheDocument()
-    })
+  it('Displays Library result on successful query', async () => {
+    expect(container).toHaveTextContent(result.items[1].museumNumber)
+  })
 
-    it('does not render NeedsRevision when user cannot transliterate fragments', async () => {
-      session = new MemorySession(['read:fragments'])
-      await renderFragmentarium()
-      expect(screen.queryByText('Needs Revision')).not.toBeInTheDocument()
-    })
+  it('Displays corpus results when clicking corpus tab', async () => {
+    userEvent.click(screen.getByRole('tab', { name: 'Corpus' }))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Updates URL anchor when clicking tab', async () => {
+    userEvent.click(screen.getByRole('tab', { name: 'Corpus' }))
+    expect(global.window.location.hash).toEqual('#corpus')
+
+    userEvent.click(screen.getByRole('tab', { name: 'Library' }))
+    expect(global.window.location.hash).toEqual('#library')
   })
 })
