18d56307b2cfbc956af0bda0b6e6320cd8a6ef60
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-05 19:39:46 +0000

    extend LemmaAnnotationForm tests for onChange and onKeyDown

--- src/fragmentarium/ui/fragment/lemma-annotation/LemmaAnnotationForm.test.tsx
+++ src/fragmentarium/ui/fragment/lemma-annotation/LemmaAnnotationForm.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-import Promise from 'bluebird'
 import LemmaAnnotationForm from './LemmaAnnotationForm'
 import WordService from 'dictionary/application/WordService'
 import EditableToken from 'fragmentarium/ui/fragment/linguistic-annotation/EditableToken'
 import { kurToken } from 'test-support/test-tokens'
 import { wordFactory } from 'test-support/word-fixtures'
 import Word from 'dictionary/domain/Word'
+import { act } from 'react-dom/test-utils'
 
 jest.mock('dictionary/application/WordService')
 
@@ -14,16 +14,24 @@ const MockWordService = WordService as jest.Mock<jest.Mocked<WordService>>
 const wordServiceMock = new MockWordService()
 
 let token: EditableToken
-const word: Word = wordFactory.build({ _id: 'foo' })
+const word: Word = wordFactory.build({
+  _id: 'foo I',
+  lemma: ['foo'],
+  homonym: 'I',
+})
+
+const onTab = jest.fn()
+const onShiftTab = jest.fn()
+const onChange = jest.fn()
 
 const renderLemmaAnnotationForm = () => {
   render(
     <LemmaAnnotationForm
       token={token}
       wordService={wordServiceMock}
-      onChange={jest.fn()}
-      onTab={jest.fn()}
-      onShiftTab={jest.fn()}
+      onChange={onChange}
+      onTab={onTab}
+      onShiftTab={onShiftTab}
     />
   )
 }
@@ -32,8 +40,7 @@ describe('LemmaAnnotationForm', () => {
   beforeEach(() => {
     token = new EditableToken(kurToken, 0, 0, 0, [])
     token.isSelected = true
-    wordServiceMock.findAll.mockReturnValue(Promise.resolve([word]))
-    wordServiceMock.searchLemma.mockReturnValue(Promise.resolve([]))
+    wordServiceMock.searchLemma.mockResolvedValue([word])
   })
   it('renders the select component', () => {
     renderLemmaAnnotationForm()
@@ -47,11 +54,37 @@ describe('LemmaAnnotationForm', () => {
     await waitFor(() =>
       expect(wordServiceMock.searchLemma).toHaveBeenCalledWith('lem')
     )
+    expect(screen.getByText('foo')).toBeInTheDocument()
   })
 
   it('disables the select when token is not selected', () => {
     token.unselect()
     renderLemmaAnnotationForm()
     expect(screen.getByLabelText('edit-token-lemmas')).toBeDisabled()
+  })
+
+  it('calls onChange when selecting a lemma', async () => {
+    renderLemmaAnnotationForm()
+    const input = screen.getByLabelText('edit-token-lemmas')
+    fireEvent.change(input, { target: { value: 'lem' } })
+    await waitFor(() =>
+      expect(wordServiceMock.searchLemma).toHaveBeenCalledWith('lem')
+    )
+    fireEvent.click(screen.getByText('foo'))
+    expect(onChange).toHaveBeenCalled()
+  })
+
+  it('selects the next or previous token', async () => {
+    renderLemmaAnnotationForm()
+    const input = screen.getByLabelText('edit-token-lemmas')
+    await act(async () => {
+      fireEvent.keyDown(input, { key: 'Tab', code: 'Tab' })
+    })
+    expect(onTab).toHaveBeenCalled()
+
+    await act(async () => {
+      fireEvent.keyDown(input, { key: 'Tab', code: 'Tab', shiftKey: true })
+    })
+    expect(onShiftTab).toHaveBeenCalled()
   })
 })
