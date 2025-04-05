9bc9d97293f8df3f62e86e22c8db8b071868ceab
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-05 17:47:07 +0000

    test modal callbacks

--- src/fragmentarium/ui/fragment/lemma-annotation/LemmaEditorModal.test.tsx
+++ src/fragmentarium/ui/fragment/lemma-annotation/LemmaEditorModal.test.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, act } from '@testing-library/react'
 import LemmaEditorModal from './LemmaEditorModal'
 import WordService from 'dictionary/application/WordService'
 import EditableToken from 'fragmentarium/ui/fragment/linguistic-annotation/EditableToken'
 import { kurToken } from 'test-support/test-tokens'
+import { wordFactory } from 'test-support/word-fixtures'
 
 jest.mock('dictionary/application/WordService')
 
 const MockWordService = WordService as jest.Mock<jest.Mocked<WordService>>
 const wordServiceMock = new MockWordService()
 const token = new EditableToken(kurToken, 0, 0, 0, [])
+const mockWord = wordFactory.build({
+  _id: 'mockLemma',
+  lemma: ['mockLemma'],
+  homonym: 'I',
+})
 const mockCallbacks = {
   handleChange: jest.fn(),
   selectNextToken: jest.fn(),
@@ -22,8 +28,12 @@ const mockCallbacks = {
   onMultiApply: jest.fn(),
   onMultiReset: jest.fn(),
 }
+const confirmSuggestionSpy = jest.spyOn(token, 'confirmSuggestion')
 
 describe('LemmaEditorModal', () => {
+  beforeEach(() => {
+    wordServiceMock.searchLemma.mockResolvedValue([mockWord])
+  })
   it('renders modal with title', () => {
     render(
       <LemmaEditorModal
@@ -79,5 +89,43 @@ describe('LemmaEditorModal', () => {
     )
     fireEvent.click(screen.getByLabelText('save-updates'))
     expect(mockCallbacks.saveUpdates).toHaveBeenCalled()
+  })
+
+  it('calls handleChange on changing input', async () => {
+    render(
+      <LemmaEditorModal
+        token={token}
+        title="Lemma Editor"
+        process={null}
+        isDirty={true}
+        wordService={wordServiceMock}
+        {...mockCallbacks}
+      />
+    )
+    const input = screen.getByLabelText('edit-token-lemmas')
+    await act(async () => {
+      fireEvent.change(input, { target: { value: 'mock' } })
+    })
+    const suggestion = await screen.findByText('mockLemma')
+    fireEvent.click(suggestion)
+    expect(mockCallbacks.handleChange).toHaveBeenCalled()
+  })
+  it('calls submit callbacks', async () => {
+    render(
+      <LemmaEditorModal
+        token={token}
+        title="Lemma Editor"
+        process={null}
+        isDirty={true}
+        wordService={wordServiceMock}
+        {...mockCallbacks}
+      />
+    )
+    const input = screen.getByLabelText('edit-token-lemmas')
+    await act(async () => {
+      fireEvent.submit(input)
+    })
+    expect(mockCallbacks.selectNextToken).toHaveBeenCalled()
+    expect(confirmSuggestionSpy).toHaveBeenCalled()
   })
 })
