5a4bf9051aaa5858dd0d0fe2862e492cbbc4749e
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-10 15:40:26 +0000

    add MetaDeleteButton

--- src/fragmentarium/ui/info/MetaEditButton.tsx
+++ src/fragmentarium/ui/info/MetaEditButton.tsx
@@ -16,13 +16,42 @@ export default function MetaEditButton({
       {(session: Session): ReactNode =>
         session.isAllowedToTransliterateFragments() ? (
           <Button
-            aria-label="Browse genres button"
+            aria-label="Browse genres"
             variant="light"
             ref={target}
             className={classNames([
               'far fa-edit',
               'mh-100',
-              'Details__edit-button',
+              'Details__meta-button',
+            ])}
+            onClick={onClick}
+          />
+        ) : null
+      }
+    </SessionContext.Consumer>
+  )
+}
+
+export function MetaDeleteButton({
+  onClick,
+  target,
+}: {
+  onClick: React.MouseEventHandler<HTMLElement>
+  target?: React.RefObject<HTMLButtonElement>
+}): JSX.Element {
+  return (
+    <SessionContext.Consumer>
+      {(session: Session): ReactNode =>
+        session.isAllowedToTransliterateFragments() ? (
+          <Button
+            aria-label="Delete genre"
+            variant="light"
+            ref={target}
+            className={classNames([
+              'fas fa-trash',
+              'mh-100',
+              'Details__meta-button',
+              'show-on-hover',
             ])}
             onClick={onClick}
           />
