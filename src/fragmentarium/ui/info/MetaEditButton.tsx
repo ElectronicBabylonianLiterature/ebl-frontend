e4c832f6042658008e0a4b1a186d330a5cd76af5
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-14 13:48:21 +0000

    refactor buttons

--- src/fragmentarium/ui/info/MetaEditButton.tsx
+++ src/fragmentarium/ui/info/MetaEditButton.tsx
@@ -1,59 +1,55 @@
 import React, { ReactNode } from 'react'
 import classNames from 'classnames'
-import { Button } from 'react-bootstrap'
+import { Button, ButtonProps } from 'react-bootstrap'
 import { Session } from 'auth/Session'
 import SessionContext from 'auth/SessionContext'
 
 export function MetaEditButton({
-  onClick,
   target,
+  ...props
 }: {
-  onClick: React.MouseEventHandler<HTMLElement>
   target?: React.RefObject<HTMLButtonElement>
-}): JSX.Element {
+} & ButtonProps): JSX.Element {
   return (
     <SessionContext.Consumer>
       {(session: Session): ReactNode =>
         session.isAllowedToTransliterateFragments() ? (
           <Button
-            aria-label="Browse genres"
             variant="light"
-            ref={target}
+            {...props}
             className={classNames([
-              'far fa-edit',
               'mh-100',
               'Details__meta-button',
+              'far fa-edit',
+              props.className,
             ])}
-            onClick={onClick}
+            ref={target}
           />
         ) : null
       }
     </SessionContext.Consumer>
   )
 }
-
 export function MetaDeleteButton({
-  onClick,
   target,
+  ...props
 }: {
-  onClick: React.MouseEventHandler<HTMLElement>
   target?: React.RefObject<HTMLButtonElement>
-}): JSX.Element {
+} & ButtonProps): JSX.Element {
   return (
     <SessionContext.Consumer>
       {(session: Session): ReactNode =>
         session.isAllowedToTransliterateFragments() ? (
           <Button
-            aria-label="Delete genre"
             variant="light"
-            ref={target}
+            {...props}
             className={classNames([
-              'fas fa-trash',
               'mh-100',
               'Details__meta-button',
-              'show-on-hover',
+              'fas fa-trash',
+              props.className,
             ])}
-            onClick={onClick}
+            ref={target}
           />
         ) : null
       }
