38c2523a58ccdd01a63e0454b490e8982b6f81b1
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-15 14:29:46 +0000

    add /record route

---
+++ src/fragmentarium/ui/fragment/RecordView.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import AppContent from 'common/AppContent'
+import { SectionCrumb, TextCrumb } from 'common/Breadcrumbs'
+import FragmentService from 'fragmentarium/application/FragmentService'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import FragmentCrumb from 'fragmentarium/ui/FragmentCrumb'
+import { RecordList } from 'fragmentarium/ui/info/Record'
+import withData from 'http/withData'
+import './RecordView.sass'
+
+function RecordView({ fragment }: { fragment: Fragment }): JSX.Element {
+  console.log(fragment.record)
+  return (
+    <AppContent
+      crumbs={[
+        new SectionCrumb('Library'),
+        new FragmentCrumb(fragment.number),
+        new TextCrumb('Record'),
+      ]}
+      title={`Record of ${fragment.number}`}
+    >
+      <section className={'FullRecord'}>
+        <RecordList record={fragment.record} />
+      </section>
+    </AppContent>
+  )
+}
+
+const FragmentWithData = withData<
+  unknown,
+  { number: string; fragmentService: FragmentService },
+  Fragment
+>(
+  ({ data, ...props }) => <RecordView fragment={data} {...props} />,
+  (props) => props.fragmentService.find(props.number)
+)
+export default FragmentWithData
