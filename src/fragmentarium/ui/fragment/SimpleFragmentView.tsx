5c43bfb77a2cddc485a740fd99b996dd9ccd82e2
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-17 12:38:37 +0000

    add SimpleFragmentView component

---
+++ src/fragmentarium/ui/fragment/SimpleFragmentView.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Fragment } from 'fragmentarium/domain/fragment'
+import withData from 'http/withData'
+import FragmentService from 'fragmentarium/application/FragmentService'
+import { Session } from 'auth/Session'
+import { Transliteration } from 'transliteration/ui/Transliteration'
+
+function SimpleView({
+  session,
+  fragment,
+}: {
+  session: Session
+  fragment: Fragment
+}): JSX.Element {
+  return session.isAllowedToReadFragments() ? (
+    <Transliteration text={fragment.text} />
+  ) : (
+    <>{"You don't have permissions to view this fragment."}</>
+  )
+}
+
+const SimpleFragmentView = withData<
+  {
+    fragmentService: FragmentService
+    session: Session
+  },
+  { number: string },
+  Fragment
+>(
+  ({ data, ...props }) => <SimpleView fragment={data} {...props} />,
+  (props) =>
+    props.fragmentService
+      .find(props.number)
+      .then((fragment) => fragment.filterFolios(props.session)),
+  {
+    watch: (props) => [props.number],
+  }
+)
+export default SimpleFragmentView
