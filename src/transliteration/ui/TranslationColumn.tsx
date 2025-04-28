bf073fdfa0e2679867a32b5f6334e93c60d933da
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-28 14:59:51 +0000

    add getTranslationLines

--- src/transliteration/ui/TranslationColumn.tsx
+++ src/transliteration/ui/TranslationColumn.tsx
@@ -1,44 +1,37 @@
+import _ from 'lodash'
 import React from 'react'
 import { AbstractLine } from 'transliteration/domain/abstract-line'
-import lineNumberToString from 'transliteration/domain/lineNumberToString'
-import { TextLine } from 'transliteration/domain/text-line'
 import TranslationLine from 'transliteration/domain/translation-line'
 import Markup from 'transliteration/ui/markup'
 import TransliterationTd from 'transliteration/ui/TransliterationTd'
 
+function getTranslationLines(
+  lines: readonly AbstractLine[],
+  lineIndex: number
+): TranslationLine[] {
+  return _.takeWhile(
+    lines.slice(lineIndex + 1),
+    (line) => line.type === 'TranslationLine'
+  ) as TranslationLine[]
+}
+
 export default function TranslationColumn({
+  language = 'en',
   lines,
   lineIndex,
 }: {
   lines: readonly AbstractLine[]
   lineIndex: number
+  language: string
 }): JSX.Element {
-  const hasTranslation =
-    lines[lineIndex].type === 'TextLine' &&
-    lines[lineIndex + 1]?.type === 'TranslationLine'
-  if (hasTranslation) {
-    const line = lines[lineIndex] as TextLine
-    const translationLine = lines[lineIndex + 1] as TranslationLine
-    return (
-      <>
-        <td>
-          <sup>
-            ({lineNumberToString(line.lineNumber)}
-            {translationLine.extent && (
-              <>-{lineNumberToString(translationLine.extent.number)}</>
-            )}
-            )
-          </sup>
-        </td>
-        <TransliterationTd type="TranslationLine">
-          <Markup container={'span'} parts={translationLine.parts} />
-        </TransliterationTd>
-      </>
-    )
-  }
-  return hasTranslation ? (
+  const translationLine = _.find(
+    getTranslationLines(lines, lineIndex),
+    (line) => line.language === language
+  )
+
+  return translationLine ? (
     <TransliterationTd type="TranslationLine">
-      <Markup parts={(lines[lineIndex + 1] as TranslationLine).parts} />
+      <Markup container={'span'} parts={translationLine.parts} />
     </TransliterationTd>
   ) : (
     <td></td>
