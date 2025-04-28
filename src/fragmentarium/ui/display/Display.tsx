e0ed666e0285152bbf257c24755a9750eb8c758c
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-04-28 15:06:43 +0000

    add translation display parameters

--- src/fragmentarium/ui/display/Display.tsx
+++ src/fragmentarium/ui/display/Display.tsx
@@ -5,12 +5,23 @@ import { Transliteration } from 'transliteration/ui/Transliteration'
 import WordService from 'dictionary/application/WordService'
 import { MarkupPart } from 'transliteration/domain/markup'
 import { MarkupText } from 'markup/ui/markup'
+import Select, { ValueType } from 'react-select'
+import TranslationLine from 'transliteration/domain/translation-line'
 
 interface Props {
   fragment: Fragment
   wordService: WordService
   activeLine: string
 }
+
+type LanguageOption = ValueType<{ label: string; value: string }, false>
+
+const languages = [
+  { label: 'English', value: 'en' },
+  { label: 'Arabic', value: 'ar' },
+  { label: 'German', value: 'de' },
+  { label: 'Spanish', value: 'es' },
+]
 
 function MarkupSection({
   title,
@@ -28,6 +39,19 @@ function MarkupSection({
 }
 
 function Display({ fragment, wordService, activeLine }: Props): JSX.Element {
+  const languagesInFragment = new Set(
+    fragment.text.lines
+      .filter((line) => line.type === 'TranslationLine')
+      .map((line) => (line as TranslationLine).language)
+  )
+  const availableLanguages = languages.filter((option) =>
+    languagesInFragment.has(option.value)
+  )
+  const defaultLanguage = availableLanguages[0]
+  const [language, setLanguage] = React.useState<LanguageOption | null>(
+    defaultLanguage
+  )
+
   return (
     <>
       {fragment.introduction.parts.length > 0 && (
@@ -36,7 +60,20 @@ function Display({ fragment, wordService, activeLine }: Props): JSX.Element {
           parts={fragment.introduction.parts}
         />
       )}
-      <Transliteration text={fragment.text} activeLine={activeLine} />
+      <section>
+        <Select
+          aria-label="select-language"
+          options={availableLanguages}
+          onChange={setLanguage}
+          value={language}
+        />
+      </section>
+      <Transliteration
+        text={fragment.text}
+        activeLine={activeLine}
+        translationStyle={'standoff'}
+        translationLanguage={language?.value}
+      />
       {fragment.notes.text.trim() && (
         <MarkupSection title={'eBL Notes'} parts={fragment.notes.parts} />
       )}
--- src/transliteration/ui/Transliteration.tsx
+++ src/transliteration/ui/Transliteration.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { Text } from 'transliteration/domain/text'
-import './Transliteration.sass'
-import TransliterationNotes from './TransliterationNotes'
-import TransliterationLines from './TransliterationLines'
+import 'transliteration/ui/Transliteration.sass'
+import TransliterationNotes from 'transliteration/ui/TransliterationNotes'
+import TransliterationLines, {
+  TranslationStyle,
+} from 'transliteration/ui/TransliterationLines'
 
 export function Transliteration({
   text,
   activeLine = '',
+  translationStyle = 'inline',
+  translationLanguage,
 }: {
   text: Text
   activeLine?: string
+  translationStyle?: TranslationStyle
+  translationLanguage?: string
 }): JSX.Element {
   return (
     <section className="Transliteration">
       <TransliterationLines
         text={text}
         activeLine={activeLine}
-        translation={'standoff'}
+        translationStyle={translationStyle}
+        translationLanguage={translationLanguage}
       />
       <TransliterationNotes notes={text.notes} />
     </section>
