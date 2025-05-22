bae745f5907715c73ac8e9ce5267ad3325cb0636
Ilya Khait <ilya.khait@lmu.de>
2025-05-22 12:24:45 +0000

    Date king adjustments

--- src/chronology/domain/Date.ts
+++ src/chronology/domain/Date.ts
@@ -1,8 +1,13 @@
 import { MesopotamianDateDto } from 'fragmentarium/domain/FragmentDtos'
 import { MesopotamianDateString } from 'chronology/domain/DateString'
+import { findKingByOrderGlobal } from 'chronology/ui/Kings/Kings'
 
 export class MesopotamianDate extends MesopotamianDateString {
   static fromJson(dateJson: MesopotamianDateDto): MesopotamianDate {
-    return new MesopotamianDate({ ...dateJson })
+    const kingOrderGlobal = dateJson?.king?.orderGlobal
+    const king = kingOrderGlobal
+      ? findKingByOrderGlobal(kingOrderGlobal)
+      : undefined
+    return new MesopotamianDate({ ...dateJson, ...{ king: king ?? undefined } })
   }
 }
