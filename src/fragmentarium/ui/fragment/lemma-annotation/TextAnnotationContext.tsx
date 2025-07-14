cf6e17c3055521b17dfc5c9df6ece99e4037a4af
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-07-14 13:38:51 +0000

    add TextAnnotationContext

---
+++ src/fragmentarium/ui/fragment/lemma-annotation/TextAnnotationContext.tsx
@@ -0,0 +1,39 @@
+import React, { Dispatch, useReducer } from 'react'
+import { EntityType } from 'fragmentarium/ui/fragment/lemma-annotation/SpanAnnotator'
+
+export interface Entity {
+  id: string
+  span: readonly string[]
+  type: EntityType
+}
+
+type State = ReadonlyArray<Entity>
+export type AnnotationContextService = [State, Dispatch<Action>]
+
+type AddAction = {
+  type: 'add'
+  entity: Entity
+}
+
+export type Action = AddAction
+
+const AnnotationContext = React.createContext<AnnotationContextService>([
+  [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  () => {},
+])
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.entity]
+  }
+}
+
+export function useAnnotationContext(
+  initial: Entity[] = []
+): AnnotationContextService {
+  return useReducer(reducer, initial)
+}
+
+export default AnnotationContext
