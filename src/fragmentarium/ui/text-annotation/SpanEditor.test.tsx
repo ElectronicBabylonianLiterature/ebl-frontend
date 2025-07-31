a37e8a77e49b9aebaf8b9e1f053ad2920214f18d
fsimonjetz <fabian.simonjetz@ruhr-uni-bochum.de>
2025-07-31 15:08:58 +0000

    extend SpanEditor tests

--- src/fragmentarium/ui/text-annotation/SpanEditor.test.tsx
+++ src/fragmentarium/ui/text-annotation/SpanEditor.test.tsx
@@ -3,11 +3,27 @@ import {
   EntityAnnotationSpan,
   EntityTypes,
 } from 'fragmentarium/ui/text-annotation/EntityType'
-import { act, render } from '@testing-library/react'
+import { screen, act, render } from '@testing-library/react'
 import SpanEditor from 'fragmentarium/ui/text-annotation/SpanEditor'
+import userEvent from '@testing-library/user-event'
 
 let container: HTMLElement
 const setActiveSpanId = jest.fn()
+const mockDispatch = jest.fn()
+const buildingName = 'BN: Building Name'
+
+jest.mock('fragmentarium/ui/text-annotation/TextAnnotationContext', () => ({
+  default: {
+    Consumer: ({ children }) => children([{ entities: [] }, mockDispatch]),
+    Provider: ({ children }) => children,
+  },
+}))
+
+const mockUseContext = jest.fn()
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useContext: () => mockUseContext(),
+}))
 
 const entitySpan: EntityAnnotationSpan = {
   id: 'Entity-1',
@@ -19,6 +35,8 @@ const entitySpan: EntityAnnotationSpan = {
 
 describe('SpanEditor', () => {
   beforeEach(async () => {
+    jest.clearAllMocks()
+    mockUseContext.mockReturnValue([{ entities: [] }, mockDispatch])
     await act(async () => {
       container = render(
         <SpanEditor entitySpan={entitySpan} setActiveSpanId={setActiveSpanId} />
@@ -27,5 +45,31 @@ describe('SpanEditor', () => {
   })
   it('shows the selection menu', () => {
     expect(container).toMatchSnapshot()
+  })
+  it('updates the annotation', () => {
+    const input = screen.getByLabelText('edit-named-entity')
+    userEvent.click(input)
+    userEvent.click(screen.getByText(buildingName))
+    userEvent.click(screen.getByLabelText('update-name-annotation'))
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'edit',
+      entity: {
+        id: entitySpan.id,
+        type: 'BUILDING_NAME',
+        span: entitySpan.span,
+      },
+    })
+  })
+  it('deletes the annotation', () => {
+    const input = screen.getByLabelText('edit-named-entity')
+    userEvent.click(input)
+    userEvent.click(screen.getByText(buildingName))
+    userEvent.click(screen.getByLabelText('delete-name-annotation'))
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'delete',
+      entity: entitySpan,
+    })
   })
 })
