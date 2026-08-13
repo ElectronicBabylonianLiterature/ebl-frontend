10a57d6eee1782c4c9a74dc2c50fb4d373b49ab8
Enrique Jiménez <39047733+ejimsan@users.noreply.github.com>
2026-08-13 09:24:51 +0000

    Extract AlignmentPopover into its own module

--- src/transliteration/ui/AlignmentPopover.test.tsx
+++ src/transliteration/ui/AlignmentPopover.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import { AlignmentPopover } from './WordInfo'
+import { AlignmentPopover } from './AlignmentPopover'
 import { LineGroup } from './LineGroup'
 import {
   highlightIndexSetterMock,
--- src/transliteration/ui/WordInfo.tsx
+++ src/transliteration/ui/WordInfo.tsx
@@ -7,15 +7,13 @@ import { useDictionary } from 'dictionary/ui/dictionary-context'
 
 import './WordInfo.sass'
 import { LineGroup } from './LineGroup'
-import { Alignments } from './WordInfoAlignments'
 import LemmaInfo from './WordInfoLemmas'
-import { isAkkadianWord, isAnyWord } from 'transliteration/domain/type-guards'
+import { isAnyWord } from 'transliteration/domain/type-guards'
 import { TokenActionWrapperProps } from 'transliteration/ui/LineAccumulator'
-import AkkadianWordAnalysis from 'akkadian/ui/akkadianWordAnalysis'
 import { LineLemmasContext, useLineLemmasContext } from './LineLemmasContext'
 import RouterLinkModeContext from 'common/ui/RouterLinkModeContext'
 
-function VariantAlignmentIndicator({
+export function VariantAlignmentIndicator({
   children,
   token,
 }: {
@@ -33,7 +31,7 @@ function VariantAlignmentIndicator({
   )
 }
 
-function PopoverTitle({
+export function PopoverTitle({
   children,
 }: {
   children?: React.ReactNode | undefined
@@ -45,147 +43,8 @@ function PopoverTitle({
   )
 }
 
-function hasLemma(token: Token): token is AnyWord {
+export function hasLemma(token: Token): token is AnyWord {
   return isAnyWord(token) && token.uniqueLemma.length > 0
-}
-
-function AlignmentHoverTrigger({
-  token,
-  lineGroup,
-  children,
-}: PropsWithChildren<{
-  token: AnyWord
-  lineGroup: LineGroup
-}>): JSX.Element {
-  return (
-    <span
-      className="word-info__hover-trigger"
-      onMouseEnter={() =>
-        lineGroup.setActiveTokenIndex(token.sentenceIndex || 0)
-      }
-      onMouseLeave={() => lineGroup.setActiveTokenIndex(0)}
-    >
-      <VariantAlignmentIndicator token={token}>
-        {children}
-      </VariantAlignmentIndicator>
-    </span>
-  )
-}
-
-function AlignmentInfoPopover({
-  token,
-  lineGroup,
-  showMeter,
-  showIpa,
-  children,
-}: PropsWithChildren<{
-  token: AnyWord
-  lineGroup: LineGroup
-  showMeter: boolean
-  showIpa: boolean
-}>): JSX.Element {
-  const dictionary = useDictionary()
-  const { lemmaMap, lemmaSetter } = useLineLemmasContext()
-
-  const popover = (
-    <Popover id={_.uniqueId('word-info-')}>
-      <PopoverTitle>{children}</PopoverTitle>
-      <Popover.Body>
-        <LineLemmasContext.Provider
-          value={{
-            lemmaMap: lemmaMap,
-            lemmaSetter: lemmaSetter,
-          }}
-        >
-          <LemmaInfo
-            word={token}
-            dictionary={dictionary}
-            manuscriptLines={lineGroup.manuscriptLines}
-          />
-          <Alignments
-            tokenIndex={token.sentenceIndex}
-            lemma={token.uniqueLemma}
-            lineGroup={lineGroup}
-            dictionary={dictionary}
-          />
-        </LineLemmasContext.Provider>
-      </Popover.Body>
-    </Popover>
-  )
-
-  return (
-    <span className={'word-info__wrapper word-info__alignment-trigger'}>
-      <OverlayTrigger
-        trigger="click"
-        rootClose
-        placement="top"
-        overlay={popover}
-      >
-        <span
-          className="word-info__trigger"
-          role="button"
-          onMouseEnter={() =>
-            lineGroup.setActiveTokenIndex(token.sentenceIndex || 0)
-          }
-          onMouseLeave={() => lineGroup.setActiveTokenIndex(0)}
-        >
-          <VariantAlignmentIndicator token={token}>
-            {children}
-          </VariantAlignmentIndicator>
-        </span>
-      </OverlayTrigger>
-      {isAkkadianWord(token) && (
-        <AkkadianWordAnalysis
-          word={token}
-          showMeter={showMeter}
-          showIpa={showIpa}
-        />
-      )}
-    </span>
-  )
-}
-
-export function AlignmentPopover({
-  token,
-  children,
-  showMeter = false,
-  showIpa = false,
-  lineGroup,
-}: TokenActionWrapperProps & {
-  lineGroup: LineGroup
-  showMeter?: boolean
-  showIpa?: boolean
-}): JSX.Element {
-  const useRouterLinks = React.useContext(RouterLinkModeContext)
-
-  if (!useRouterLinks) {
-    return (
-      <VariantAlignmentIndicator token={token}>
-        {children}
-      </VariantAlignmentIndicator>
-    )
-  }
-
-  if (hasLemma(token)) {
-    return (
-      <AlignmentInfoPopover
-        token={token}
-        lineGroup={lineGroup}
-        showMeter={showMeter}
-        showIpa={showIpa}
-      >
-        {children}
-      </AlignmentInfoPopover>
-    )
-  }
-
-  return isAnyWord(token) ? (
-    <AlignmentHoverTrigger token={token} lineGroup={lineGroup}>
-      {children}
-    </AlignmentHoverTrigger>
-  ) : (
-    <>{children}</>
-  )
 }
 
 function LemmaInfoPopover({
